ce8e3ae5d79b462a1c462f8536db883242b4947d
El Sonny <sonny@spaceshiplabs.com>
2013-03-05 12:58:46 -0600

    import progres

---
+++ config/config.default_config.php
@@ -0,0 +1,21 @@
+<?php
+class default_config{
+	public function __construct(){
+		//system configuration
+		$this->site_name =  'comparatuescuela.org';
+		$this->theme = 'finalabuso';
+		$this->default_controler = 'home';
+		$this->default_action = 'index';
+	
+		//Security 
+		$this->secured = false;
+		
+		//Sofware
+		$this->document_root = $_SERVER['DOCUMENT_ROOT']."/";
+		$this->lang = "en";
+		$this->dev_mode = false;
+		
+		
+	}
+}
+?>
