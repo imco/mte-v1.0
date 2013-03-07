a77e375f8a5eabc8ebe8b43cd0ca4fc8ec701535
Nish <nmukh1@gmail.com>
2013-03-07 16:48:20 -0600

    updated turnos
    Former-commit-id: 40b0ad9644389ca9f5f50ad2803a0f70bd1d7c77

---
+++ sql/turnos.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: ***REMOVED***
+-- Generation Time: Mar 07, 2013 at 04:45 PM
+-- Server version: 5.5.29
+-- PHP Version: 5.3.10-1ubuntu3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `comparatuescuela`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `turnos`
+--
+
+CREATE TABLE IF NOT EXISTS `turnos` (
+  `id` int(20) NOT NULL,
+  `nombre` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `cct_count` int(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `turnos`
+--
+
+INSERT INTO `turnos` (`id`, `nombre`, `cct_count`) VALUES
+(100, 'MATUTINO', 228587),
+(120, 'MATUTINO Y VESPERTINO', 9143),
+(123, 'MATUTINO,  VESPERTINO Y NOCTURNO', 724),
+(124, 'MATUTINO, VESPERTINO Y DISCONTINUO', 34),
+(125, 'MATUTINO, VESPERTINO Y CONTINUO', 2),
+(130, 'MATUTINO Y NOCTURNO', 219),
+(140, 'MATUTINO Y DISCONTINUO', 1),
+(200, 'VESPERTINO', 24212),
+(230, 'VESPERTINO Y NOCTURNO', 47),
+(240, 'VESPERTINO Y DISCONTINUO', 25),
+(300, 'NOCTURNO', 1981),
+(400, 'DISCONTINUO', 48355),
+(500, 'CONTINUO (TIEMPO COMPLETO)', 3352),
+(600, 'COMPLEMENTARIO', 191),
+(700, 'CONTINUO (JORNADA AMPLIA)', 5);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
