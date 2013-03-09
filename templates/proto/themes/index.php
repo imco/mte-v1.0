82a769646a73f9c798e9848ecd821d6c39310a99
El Sonny <sonny@spaceshiplabs.com>
2013-03-09 17:03:22 -0600

    home search
    Former-commit-id: bb79b37ceaff866c2d4d9357978e6ca9e87c54bf

--- templates/proto/themes/index.php
+++ templates/proto/themes/index.php
@@ -2,39 +2,57 @@
  <html lang="es">
  <head>
 	<meta charset="utf-8"/>
-
+	<?php
+		$css_scripts = array(
+			"reset.css",
+			"main.css"
+		);
+		
+		$js_scripts = array(
+			"jquery.js",
+			"interactions.js"
+		);
+		
+		$cssmin = new mxnphp_min($this->config,$css_scripts,"css","css-min-zavia-erp");
+		$jsmin = new mxnphp_min($this->config,$js_scripts,"js","js-min-zavia-erp");
+		$cssmin->tag('css');
+		$jsmin->tag('js');
+	?>
  </head>
  <body>
- <form action='/search' method='post' accept-charset='utf-8'>
- 	<table>
- 		<tr>
- 			<td>
- 				<select name=''>
-			 		<option value=''>Nivel de Escolaridad</option>
-					<?php foreach($this->niveles as $nivel) echo "<option value='{$nivel->id}'>{$nivel->nombre}</option>"; ?>
-			 	</select>
- 			</td>
-			<td>
-				<select name=''>
-			 		<option value=''>Estado</option>
-					<?php foreach($this->entidades as $entidad) echo "<option value='{$entidad->id}'>{$entidad->nombre}</option>"; ?>
-			 	</select>
+ <div id='header'></div>
+ <div id='content'>
+	 <form action='/search' method='post' accept-charset='utf-8' class='home-form'>
+	 	<table>
+	 		<tr>
+	 			<td>
+	 				<select name=''>
+				 		<option value=''>Nivel de Escolaridad</option>
+						<?php foreach($this->niveles as $nivel) echo "<option value='{$nivel->id}'>".ucwords(strtolower($nivel->nombre))."</option>"; ?>
+				 	</select>
+	 			</td>
+				<td>
+					<select name='' id='state-input'>
+				 		<option value=''>Estado</option>
+						<?php foreach($this->entidades as $entidad) echo "<option value='{$entidad->id}'>".ucwords(strtolower($entidad->nombre))."</option>"; ?>
+				 	</select>
 
-			</td>
- 		</tr>
- 		<tr>
- 			<td>
- 				<select name=''>
-			 		<option value=''>Municipio</option>
-					<?php foreach($this->municipios as $municipio) echo "<option value='{$municipio->id}'>{$municipio->nombre}</option>"; ?>
-			 	</select>
- 			</td>
-
- 		</tr>
- 	
-	</table>
-
- </form>
-
+				</td>
+	 		</tr>
+	 		<tr>
+	 			<td>
+	 				<select name='' id='municipio-input'>
+				 		<option value=''>Municipio</option>
+						<?php foreach($this->municipios as $municipio) echo "<option value='{$municipio->id}'>".ucwords(strtolower($municipio->nombre)).", ".ucwords(strtolower($municipio->entidad->nombre))."</option>"; ?>
+				 	</select>
+	 			</td>
+				<td>
+					<input name='nombre' type='text' placeholder='Nombre' />
+				</td>
+	 		</tr>
+		</table>
+		<input type='submit' value='Buscar'/>
+	 </form>
+</div>
  </body>
  </html>
