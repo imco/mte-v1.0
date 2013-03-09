b198d260b7adfc39f00f0801f16a5a7e00334e07
El Sonny <sonny@spaceshiplabs.com>
2013-03-09 17:03:22 -0600

    home search
    Former-commit-id: 862571486606c81b103e764627a8780ec4079a12

--- controlers/controler.main.php
+++ controlers/controler.main.php
@@ -5,5 +5,21 @@ class main extends controler{
 		$this->dbConnect();
 		$this->location = get_class($this);
 	}
+	public function load_municipios(){
+		$q = new municipio();
+		$q->search_clause = $this->post('entidad') ? 'municipios.entidad = "'.$this->post('entidad').'"' : '1';
+		$q->order_by = 'municipios.nombre';
+		$this->municipios = $q->read('id,nombre,entidad=>nombre,entidad=>id');
+		if($this->post('json')){
+			$response = array();
+			foreach($this->municipios as $key => $municipio){
+				$response[$key]->id = $municipio->id;
+				$response[$key]->nombre = ucwords(strtolower($municipio->nombre)).", ".ucwords(strtolower($municipio->entidad->nombre));
+			}
+			echo json_encode($response);
+		}
+		
+	}
+
 }
 ?>
