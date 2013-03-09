ecce5420120c760d25ebc8cbc7e894030a5cf4e6
El Sonny <sonny@spaceshiplabs.com>
2013-03-09 12:41:19 -0600

    templates and shit
    Former-commit-id: a6db5bbd0736f870ae37ff3ebd8214e8c0e220fb

--- controlers/controler.home.php
+++ controlers/controler.home.php
@@ -2,14 +2,25 @@
 class home extends main{
 	public function index(){
 		$this->load_niveles();
+		$this->load_entidades();
+		$this->load_municipios();
 		$this->include_theme('index','index');
 	}
 	public function load_niveles(){
 		$q = new nivel();
-		//$q->search_clause = 'niveles.id != "0"';
 		$q->search_clause = '1';
-		//$q->debug = true;
 		$this->niveles = $q->read('id,nombre');
+	}
+	public function load_entidades(){
+		$q = new entidad();
+		$q->search_clause = '1';
+		$this->entidades = $q->read('id,nombre');
+	}
+	public function load_municipios(){
+		$q = new municipio();
+		$q->search_clause = '1';
+		$q->order_by = 'municipios.nombre';
+		$this->municipios = $q->read('id,nombre,entidad=>nombre,entidad=>id');
 	}
 }
 ?>
