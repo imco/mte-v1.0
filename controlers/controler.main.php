d51d7012a8f466e186670c7641f8f10d01e97cc8
El Sonny <sonny@spaceshiplabs.com>
2013-03-10 14:35:04 -0600

    beta search
    Former-commit-id: 9153c3f7824ff4717edfab09c259ea046c442fef
    Former-commit-id: b5132df042243464eb9aa925ade934f0e9c9e474
    Former-commit-id: f0141def7a5c2e5e396741a0e1241e49cb71960b

--- controlers/controler.main.php
+++ controlers/controler.main.php
@@ -7,6 +7,7 @@ class main extends controler{
 	}
 	public function load_municipios(){
 		$q = new municipio();
+		//$q->debug = true;
 		$q->search_clause = $this->post('entidad') ? 'municipios.entidad = "'.$this->post('entidad').'"' : '1';
 		$q->order_by = 'municipios.nombre';
 		$this->municipios = $q->read('id,nombre,entidad=>nombre,entidad=>id');
@@ -14,46 +15,50 @@ class main extends controler{
 			$response = array();
 			foreach($this->municipios as $key => $municipio){
 				$response[$key]->id = $municipio->id;
-				$response[$key]->nombre = ucwords(strtolower($municipio->nombre)).", ".ucwords(strtolower($municipio->entidad->nombre));
+				$response[$key]->nombre = $this->capitalize($municipio->nombre).", ".$this->capitalize($municipio->entidad->nombre);
 			}
 			echo json_encode($response);
 		}
 		
 	}
 	public function load_localidades(){
-		$q = new localidad();
-		$q->search_clause = $this->post('entidad') ? 'localidades.entidad = "'.$this->post('entidad').'"' : '1';
-		$q->search_clause = $this->post('municipio') ? 'localidades.municipio = "'.$this->post('municipio').'"' : $q->search_clause;
-		$q->order_by = 'localidades.nombre';
-		//$q->debug = true;
-		$this->localidades = $q->read('id,nombre,entidad=>nombre,entidad=>id');
-		if($this->post('json')){
-			$response = array();
-			foreach($this->localidades as $key => $localidad){
-				$response[$key]->id = $localidad->id;
-				$response[$key]->nombre = ucwords(strtolower($localidad->nombre)).", ".ucwords(strtolower($localidad->entidad->nombre));
+		if($this->post('entidad') || $this->post('municipio')){
+			$q = new localidad();
+			$q->search_clause = $this->post('entidad') ? 'localidades.entidad = "'.$this->post('entidad').'"' : '1';
+			$q->search_clause = $this->post('municipio') ? 'localidades.municipio = "'.$this->post('municipio').'"' : $q->search_clause;
+			$q->order_by = 'localidades.nombre';
+			//$q->debug = true;
+			$this->localidades = $q->read('id,nombre,entidad=>nombre,entidad=>id');
+			if($this->post('json')){
+				$response = array();
+				foreach($this->localidades as $key => $localidad){
+					$response[$key]->id = $localidad->id;
+					$response[$key]->nombre = $this->capitalize($localidad->nombre);
+				}
+				echo json_encode($response);
 			}
-			echo json_encode($response);
+		}else{
+			$this->localidades = false;
 		}
 		
 	}
-	public function get_escuelas(){
-		$term = $this->request('term');
-		if($term){
-			$q = new escuela();
-			//$q->debug = true;
-			$q->search_clause = "escuelas.nombre LIKE '$term%' ";
-			$q->search_clause .= $this->post('entidad') ? 'AND escuelas.entidad = "'.$this->post('entidad').'" ' : '';
-			$q->search_clause .= $this->post('municipio') ? 'AND escuelas.municipio = "'.$this->post('municipio').'" ' : '';
-			$q->search_clause .= $this->post('localidad') ? 'AND escuelas.localidad = "'.$this->post('localidad').'" ' : '';
-			$q->order_by = 'escuelas.nombre';
-			$q->limit = '0, 10';
-			$escuelas = $q->read('cct,nombre,localidad=>nombre,localidad=>id,entidad=>nombre,entidad=>id');
+	public function get_escuelas(){		
+		$q = new escuela();
+		$q->search_clause = '1 ';
+		$q->search_clause .= $this->post('term') ? " AND escuelas.nombre LIKE '".$this->post('term')."%' " : '';
+		$q->search_clause .= $this->post('entidad') ? ' AND escuelas.entidad = "'.$this->post('entidad').'" ' : '';
+		$q->search_clause .= $this->post('municipio') ? ' AND escuelas.municipio = "'.$this->post('municipio').'" ' : '';
+		$q->search_clause .= $this->post('localidad') ? ' AND escuelas.localidad = "'.$this->post('localidad').'" ' : '';
+		$q->search_clause .= $this->post('nivel') === false || $this->post('nivel') === '' ? '' : ' AND escuelas.nivel = "'.$this->post('nivel').'" ';
+		$q->order_by = 'escuelas.nombre';
+		$q->limit = '0, 15';
+		$this->escuelas = $q->read('cct,nombre,localidad=>nombre,localidad=>id,entidad=>nombre,entidad=>id,nivel=>nombre');
+		if($this->post('json')){
 			$response = array();
-			if($escuelas){
-				foreach($escuelas as $key => $escuela){
-					$response[$key]->label = ucwords(strtolower($escuela->nombre)).', '.ucwords(strtolower($escuela->localidad->nombre)).', '.ucwords(strtolower($escuela->entidad->nombre));
-					$response[$key]->value = ucwords(strtolower($escuela->nombre));
+			if($this->escuelas){
+				foreach($this->escuelas as $key => $escuela){
+					$response[$key]->label = $this->capitalize($escuela->nombre).' | '.$this->capitalize($escuela->localidad->nombre).', '.$this->capitalize($escuela->entidad->nombre);
+					$response[$key]->value = $this->capitalize($escuela->nombre);
 				}
 			}
 			echo json_encode($response);
@@ -69,6 +74,9 @@ class main extends controler{
 		$q = new entidad();
 		$q->search_clause = '1';
 		$this->entidades = $q->read('id,nombre');
+	}
+	protected function capitalize($string){
+		return ucwords(strtolower($string));
 	}
 }
 ?>
--- templates/proto/global/general_search.php
+++ templates/proto/global/general_search.php
@@ -1,30 +1,55 @@
-<form action='/search' method='post' accept-charset='utf-8' class='home-form container'>
+<form action='/busqueda' method='post' accept-charset='utf-8' class='home-form container'>
  	<table>
  		<tr>
  			<td>
- 				<select name=''>
+ 				<select name='nivel' id='nivel-input'>
 			 		<option value=''>Nivel de Escolaridad</option>
-					<?php foreach($this->niveles as $nivel) echo "<option value='{$nivel->id}'>".ucwords(strtolower($nivel->nombre))."</option>"; ?>
+					<?php 
+					foreach($this->niveles as $nivel){
+						$selected = $this->post('nivel') == $nivel->id && $this->post('nivel') != '' ? "selected='selected'" : '';
+						echo "<option $selected value='{$nivel->id}'>".$this->capitalize($nivel->nombre)."</option>"; 
+					}
+					?>
 			 	</select>
  			</td>
 			<td>
-				<select name='' id='state-input'>
+				<select name='entidad' id='state-input'>
 			 		<option value=''>Estado</option>
-					<?php foreach($this->entidades as $entidad) echo "<option value='{$entidad->id}'>".ucwords(strtolower($entidad->nombre))."</option>"; ?>
+					<?php 
+					foreach($this->entidades as $entidad){
+						$selected = $this->post('entidad') == $entidad->id ? "selected='selected'" : '';
+						echo "<option $selected value='{$entidad->id}'>".$this->capitalize($entidad->nombre)."</option>";
+
+					} 
+					?>
 			 	</select>
 
 			</td>
  		</tr>
  		<tr>
  			<td>
- 				<select name='' id='municipio-input'>
+ 				<select name='municipio' id='municipio-input'>
 			 		<option value=''>Municipio</option>
-					<?php foreach($this->municipios as $municipio) echo "<option value='{$municipio->id}'>".ucwords(strtolower($municipio->nombre)).", ".ucwords(strtolower($municipio->entidad->nombre))."</option>"; ?>
+					<?php 
+					foreach($this->municipios as $municipio){
+						$selected = $this->post('municipio') == $municipio->id ? "selected='selected'" : '';
+						echo "<option $selected value='{$municipio->id}'>".$this->capitalize($municipio->nombre).", ".$this->capitalize($municipio->entidad->nombre)."</option>";
+					} 
+					?>
 			 	</select>
  			</td>
 			<td>					
- 				<select name='' id='localidad-input' disabled='disabled'>
+				<?php $disabled = !$this->localidades ? "disabled='disabled'" : ''; ?>
+ 				<select name='localidad' id='localidad-input' <?=$disabled?>>
 			 		<option value=''>Localidad</option>
+			 		<?php
+			 		if($this->localidades){
+			 			foreach($this->localidades as $localidad){
+							$selected = $this->post('localidad') == $localidad->id ? "selected='selected'" : '';
+							echo "<option $selected value='{$localidad->id}'>".$this->capitalize($localidad->nombre)."</option>";
+						} 
+			 		}
+			 		?>
 			 	</select>
 			</td>
  		</tr>
@@ -32,7 +57,7 @@
  			<td>
  			</td>
 			<td>
-				<input name='nombre' id='name-input' type='text' placeholder='Nombre' />
+				<input name='term' id='name-input' type='text' placeholder='Nombre' />
 			</td>
  		</tr>
 	</table>
