83efab412a5a512e849ac8b4752861ffc7b6d76d
El Sonny <sonny@spaceshiplabs.com>
2013-03-10 15:40:01 -0600

    config fix
    Former-commit-id: 9b861ab3b71d07d1463ac865f55ef83008e07dd5
    Former-commit-id: ff6aa424fec8e113c2ca17c0fb58bdba965b34fd
    Former-commit-id: 3f2183cc2e8d19f63c2b0364f37ef201523d6904

--- config/config.git_config.php
+++ config/config.git_config.php
@@ -8,7 +8,7 @@ class git_config extends default_config{
 		
 		//Database
 		$this->db_host = '***REMOVED***';
-		$this->db_name = 'compara';
+		$this->db_name = 'comparatuescuela';
 		$this->db_user = 'root';
 		$this->db_pass = 'RtG/()rERtfkfGKLF';
 		
