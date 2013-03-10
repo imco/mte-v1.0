62b2bf427caf1d21e3e02b6ecf5e1d7b16e304b4
El Sonny <sonny@spaceshiplabs.com>
2013-03-09 22:12:27 -0600

    a bunch of shit
    Former-commit-id: 49361072b09e365d818e67e52b287953172307c1
    Former-commit-id: e89200cfc23605e8f61f7c8a07fb92e7d6218265
    Former-commit-id: bb22c504b8b0fa87ebc3c2905fdafa4442b15296

--- controlers/controler.home.php
+++ controlers/controler.home.php
@@ -6,15 +6,5 @@ class home extends main{
 		$this->load_municipios();
 		$this->include_theme('index','index');
 	}
-	public function load_niveles(){
-		$q = new nivel();
-		$q->search_clause = '1';
-		$this->niveles = $q->read('id,nombre');
-	}
-	public function load_entidades(){
-		$q = new entidad();
-		$q->search_clause = '1';
-		$this->entidades = $q->read('id,nombre');
-	}
 }
 ?>
