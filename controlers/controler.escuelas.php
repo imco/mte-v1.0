c066b3a3222ec5e4971cdc6d06e56dfa6a98dc81
El Sonny <sonny@spaceshiplabs.com>
2013-03-12 12:47:20 -0600

    advance
    Former-commit-id: 2087c76ef06a16df56c6bb90c4308cc790f20c09
    Former-commit-id: 3c3f040c6ee83af856217af7d0e430090c10e5ff
    Former-commit-id: cbd39059783fcd80dd46c13a9fff0c0cfa5f4364

--- controlers/controler.escuelas.php
+++ controlers/controler.escuelas.php
@@ -2,11 +2,12 @@
 class escuelas extends main{
 	public function index(){
 		$this->escuela_info();
+		$this->get_escuelas($this->escuela->localidad->id);
 		$this->include_theme('index','index');
 	}
 	public function escuela_info(){
 		$this->escuela = new escuela($this->get('id'));
-		$this->escuela->read('cct,nombre,domicilio,entrecalle,ycalle,entidad=>nombre,municipio=>nombre,localidad=>nombre,codigopostal,telefono,telextension,fax,faxextension,correoelectronico,paginaweb,turno=>nombre,latitud,longitud,tipo=>nombre,nivel=>nombre,subnivel=>nombre,servicio=>nombre,control=>nombre,subcontrol=>nombre,sostenimiento=>nombre,status=>nombre');
+		$this->escuela->read('cct,nombre,domicilio,entrecalle,ycalle,entidad=>nombre,municipio=>nombre,localidad=>nombre,localidad=>id,codigopostal,telefono,telextension,fax,faxextension,correoelectronico,paginaweb,turno=>nombre,latitud,longitud,tipo=>nombre,nivel=>nombre,subnivel=>nombre,servicio=>nombre,control=>nombre,subcontrol=>nombre,sostenimiento=>nombre,status=>nombre');
 	}
 
 }
