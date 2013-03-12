00f1cb3519a116de88169d284794c1811f1ba159
El Sonny <sonny@spaceshiplabs.com>
2013-03-12 15:37:23 -0600

    perfil advance
    Former-commit-id: 8ddb9accbdc605760fab480b52b2eed7fa94669f
    Former-commit-id: e7bfd4e565f1d25e34f11a7056da30c30245a31b
    Former-commit-id: 8fafbe65fc027dfe100bdcc0a0ea6178b196032f

--- controlers/controler.escuelas.php
+++ controlers/controler.escuelas.php
@@ -2,13 +2,24 @@
 class escuelas extends main{
 	public function index(){
 		$this->escuela_info();
-		$this->get_escuelas($this->escuela->localidad->id);
+		$params->limit = '0,100';
+		$params->localidad = $this->escuela->localidad->id;
+		$params->nivel = $this->escuela->nivel->id;
+
+		$this->get_escuelas($params);
+		$this->process_escuelas();
+		//$this->escuelas_digest->escuelas = array();
+		$this->escuelas_digest->escuelas[] = $this->escuela;
+		$this->escuelas_digest->centerlat = $this->escuela->latitud;
+		$this->escuelas_digest->centerlong = $this->escuela->longitud;
+		$this->escuelas_digest->zoom++;
 		$this->include_theme('index','index');
 	}
 	public function escuela_info(){
 		$this->escuela = new escuela($this->get('id'));
-		$this->escuela->read('cct,nombre,domicilio,entrecalle,ycalle,entidad=>nombre,municipio=>nombre,localidad=>nombre,localidad=>id,codigopostal,telefono,telextension,fax,faxextension,correoelectronico,paginaweb,turno=>nombre,latitud,longitud,tipo=>nombre,nivel=>nombre,subnivel=>nombre,servicio=>nombre,control=>nombre,subcontrol=>nombre,sostenimiento=>nombre,status=>nombre');
+		$this->escuela->read('cct,nombre,domicilio,entrecalle,ycalle,entidad=>nombre,municipio=>nombre,localidad=>nombre,localidad=>id,codigopostal,telefono,telextension,fax,faxextension,correoelectronico,paginaweb,turno=>nombre,latitud,longitud,tipo=>nombre,nivel=>nombre,nivel=>id,subnivel=>nombre,servicio=>nombre,control=>nombre,subcontrol=>nombre,sostenimiento=>nombre,status=>nombre');
 	}
+
 
 }
 ?>
--- templates/proto/escuelas/index.php
+++ templates/proto/escuelas/index.php
@@ -3,10 +3,31 @@
 		
 	</div>
 	<h1><?=$this->capitalize($this->escuela->nombre)?></h1>
+
 	<div class='clear'></div>
+	<div id='map-data' class='hidden'><?= json_encode($this->escuelas_digest)?></div>
 	<div id='mapa'></div>
 	<div class='info'>
-		
-		
+		<div class='column'>
+			<h3><?=$this->capitalize($this->escuela->nivel->nombre)?> <?=$this->capitalize($this->escuela->nombre)?></h3>
+			<p><?=$this->capitalize($this->escuela->domicilio)?></p>
+			<p><?=$this->capitalize($this->escuela->localidad->nombre)?></p>
+			<p><?=$this->capitalize($this->escuela->municipio->nombre)?>, <?=$this->capitalize($this->escuela->entidad->nombre)?></p>
+
+			<ul class='features'>				
+				<li><?=$this->capitalize($this->escuela->turno->nombre)?></li>
+				<li><?=$this->capitalize($this->escuela->nivel->nombre)?></li>
+				<li><?=$this->capitalize($this->escuela->subnivel->nombre)?></li>
+				<li><?=$this->capitalize($this->escuela->control->nombre)?></li>
+				<li><?=$this->capitalize($this->escuela->subcontrol->nombre)?></li>
+				<li><?=$this->capitalize($this->escuela->sostenimiento->nombre)?></li>
+				<li><?=$this->capitalize($this->escuela->tipo->nombre)?></li>
+			</ul>
+		</div>
+		<div class='column'>
+			<p><?=$this->capitalize($this->escuela->servicio->nombre)?></p>
+			<p><?=$this->escuela->cct?></p>
+		</div>
+		<div class='clear'></div>
 	</div>
 </div>
