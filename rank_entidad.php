b9f74d8b845cb0ff02462a3f3171f95999cb1257
Nish <nmukh1@gmail.com>
2013-04-08 17:28:27 -0400

    added rank_entidad function
    Former-commit-id: 2d1a45c78f1bf2f477e41d49e8783ae3f58737d1
    Former-commit-id: 228fcbf3af30344fa6f5e5bcabd71b413e23967b
    Former-commit-id: b350760e3127800ff57e576247fc869b1912de9a

---
+++ rank_entidad.php
@@ -0,0 +1,48 @@
+<?php
+$host = '***REMOVED***';
+$user = 'root';
+$pwd = ''; /* Put your password here */
+$db = 'comparatuescuela';
+
+$conn = new mysqli($host,$user,$pwd,$db);
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+/*This will rank the schools at the entidad level */
+
+$entidades = range(1,32);
+$niveles = array(12,13,22);
+
+foreach($niveles as $nivel){
+
+	foreach($entidades as $entidad){
+		/*Using bound variables for efficiency
+		$stmt = $conn->stmt_init();
+		$stmt->prepare($query);
+		$stmt->bind_param('ii', $nivel, $municipio);
+		$stmt->execute();*/
+		$sql = "SET @rownum = 0, @rank = 0, @prev_val = NULL; ";
+		$conn->query($sql);
+		$sql = "UPDATE escuelas t1
+				JOIN (
+				SELECT @rownum := @rownum + 1 AS row,
+				@rank := IF(@prev_val!=promedio_general,@rownum,@rank) AS rank,
+				@prev_val := promedio_general AS promedio_general,
+				cct
+				FROM escuelas
+				WHERE nivel LIKE '$nivel' AND entidad LIKE '$entidad' AND `promedio_general` IS NOT NULL
+				ORDER BY promedio_general DESC) t2
+				ON t1.cct=t2.cct
+				SET t1.rank_entidad=t2.rank;";
+		if(!$conn->query($sql)){
+			echo "Table creation failed: (" . $conn->errno . ") " . $conn->error;
+		}
+	}
+}
+
+$conn->close();
+
+?>
