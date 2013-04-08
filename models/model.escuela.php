85755193197fcab100fc3cdc68f982135673e08b
El Sonny <sonny@spaceshiplabs.com>
2013-04-08 16:59:56 -0500

    nish rank
    Former-commit-id: 0579ac2cf387cf5b975ce7476174138e0dad0b15
    Former-commit-id: 73705ffe21ea92d0dd321c62c68051890083fcca
    Former-commit-id: 5e3cc67032337df47517dc9821dfbefb0ced35e0

---
+++ models/model.escuela.php
@@ -0,0 +1,38 @@
+<?php
+class escuela extends table{
+	function info(){
+		$this->table_name = "escuelas";
+		$this->key = 'cct';
+		$this->objects['localidad'] = 'localidad';
+		$this->objects['entidad'] = 'entidad';
+		$this->objects['municipio'] = 'municipio';
+		$this->objects['nivel'] = 'nivel';
+		$this->objects['turno'] = 'turno';
+		$this->objects['tipo'] = 'tipo';
+		$this->objects['subnivel'] = 'subnivel';
+		$this->objects['servicio'] = 'servicio';
+		$this->objects['control'] = 'control';
+		$this->objects['subcontrol'] = 'subcontrol';
+		$this->objects['sostenimiento'] = 'sostenimiento';
+		$this->objects['status'] = 'status';
+	}
+	
+	function rank($nivel,$entidad = false,$municipio = false){
+		$entidad_clause = $entidad ? " AND entidad LIKE '$entidad'" : '';
+		$sql = "SET @rownum = 0, @rank = 0, @prev_val = NULL; ";
+		mysql_query($sql);
+		$sql = "UPDATE escuelas t1
+				JOIN (
+				SELECT @rownum := @rownum + 1 AS row,
+				@rank := IF(@prev_val!=promedio_general,@rownum,@rank) AS rank,
+				@prev_val := promedio_general AS promedio_general,
+				cct
+				FROM escuelas
+				WHERE nivel LIKE '$nivel' $entidad_clause AND `promedio_general` IS NOT NULL
+				ORDER BY promedio_general DESC) t2
+				ON t1.cct=t2.cct
+				SET t1.rank_entidad=t2.rank;";
+		return mysql_query($sql);		
+	}
+}
+?>
