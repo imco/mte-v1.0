ebf21442566f345d9609b6a20a2a47978aab1f1d
El Sonny <sonny@spaceshiplabs.com>
2013-04-16 18:02:55 -0500

    home menu
    Former-commit-id: 77c6629930d283e88e4d0d188776b5a6ce9b7733
    Former-commit-id: 3af01ea2d9433545fca5df0d625dca0bbffce5ce
    Former-commit-id: a58dd8fc4e6c620f798ee6cd05dbba4a3feeb81a

---
+++ templates/mtev1/global/header.php
@@ -0,0 +1,10 @@
+<div class='menu'><div class='container'>
+	<a href='/' class='logo'><?php $this->print_img_tag('logo.png'); ?></a>
+	<a href='/blog'>Blog Iniciativas</a>
+	<a href='/comparador'>Comparador</a>
+	<a href='/denuncia'>Denuncia</a>
+	<a href='/voluntarios'>Voluntarios</a>
+	<a href='/webinars'>Webinars</a>
+	<a href='/contacto'>Contacto</a>
+	<div class='clear'></div>
+</div></div>
