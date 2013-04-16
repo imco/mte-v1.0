28d847faca0f5a181c1b153d34922c8c39eee213
El Sonny <sonny@spaceshiplabs.com>
2013-04-16 18:02:55 -0500

    home menu
    Former-commit-id: 401560ee6ff51b3199e76f097c7b9b2b1820fa56
    Former-commit-id: 0884487ad8287b965a6ce913afc50e3df79912a6
    Former-commit-id: df6f685596a50a355cac55109251c333a40cb9f3

---
+++ templates/mtev1/js/interactions.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+	$( "#name-input" ).autocomplete({
+  		source: function(request,response){
+  			$.post("/main/get_escuelas/",{
+  				term : request.term,
+  				entidad : $("#state-input").val(),
+  				municipio : $("#municipio-input").val(),
+  				localidad : $("#localidad-input").val(),
+  				nivel : $("#nivel-input").val(),
+  				json : true
+  			},response,'json');
+  		},
+  		minLength: 3
+	});
+	$('#map-button').click(function(e){
+		e.preventDefault();
+		$('#general-search').attr('action','/mapa/');
+		$('#general-search').submit();
+
+	});
+	$('#state-input').change(function(e){
+		load_location_options(
+			$("#municipio-input"),
+			'load_municipios',
+			{entidad:$(this).val(),json:true},
+			"Municipio"
+		);
+		if($(this).val() != ''){
+			load_location_options(
+				$("#localidad-input"),
+				'load_localidades',
+				{entidad:$(this).val(),json:true},
+				"Localidad"
+			);
+		}else{
+			$("#localidad-input").prop('disabled', true);
+			$("#localidad-input").html('<option value="">Localidad</option>');
+		}
+
+	});
+	$('#municipio-input').change(function(){
+		if($(this).val() != ""|| $("#state-input option:selected").val() != ""){
+			load_location_options(
+				$("#localidad-input"),
+				'load_localidades',
+				{municipio:$(this).val(),entidad:$("#state-input option:selected").val(),json:true},
+				"Localidad"
+			);
+		}else{
+			$("#localidad-input").prop('disabled', true);
+			$("#localidad-input").html('<option value="">Localidad</option>');
+		}
+	});
+});
+function load_location_options(input,directive,options,name){
+	input.prop('disabled', true);
+	$.post('/main/'+directive,options,function(data){
+		input.html('<option value="">'+name+'</option>');
+		for(x in data){
+			var item = data[x];
+			input.append('<option value="'+item.id+'">'+item.nombre+'</option>');
+		}
+		input.prop('disabled', false);
+		input.trigger('change');
+	},'json');
+}
