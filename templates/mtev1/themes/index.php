0187110fd3e8135bb01685e8bf7a52a847d70e7f
El Sonny <sonny@spaceshiplabs.com>
2013-04-16 18:02:55 -0500

    home menu
    Former-commit-id: d083f6229e01adf0f60c0e86a2eb1bc46d422193
    Former-commit-id: 1677a04aa6b17dd751f78e521e18554817643f05
    Former-commit-id: 101e7b46f12f02c18e92d03d501cecc478278816

---
+++ templates/mtev1/themes/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+ <html lang="es">
+ <head>
+	<meta charset="utf-8"/>
+
+	<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<?php
+
+		$css_scripts = array(
+			"reset.css",
+			"main.css",
+			"jquery-ui.css" 
+		);
+		
+		$js_scripts = array(
+			"jquery.js",
+			"interactions.js",
+			"jquery-ui.js"
+		);
+		if($this->location == 'mapa' || $this->location == 'escuelas'){
+			echo '<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBlzbyX3J7GwOXdoRwMDfYVbqxNG1D9Jy0&sensor=false"></script>';
+			$js_scripts[] = 'map.js';
+		}
+		
+		$cssmin = new mxnphp_min($this->config,$css_scripts,"css","css-min-mte");
+		$jsmin = new mxnphp_min($this->config,$js_scripts,"js","js-min-mte");
+		$cssmin->tag('css');
+		$jsmin->tag('js');
+	?>
+
+ </head>
+ <body>
+ <div id='header'><?php $this->include_template('header','global'); ?></div>
+ <div id='content'>
+ 	<?php $this->include_template($this->template,$this->location);	?>
+</div>
+ </body>
+ </html>
