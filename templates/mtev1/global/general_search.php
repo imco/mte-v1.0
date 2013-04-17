380c7e922219560e083d3dd46220af08660c835d
El Sonny <sonny@spaceshiplabs.com>
2013-04-16 19:17:55 -0500

    home adv
    Former-commit-id: 3d2ffd2a2a09fe5092497d9791db835b08868e79
    Former-commit-id: f676567d2fd73f56874520f671aebaf435e40a76
    Former-commit-id: 7e40cdc5a5be58ef9d5ff2daad543d6d3a5f3ea4

--- templates/mtev1/global/general_search.php
+++ templates/mtev1/global/general_search.php
@@ -1,69 +1,53 @@
 <form action='/busqueda' method='get' accept-charset='utf-8' class='home-form container' id='general-search'>
- 	<table>
- 		<tr>
- 			<td>
- 				<select name='nivel' id='nivel-input'>
-			 		<option value=''>Nivel de Escolaridad</option>
-					<?php 
-					foreach($this->niveles as $nivel){
-						$selected = $this->request('nivel') == $nivel->id && $this->request('nivel') != '' ? "selected='selected'" : '';
-						echo "<option $selected value='{$nivel->id}'>".$this->capitalize($nivel->nombre)."</option>"; 
-					}
-					?>
-			 	</select>
- 			</td>
-			<td>
-				<select name='entidad' id='state-input'>
-			 		<option value=''>Estado</option>
-					<?php 
-					foreach($this->entidades as $entidad){
-						$selected = $this->request('entidad') == $entidad->id ? "selected='selected'" : '';
-						echo "<option $selected value='{$entidad->id}'>".$this->capitalize($entidad->nombre)."</option>";
+	<h3>BUSCAR ESCUELA</h3>
+	<input name='term' id='name-input' type='text' placeholder='Nombre' value='<?=$this->request('term');?>' />
+	<a href='/home/avanzada/'> </a>
+	<fieldset name='busqueda-avanzada'>
+		<select name='nivel' id='nivel-input'>
+			<option value=''>Nivel de Escolaridad</option>
+			<?php 
+			foreach($this->niveles as $nivel){
+			$selected = $this->request('nivel') == $nivel->id && $this->request('nivel') != '' ? "selected='selected'" : '';
+			echo "<option $selected value='{$nivel->id}'>".$this->capitalize($nivel->nombre)."</option>"; 
+			}
+			?>
+		</select>
+		<select name='entidad' id='state-input'>
+			<option value=''>Estado</option>
+			<?php 
+			foreach($this->entidades as $entidad){
+				$selected = $this->request('entidad') == $entidad->id ? "selected='selected'" : '';
+				echo "<option $selected value='{$entidad->id}'>".$this->capitalize($entidad->nombre)."</option>";
 
-					} 
-					?>
-			 	</select>
+			} 
+			?>
+		</select>
 
-			</td>
- 		</tr>
- 		<tr>
- 			<td>
- 				<select name='municipio' id='municipio-input'>
-			 		<option value=''>Municipio</option>
-					<?php 
-					foreach($this->municipios as $municipio){
-						$selected = $this->request('municipio') == $municipio->id ? "selected='selected'" : '';
-						echo "<option $selected value='{$municipio->id}'>".$this->capitalize($municipio->nombre).", ".$this->capitalize($municipio->entidad->nombre)."</option>";
-					} 
-					?>
-			 	</select>
- 			</td>
-			<td>					
-				<?php $disabled = !$this->localidades ? "disabled='disabled'" : ''; ?>
- 				<select name='localidad' id='localidad-input' <?=$disabled?>>
-			 		<option value=''>Localidad</option>
-			 		<?php
-			 		if($this->localidades){
-			 			foreach($this->localidades as $localidad){
-							$selected = $this->request('localidad') == $localidad->id ? "selected='selected'" : '';
-							echo "<option $selected value='{$localidad->id}'>".$this->capitalize($localidad->nombre)."</option>";
-						} 
-			 		}
-			 		?>
-			 	</select>
-			</td>
- 		</tr>
- 		<tr>
- 			<td>
- 			</td>
-			<td>
-				<input name='term' id='name-input' type='text' placeholder='Nombre' value='<?=$this->request('term');?>' />
-			</td>
- 		</tr>
-	</table>
+		<select name='municipio' id='municipio-input'>
+			<option value=''>Municipio</option>
+			<?php 
+			foreach($this->municipios as $municipio){
+			$selected = $this->request('municipio') == $municipio->id ? "selected='selected'" : '';
+			echo "<option $selected value='{$municipio->id}'>".$this->capitalize($municipio->nombre).", ".$this->capitalize($municipio->entidad->nombre)."</option>";
+			} 
+			?>
+		</select>
+
+		<?php $disabled = !$this->localidades ? "disabled='disabled'" : ''; ?>
+		<select name='localidad' id='localidad-input' <?=$disabled?>>
+			<option value=''>Localidad</option>
+			<?php
+			if($this->localidades){
+				foreach($this->localidades as $localidad){
+					$selected = $this->request('localidad') == $localidad->id ? "selected='selected'" : '';
+					echo "<option $selected value='{$localidad->id}'>".$this->capitalize($localidad->nombre)."</option>";
+				} 
+			}
+			?>
+		</select>
+	</fieldset>
 	<p class='submits'>
 		<input type='submit' value='Buscar'/>
 		<input type='submit' value='Mapa' id='map-button' class='on'/>
-
 	</p>
  </form>
