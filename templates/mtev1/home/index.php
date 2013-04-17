525e0ad52cbd17d95e60b7db9001d431757704a9
El Sonny <sonny@spaceshiplabs.com>
2013-04-17 09:52:41 -0500

    mid morn
    Former-commit-id: 374d412362264110165a6db0504c4ef5fe80acb2
    Former-commit-id: b2f5fdba3b8df53ff495bb17539bdf3a289f7b73
    Former-commit-id: 4c45f3a4751082275eccf40ce7de1aa9f77d7a56

--- templates/mtev1/home/index.php
+++ templates/mtev1/home/index.php
@@ -4,27 +4,36 @@
 		<div class='gray-box'>
 			<h2>NOMBRE DE LA ESCUELA</h2>
 			<p>Datos generales.Nam justo dolor, porttitor eget iaculis vitae, congue porttitor nibh. Pellentesque sit amet mauris eget diam luctus bibendum. Nulla eget odio nunc </p>
-			<h3><a href="/">Leer más</a><h3>
+			<h3><a href="/">Leer más</a></h3>
 		</div>
 		<h1 class='cap blue'>Iniciativa más votada</h1>
 		<div class='gray-box'>
-			<p>Datos generales.Nam justo dolor, porttitor eget iaculis vitae, congue porttitor nibh. Pellentesque sit amet mauris eget diam luctus bibendum. Nulla eget odio nunc. Integer pretium gravida nunc, Datos generales.Nam justo dolor, porttitor eget iaculis vitae, congue porttitor nibh. Pellentesque sit amet mauris eget diam luctus bibendum. Nulla eget odio nunc. Integer pretium gravida nunc, </p>
-			<h3><a href="/">Leer más</a><h3>
+			<p>Datos generales.Nam justo dolor, porttitor eget iaculis vitae, congue porttitor nibh. Pellentesque sit amet mauris eget diam luctus bibendum. Nulla eget odio nunc. Integer pretium gravida nunc, Datos generales.Nam justo dolor, porttitor eget iaculis vitae, congue </p>
+			<h3><a href="/">Leer más</a></h3>
 		</div>
 		<div>
 			<div class='column'>
 				<h2>Título Iniciativa o Tip</h2>
 				<hr/>
-				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum viverra, metus in lobortis tempus, massa elit lacinia neque, vitae rhoncus turpis urna eget massa. Proin iaculis elementum fermentum. Aenean odio augue, hendrerit id consectetur quis, lacinia ac augue. Sed non mauris lectus, sed eleifend erat. Nullam nisl metus, ultricies eu porta vitae, convallis a odio. Vestibulum non justo ipsum. Suspendisse potenti. Quisque eleifend enim vel mauris tempus mollis. Quisque at magna sit amet mi vulputate facilisis. In hac habitasse platea dictumst. Curabitur rutrum ornare sem in cursus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque </p>
+				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum viverra, metus in lobortis tempus, massa elit lacinia neque, vitae rhoncus turpis urna eget massa. Proin iaculis elementum fermentum. Aenean odio augue, hendrerit id consectetur quis, lacinia ac augue. Sed non mauris lectus, sed eleifend erat. Nullam nisl metus, ultricies eu porta vitae, convallis a odio. Vestibulum non justo ipsum. Suspendisse potenti. Quisque eleifend enim vel mauris tempus mollis.</p>
 				<p><a href="/">Leer más</a><p>
 			</div>
 			<div class='column'>
 				<h2>Título Iniciativa o Tip</h2>
 				<hr/>
-				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum viverra, metus in lobortis tempus, massa elit lacinia neque, vitae rhoncus turpis urna eget massa. Proin iaculis elementum fermentum. Aenean odio augue, hendrerit id consectetur quis, lacinia ac augue. Sed non mauris lectus, sed eleifend erat. Nullam nisl metus, ultricies eu porta vitae, convallis a odio. Vestibulum non justo ipsum. Suspendisse potenti. Quisque eleifend enim vel mauris tempus mollis. Quisque at magna sit amet mi vulputate facilisis. In hac habitasse platea dictumst. Curabitur rutrum ornare sem in cursus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque </p>
+				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum viverra, metus in lobortis tempus, massa elit lacinia neque, vitae rhoncus turpis urna eget massa. Proin iaculis elementum fermentum. Aenean odio augue, hendrerit id consectetur quis, lacinia ac augue. Sed non mauris lectus, sed eleifend erat. Nullam nisl metus, ultricies eu porta vitae, convallis a odio. Vestibulum non justo ipsum. Suspendisse potenti. Quisque eleifend enim vel mauris tempus mollis.</p>
 				<p><a href="/">Leer más</a><p>
 			</div>
 			<div class='clear'></div>
+		</div>
+		<div>
+			<div class='white-box column'>
+				<?php $this->print_img_tag('schoolchildren.png');?>
+				<h2>Título Iniciativa o Tip</h2>
+				<hr/>
+				<p>roin iaculis elementum fermentum. Aenean odio augue, hendrerit id consectetur quis, lacinia ac augue. Sed non mauris lectus, sed </p>
+				<p><a href="/">Leer más</a></p>
+			</div>
 		</div>
 	</div>
 </div>
