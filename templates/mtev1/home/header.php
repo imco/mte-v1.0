8f89627246064908e2b70501eb1faf50c588284f
El Sonny <sonny@spaceshiplabs.com>
2013-04-16 19:17:55 -0500

    home adv
    Former-commit-id: d56033160799c8c78161a1dde7e3d4497c0ea150
    Former-commit-id: 89348c1824e2a4fe34dfa753c882a472ee22c4a7
    Former-commit-id: 871da7cb11f01f3fdcf99fd2ca6ed9dab8225647

---
+++ templates/mtev1/home/header.php
@@ -0,0 +1,15 @@
+<div class='container'>
+<?php $this->include_template('social-links','global');  ?>
+</div>
+<div class=' home  container'>
+	<div class='titles'>
+		<h1>MEJORA TU ESCUELA</h1>
+		<hr/>
+		<h2>CAMBIAR LA HISTORIA DEPENDE DE LO QUE HAGAMOS AHORA</h2>
+		<hr/>
+	</div>
+	<a href='' class='button'><span>Mandar Iniciativa</span></a>
+	<div class='clear'></div>
+	<?php $this->include_template('general_search','global'); ?>
+
+</div>
