cdda8eebf8c602f2794ffd7a69f177c9f12881c8
El Sonny <sonny@spaceshiplabs.com>
2013-04-18 22:40:16 -0500

    watup
    Former-commit-id: c90cc8355bfc53862c3fe64c3b3fc1578d56deae
    Former-commit-id: a54f370e77f87ab5a19d66400fc93c467f05f89e
    Former-commit-id: f638231031069f08fa0bc9cc15b9e1a6df1e4d9a

---
+++ templates/mtev1/global/footer.php
@@ -0,0 +1,11 @@
+<div class='img'>
+	<a href="http://imco.org.mx"><?php $this->print_img_tag('home/imco_footer.png');?></a>
+	<a href="http://imco.org.mx"><?php $this->print_img_tag('home/mexprimero_footer.png');?></a>
+</div>
+<p class='menu'>
+	<a href=''>¿Quienes somos?</a> |
+	<a href=''>Aviso Legal</a> |
+	<a href=''>Aviso de Privacidad</a> |
+	<a href=''>Contactanos</a>
+</p>
+<p class='sole'>Todos los derechos reservados. | Diseño y Desarollo web por <a href='http://spaceshiplabs.com'>Spaceshiplabs.</a></p>
