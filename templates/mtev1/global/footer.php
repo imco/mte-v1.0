3c4b1458d762e0cb45e48dc721096cea3031ede0
El Sonny <sonny@spaceshiplabs.com>
2013-04-18 22:49:48 -0500

    home full
    Former-commit-id: ac5f21ec5592355b93e0083e8b341dd8520e72be
    Former-commit-id: cb50f40b05f09cd4fec84bb2d3b6c7357f5bde07
    Former-commit-id: 5a68d503db350c064eab1b3abd7d43fc02991893

--- templates/mtev1/global/footer.php
+++ templates/mtev1/global/footer.php
@@ -1,6 +1,7 @@
 <div class='img'>
 	<a href="http://imco.org.mx"><?php $this->print_img_tag('home/imco_footer.png');?></a>
 	<a href="http://imco.org.mx"><?php $this->print_img_tag('home/mexprimero_footer.png');?></a>
+	<div class='clear'></div>
 </div>
 <p class='menu'>
 	<a href=''>¿Quienes somos?</a> |
