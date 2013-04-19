e090e5d2ed2de838aa44c892cded4bb5cdf4e4e8
El Sonny <sonny@spaceshiplabs.com>
2013-04-19 13:57:11 -0500

    watup
    Former-commit-id: c18cc173fbc3246eff7cc268f789a5d4a44d5738
    Former-commit-id: 27094d3c0eae83a49ebb785c0e3ff166ea516759
    Former-commit-id: a0814808bb94e2d20aca61412d03d78cb5d44bb8

--- templates/mtev1/themes/index.php
+++ templates/mtev1/themes/index.php
@@ -2,16 +2,13 @@
  <html lang="es">
  <head>
 	<meta charset="utf-8"/>
-
 	<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
 	<?php
-
 		$css_scripts = array(
 			"reset.css",
 			"main.css",
 			"jquery-ui.css"
 		);
-		
 		$js_scripts = array(
 			"jquery.js",
 			"interactions.js",
@@ -21,13 +18,11 @@
 			echo '<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBlzbyX3J7GwOXdoRwMDfYVbqxNG1D9Jy0&sensor=false"></script>';
 			$js_scripts[] = 'map.js';
 		}
-		
 		$cssmin = new mxnphp_min($this->config,$css_scripts,"css","css-min-mte");
 		$jsmin = new mxnphp_min($this->config,$js_scripts,"js","js-min-mte");
 		$cssmin->tag('css');
 		$jsmin->tag('js');
 	?>
-
  </head>
  <body>
  	<div id="wrap"><div id="main" class="clearfix"><div id="topBackRepeat">
@@ -37,7 +32,7 @@
 			$this->include_template('header',$this->header_folder); 
 			?>
 		</div>
-		<div id='content'><?php $this->include_template($this->template,$this->location);	?></div>
+		<div id='content'><?php $this->include_template($this->template,$this->location);?></div>
 	</div></div></div>	
 	<div id='footer'><?php $this->include_template('footer','global'); ?></div>	 
  </body>
