925b546c0da07ced79c84253c6c914a1fd5f237a
El Sonny <sonny@spaceshiplabs.com>
2013-04-28 16:01:37 -0500

    advances
    Former-commit-id: c1d204f0052f913960c7a438eb6be326ef4ec42c
    Former-commit-id: 143db053f26168bfdc4d7b3ea69dce0f4018d92c
    Former-commit-id: 84cc23936ffe2586b32f9198c8e4b0f86d7db71e

---
+++ templates/mtev1/escuelas/header.php
@@ -0,0 +1,2 @@
+<div class='container'><?php $this->include_template('social-links','global');  ?></div>
+<div class='perfil container'><?php $this->include_template('general_search','global'); ?></div>
