8cecfe59807acc0605abfc3f8cd7b4927773321f
El Sonny <sonny@spaceshiplabs.com>
2013-04-28 16:01:37 -0500

    advances
    Former-commit-id: 951edd9d7250af5205b45ed5bf7c0fc2a34cffea
    Former-commit-id: b1c55519d70a2b4dfae11421833e2c40f3a8eeee
    Former-commit-id: e8e3e894a8662c33fb6a9afc54869285aafc5c96

--- controlers/controler.escuelas.php
+++ controlers/controler.escuelas.php
@@ -12,15 +12,13 @@ class escuelas extends main{
 		$this->escuelas_digest->centerlat = $this->escuela->latitud;
 		$this->escuelas_digest->centerlong = $this->escuela->longitud;
 		$this->escuelas_digest->zoom++;
+		$this->header_folder = 'escuelas';
 		$this->include_theme('index','index');
 	}
 	public function escuela_info(){
 		$this->escuela = new escuela($this->get('id'));
 		//$this->escuela->debug = true;
-		$this->escuela->read('cct,nombre,domicilio,paginaweb,entrecalle,ycalle,entidad=>nombre,municipio=>nombre,localidad=>nombre,localidad=>id,codigopostal,telefono,telextension,fax,faxextension,correoelectronico,turno=>nombre,latitud,longitud,tipo=>nombre,nivel=>nombre,nivel=>id,subnivel=>nombre,servicio=>nombre,control=>nombre,subcontrol=>nombre,sostenimiento=>nombre,status=>nombre');
+		$this->escuela->read('cct,nombre,colonia,domicilio,paginaweb,entrecalle,ycalle,entidad=>nombre,municipio=>nombre,localidad=>nombre,localidad=>id,codigopostal,telefono,telextension,fax,faxextension,correoelectronico,turno=>nombre,latitud,longitud,tipo=>nombre,nivel=>nombre,nivel=>id,subnivel=>nombre,servicio=>nombre,control=>nombre,subcontrol=>nombre,sostenimiento=>nombre,status=>nombre');
 	}
-
-
-
 }
 ?>
