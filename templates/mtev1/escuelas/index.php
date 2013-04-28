fc454bcebbfdc9011e8b3159d48e927f51d0df02
El Sonny <sonny@spaceshiplabs.com>
2013-04-28 16:01:37 -0500

    advances
    Former-commit-id: fb71183a99adc1b3acdfe88a000a08b5ceff03ef
    Former-commit-id: 92529a49330ff9a197d2283f8ee96b6438a46c17
    Former-commit-id: 9fa7d1d8f8daa95733974d6a3cd130e6d045d98b

--- templates/mtev1/escuelas/index.php
+++ templates/mtev1/escuelas/index.php
@@ -1,43 +1,51 @@
 <div class='perfil container'>
-	<div class='ranking'>
-		
-	</div>
-	<h1><?=$this->capitalize($this->escuela->nombre)?></h1>
-
-	<div class='clear'></div>
-	<div id='map-data' class='hidden'><?= json_encode($this->escuelas_digest)?></div>
-	<div id='mapa'></div>
-	<div class='info'>
-		<div class='column'>
-			<h3><?=$this->capitalize($this->escuela->nombre)?></h3>
-			<p><?=$this->capitalize($this->escuela->domicilio)?></p>
-			<p><?=$this->capitalize($this->escuela->localidad->nombre)?></p>
-			<p><?=$this->capitalize($this->escuela->municipio->nombre)?>, <?=$this->capitalize($this->escuela->entidad->nombre)?></p>
-
-			<ul class='features'>				
-				
-				<li><?=$this->capitalize($this->escuela->nivel->nombre)?></li>
-				<li><?=$this->capitalize($this->escuela->turno->nombre)?></li>
-				<li><?=$this->capitalize($this->escuela->control->nombre)?></li>
-
-				
-			</ul>
+	<div class='head'>
+		<div class='ranking'>
+			<h1>46</h1>
+			<p>Posición <br/>Nivel Nacional</p>
 		</div>
-		<div class='column'>
-			<p>Clave SEP: <?=$this->escuela->cct?></p>
+		<div class='nivel elemental'><div class='bubble'></div>Elemental</div>
+		<h1 class='main-name'><?=$this->capitalize($this->escuela->nombre)?></h1>
+		<div class='semaforo'>
+			<h2>Semáforo Educativo</h2>
+			<h3 class='nivel reprobado'>Reprobado</h3>
+			<h3 class='nivel elemental'>Elemental</h3>
+			<h3 class='nivel bien'>Bien</h3>
+			<h3 class='nivel excelente'>Excelente</h3>
+		</div>
+	</div>
+	
+	<div class='info'>
+		<h1><?=$this->capitalize($this->escuela->nombre)?></h1>
+		<h2><?=$this->capitalize($this->escuela->nivel->nombre)?> | <?=$this->capitalize($this->escuela->turno->nombre)?> | <?=$this->capitalize($this->escuela->control->nombre)?></h2>
+		<hr />
+		<p class='icon dom'>
+			<?=$this->capitalize($this->escuela->domicilio)?><br/>
+			<?=$this->capitalize($this->escuela->localidad->nombre)?>, <?=$this->capitalize($this->escuela->entidad->nombre)?> <br />
+			<?=$this->capitalize($this->escuela->municipio->nombre)?> <br/>
+			<?=$this->escuela->cct?>
+		</p>
+		<p class='icon fon'><?=$this->escuela->telefono?></p>
+		<p class='icon email'><?=$this->escuela->correoelectronico?></p>
+		<p class='website'><?=$this->escuela->paginaweb?></p>
+
+
+			<!--<p>Clave SEP: </p>
 			<p>Servicio: <?=$this->capitalize($this->escuela->servicio->nombre)?></p>
 			<p>Subnivel: <?=$this->capitalize($this->escuela->subnivel->nombre)?></p>
 			<p>Subcontrol: <?=$this->capitalize($this->escuela->subcontrol->nombre)?></p>
 			<p>Sostenimiento: <?=$this->capitalize($this->escuela->sostenimiento->nombre)?></p>
 			<p>Tipo:<?=$this->capitalize($this->escuela->tipo->nombre)?></p>
 			<div class='contact'>
-				<p>Telefono: <?=$this->escuela->telefono?></p>
-				<p>Correo Electronico: <?=$this->escuela->correoelectronico?></p>
-				<p>Pagina Web: <?=$this->escuela->paginaweb?></p>
-			</div>
-		</div>
-		<div class='clear'></div>
+				<p>Telefono: </p>
+				<p>Correo Electronico: </p>
+				<p>Pagina Web: </p>
+			</div> -->
 	</div>
+
+	<div id='map-data' class='hidden'><?= json_encode($this->escuelas_digest)?></div>
+	<div id='mapa' class='map'></div>
+	
 	<ul class='tabs'>
 		<li><a href='#' >Comentarios | Reviews</a></li>
 		<li><a href='#' >Mas Info</a></li>
