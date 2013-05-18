62e5a550195eaa25b5a92b1fbdbe7002df06b93c
El Sonny <sonny@spaceshiplabs.com>
2013-05-17 21:01:14 -0500

    home updates
    Former-commit-id: 7052661adfcca08777c2812354a1bd9f719b86f9
    Former-commit-id: 5df983e3e6349d7d38e8d4ea6165a3561bb6ba02
    Former-commit-id: edd8d87a5b2c52a0868786968cb48b618a5c1621
    Former-commit-id: 6f1264e000145eda6e04a0ed800068e6afbdb38f

--- templates/mtev1/global/header.php
+++ templates/mtev1/global/header.php
@@ -1,10 +1,13 @@
 <div class='menu <?= $this->location?>'><div class='container'>
-	<a href='/' class='logo'><?php $this->print_img_tag('home/logo.png'); ?></a>
-	<a href='/blog'>Blog Iniciativas</a>
+	<a href='/' class='logo'><?php $this->print_img_tag('home/logo.png'); ?></a>	
 	<a href='/comparador'>Comparador</a>
-	<a href='/denuncia'>Denuncia</a>
-	<a href='/voluntarios'>Voluntarios</a>
-	<a href='/webinars'>Webinars</a>
-	<a href='/contacto'>Contacto</a>
+	<a href='/reportes-ciudadanos'>Reportes Ciudadanos</a>
+	<a href='/datos-abiertos'>Datos Abiertos</a>
+	<a href='/ayuda'>Ayuda</a>
+	<form method='get' action='/buscar/' accept-charset='utf-8' ><input type='text' placeholder='Buscar' /></form>
+	<div class='social'>
+		<a href='http://facebook.com' class='fb'></a>
+		<a href='http://twitter.com' class='twitter'></a>
+	</div>
 	<div class='clear'></div>
 </div></div>
