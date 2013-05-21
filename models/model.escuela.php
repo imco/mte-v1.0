87961a1d50571ceab393e30365b56357a95ecec6
El Sonny <sonny@spaceshiplabs.com>
2013-05-21 16:48:21 -0500

    escuelas negro
    Former-commit-id: 198065b4e577a05368b0d80ea8ecf0afd24c9ea2
    Former-commit-id: 6d0934b66adffab6d51c2875244391f0ed12b833
    Former-commit-id: a554463ab256ab3713a95edd167e3e16d8e8a6bf
    Former-commit-id: 6537fc7380410529773e1f3e2cffcdc3998fb104

--- models/model.escuela.php
+++ models/model.escuela.php
@@ -22,7 +22,7 @@ class escuela extends table{
 		$this->has_many['calificaciones'] = 'calificacion';
 		$this->has_many_keys['calificaciones'] = 'cct';
 
-		$this->semaforos = array('Reprobado','De Panzaso','Bien','Excelente','No Enlace');
+		$this->semaforos = array('Reprobado','De Panzaso','Bien','Excelente','Sin Enlace');
 		$this->semaforo_rangos[12] = array(400,480,590,900);
 		$this->semaforo_rangos[13] = array(400,467,575,900);
 		$this->semaforo_rangos[22] = array(349,416,497,900);
@@ -30,7 +30,7 @@ class escuela extends table{
 	}
 	function get_semaforo(){
 		$this->semaforo = 0;
-		if(isset($this->semaforo_rangos[$this->nivel->id]))
+		if(isset($this->semaforo_rangos[$this->nivel->id]) && $this->promedio_general != 0)
 			while($this->promedio_general > $this->semaforo_rangos[$this->nivel->id][$this->semaforo])	$this->semaforo++;
 		else
 			$this->semaforo = 4;
