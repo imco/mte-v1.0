073c2e58f5bf3df4a2dbc383f15a1873a4c4cf48
El Sonny <sonny@spaceshiplabs.com>
2013-05-21 12:05:55 -0500

    ccheckpoitn
    Former-commit-id: 34a075e0193345ef32df278962c5dfb045c71752
    Former-commit-id: cac5365cde554cee284e8ee1da56f3513be91113
    Former-commit-id: b14034d8bd78c9cd6fb017733dab251acc93c443
    Former-commit-id: 65c42e5db4a0d201a5937316bfabd75e9f481771

--- templates/mtev1/escuelas/header.php
+++ templates/mtev1/escuelas/header.php
@@ -1,1 +1,1 @@
-<div class='perfil container'><?php $this->include_template('general_search','global'); ?></div>
+<div class='perfil container'><?php $this->include_template('simple_search','global'); ?></div>
