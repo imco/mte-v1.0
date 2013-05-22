0c4dc70fd9c7093b14d70cb8be0008b0fb94eeb4
El Sonny <sonny@spaceshiplabs.com>
2013-05-22 08:18:48 -0500

    checkpoint
    Former-commit-id: 1d915958d27c50e1d99c70c56dfc966714141c9e
    Former-commit-id: 1b67856706a55ebab821a88ee41589acbcb311de
    Former-commit-id: 7846d9997abdf8f74abed6bd94a2b9c8a5bf59e4
    Former-commit-id: e1616c76d9af2bff6fcea33a815cdabb9c675f8d

--- templates/mtev1/global/general_search.php
+++ templates/mtev1/global/general_search.php
@@ -1,5 +1,5 @@
 <form action='/busqueda' method='get' accept-charset='utf-8' class='general-search' id='general-search'>
-	<p class='button-frame'><input name='term' id='name-input' type='text' placeholder='Búscar Escuela' value='<?=$this->request('term');?>' /></p>
+	<p class='button-frame'><input name='term' id='name-input' type='text' placeholder='Nombre de la Escuela' value='<?=$this->request('term');?>' /></p>
 	<p class='adv-search'><a href='/compara/' >Búsqueda Avanzada</a></p>
 	<fieldset name='busqueda-avanzada'>
 		<select name='nivel' id='nivel-input'>
