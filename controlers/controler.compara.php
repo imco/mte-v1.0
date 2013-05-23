f2d154316904143a16545f252b18045a64f82dd9
El Sonny <sonny@spaceshiplabs.com>
2013-05-22 21:48:33 -0500

    search fix
    Former-commit-id: 7df99d20b583475964b37e8897e5274490a2960c
    Former-commit-id: 3daa5677cc3519445f1502fb787bcdd187aadb2b
    Former-commit-id: b9846f8ba001ee743734054ea8d168f629d484a4
    Former-commit-id: 18a99cef818c387e23dca21ddde28bbfcad52ba9

--- controlers/controler.compara.php
+++ controlers/controler.compara.php
@@ -8,8 +8,7 @@ class compara extends main{
 		$this->get_location();
 		$this->header_folder = 'compara';
 		$params->limit = '0,10';
-		$params->entidad = $this->request('entidad') ? $this->request('entidad') : ( $this->user_location ? $this->user_location : 9 ) ;
-		
+		if(!$this->request('search')) $params->entidad = $this->user_location ? $this->user_location : 9 ;
 		$params->order_by = 'escuelas.promedio_general DESC';
 		$this->get_escuelas($params);
 		$this->include_theme('index','resultados');
