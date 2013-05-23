e31482ec247cff8e7a2fe14cfb9d0f46f100cd50
El Sonny <sonny@spaceshiplabs.com>
2013-05-22 21:07:54 -0500

    compara check
    Former-commit-id: 4acac737e958e2bb6d983592dc3d5faf3aca7e6f
    Former-commit-id: f7852038015e95fd5ce8e176f62750a5c50b9954
    Former-commit-id: 7572e917366b1081cb26ced34e814ab1a50220c1
    Former-commit-id: 980583021c774b76878469f6999e1246b31260cf

--- controlers/controler.compara.php
+++ controlers/controler.compara.php
@@ -5,7 +5,12 @@ class compara extends main{
 		$this->load_entidades();
 		$this->load_municipios();
 		$this->load_localidades();
+		$this->get_location();
 		$this->header_folder = 'compara';
+		$params->limit = '0,10';
+		$params->entidad = $this->user_location ? $this->user_location : 9;
+		//$this->get_escuelas($params);
+
 		$this->include_theme('index','index');
 	}
 }
