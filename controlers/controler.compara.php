8df49130685feb16513205118beb47d3dad2b92d
El Sonny <sonny@spaceshiplabs.com>
2013-05-22 21:33:28 -0500

    state stuff
    Former-commit-id: f99da7217f3879c8eec1379d3268f77c74c13f75
    Former-commit-id: 0261ae1a798da36fa854ff03a8dd5857fbae89b6
    Former-commit-id: ac99cc15b02e65c7bee8894418c68e2e06773384
    Former-commit-id: b4767d2a1a6ba98302cc6a6e060d68b7ed0ca9a3

--- controlers/controler.compara.php
+++ controlers/controler.compara.php
@@ -9,8 +9,8 @@ class compara extends main{
 		$this->header_folder = 'compara';
 		$params->limit = '0,10';
 		$params->entidad = $this->user_location ? $this->user_location : 9;
+		$params->order_by = 'escuelas.promedio_general DESC';
 		$this->get_escuelas($params);
-
 		$this->include_theme('index','resultados');
 	}
 }
