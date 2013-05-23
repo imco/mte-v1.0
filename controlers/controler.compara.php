ab821c45e76176c5ab5b570fe7366660a80b335e
El Sonny <sonny@spaceshiplabs.com>
2013-05-23 12:35:31 -0500

    compara cookie and link
    Former-commit-id: ca2612cd70f0bee71ba7e6fbd8b236176d8256c1
    Former-commit-id: 8c85735e09242c2e4dbd4cbfb9c2c093170882f6
    Former-commit-id: e12282e0ccd546ff973031a8d8e702300f82fa92
    Former-commit-id: d5f2451565e161f367c0d23176d0f98d0e823a5f

--- controlers/controler.compara.php
+++ controlers/controler.compara.php
@@ -5,14 +5,17 @@ class compara extends main{
 		$this->load_entidades();
 		$this->load_municipios();
 		$this->load_localidades();
+		$this->load_compara_cookie();
 		if(!$this->get('search')) $this->get_location();
-		$this->header_folder = 'compara';
-		$params->limit = '0,6';
-		$params->pagination = 6;
 		if(!$this->request('search')) $params->entidad = $this->user_location ? $this->user_location : 9 ;
-		$params->order_by = 'escuelas.promedio_general DESC';
+		$params->pagination = 6;		
+		$params->order_by = 'escuelas.promedio_general DESC';		
 		$this->get_escuelas($params);
+		$this->header_folder = 'compara';		
 		$this->include_theme('index','resultados');
+	}
+	public function escuelas(){
+		var_dump($this->get('id'));
 	}
 }
 ?>
