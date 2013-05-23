a5191710725a207ce4902a66b3adf565f88e9641
El Sonny <sonny@spaceshiplabs.com>
2013-05-23 10:56:24 -0500

    pagination
    Former-commit-id: 43dd2670e0b7d37c2215b1deb0123b36bf153f49
    Former-commit-id: acd7e26ae6ea41bc7de74f7576dc3b6c4f4e2c3f
    Former-commit-id: e4de3f27a7b36c042a7d35cfbbfda23243d4485c
    Former-commit-id: d7a1ad1c43dba052ca0db12e859a46bad4c7bbb4

--- controlers/controler.compara.php
+++ controlers/controler.compara.php
@@ -5,7 +5,7 @@ class compara extends main{
 		$this->load_entidades();
 		$this->load_municipios();
 		$this->load_localidades();
-		$this->get_location();
+		if(!$this->get('search')) $this->get_location();
 		$this->header_folder = 'compara';
 		$params->limit = '0,6';
 		$params->pagination = 6;
