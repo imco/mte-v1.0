cb512c176283224d5fb61ab53f196a8ee47a8b98
El Sonny <sonny@spaceshiplabs.com>
2013-05-22 21:25:14 -0500

    location test
    Former-commit-id: 56751cbf8b39082fcf65470810ce9fa81eb64a43
    Former-commit-id: 888111ed15cfadfeea606f9807df61a5297353fd
    Former-commit-id: 93ecee44387c117c66610f8a66b64a9af645a5ff
    Former-commit-id: f979e2ad9fece1e75a32077f37c7d7597fbc9ef1

---
+++ templates/mtev1/compara/resultados.php
@@ -0,0 +1,21 @@
+<div class='resultados container'>
+	<h1>Resultados</h1>
+	<table>
+	<?php
+	if($this->escuelas){
+		foreach($this->escuelas as $escuela){
+			echo "
+			<tr>
+				<td class='checkbox'><a href='#'></a></td>
+				<td class='score'></td>
+				<td class='school'><a href='/escuelas/index/{$escuela->cct}'>".
+					$this->capitalize($escuela->nombre)." (".$this->capitalize($escuela->nivel->nombre).") | ".
+					$this->capitalize($escuela->localidad->nombre).", ".$this->capitalize($escuela->entidad->nombre).
+				"</a></td>
+			</tr>
+			";
+		}
+	}	
+	?>
+	</table>
+</div>
