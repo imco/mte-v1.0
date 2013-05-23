35c0c6ddc338fcb2619fc39ebcfb816e8ac22026
El Sonny <sonny@spaceshiplabs.com>
2013-05-23 18:02:07 -0500

    compara tabs
    Former-commit-id: c64a2dfcf99b07b1b63b70ed7460bd8f741b4514
    Former-commit-id: e5fccc5a08b3475dada7e6e266d4baae0d402839
    Former-commit-id: c173b60bf6f72bdd2b2bde3aaa0bb3ae7dee3f72
    Former-commit-id: 1cd08fe20e45a6412e92f150f97cbc7565d1fd0e

--- templates/mtev1/compara/index.php
+++ templates/mtev1/compara/index.php
@@ -8,29 +8,20 @@
 		<div class='clear'></div>
 		<div class='shadow'></div>
 	</div>
-	<table>
-		<tr>
-			<th class='school'>Escuelas comparadas</th>
-			<th>Nivel Escolar</th>
-			<th class='rank'>Ranking Estatal</th>
-			<th>Privada | Pública</th>
-			<th class='calificacion'>Calificación Enlace de Español</th>
-			<th class='calificacion'>Calificación Enlace de Matematicas</th>			
-			<th class='semaforos'>Semaforo Educativo</th>
-		</tr>
-		<?php 
-		foreach($this->escuelas as $escuela){
-			$escuela->get_semaforo();
-			echo "<tr>";
-			echo "<td class='school'><a href='/escuelas/index/$escuela->cct'>".$this->capitalize($escuela->nombre)."</td>";
-			echo "<td>".$this->capitalize($escuela->nivel->nombre)."</td>";
-			echo "<td class='rank'><span>".$escuela->rank_entidad."</span></td>";
-			echo "<td>".$this->capitalize($escuela->control->nombre)."</td>";
-			echo "<td class='rank'><span>".round($escuela->promedio_espaniol)."</span></td>";
-			echo "<td class='rank'><span>".round($escuela->promedio_matematicas)."</span></td>";
-			echo "<td class='semaforo sem{$escuela->semaforo}'><span></span></td>";
-			echo "</tr>";
-		}
-		?>
-	</table>
+	<div class='compara-tab-container'>
+		<div class='tab on'>
+			<?php 
+			$this->current_rank->name = 'Estatal';
+			$this->current_rank->slug = 'rank_entidad';
+			$this->include_template('compare-table','compara');
+			?>
+		</div>
+		<div class='tab'>
+			<?php 
+			$this->current_rank->name = 'Nacional';
+			$this->current_rank->slug = 'rank_nacional';
+			$this->include_template('compare-table','compara');
+			?>
+		</div>
+	</div>
 </div>
