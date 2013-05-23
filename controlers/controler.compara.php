b10147eb265d05f4e16b3aaa65efab5e1015a91e
El Sonny <sonny@spaceshiplabs.com>
2013-05-22 21:25:45 -0500

    location test
    Former-commit-id: 78d8f95affe65940cbcb6607c083324b5eb820a8
    Former-commit-id: de8341603d0d9748ab1298959f9041ac193a23a5
    Former-commit-id: f43e29f8371580fd287d26d9a9246db0366f7f1d
    Former-commit-id: 2605edb38b1b10752f5d10f02f2eadb0c9ada863

--- controlers/controler.compara.php
+++ controlers/controler.compara.php
@@ -7,8 +7,8 @@ class compara extends main{
 		$this->load_localidades();
 		$this->get_location();
 		$this->header_folder = 'compara';
-		//$params->limit = '0,10';
-		//$params->entidad = $this->user_location ? $this->user_location : 9;
+		$params->limit = '0,10';
+		$params->entidad = $this->user_location ? $this->user_location : 9;
 		$this->get_escuelas($params);
 
 		$this->include_theme('index','resultados');
