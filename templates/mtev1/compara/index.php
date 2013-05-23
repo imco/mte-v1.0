b363d033fdcb2ba0a2d4f8996bb485c106241cb8
El Sonny <sonny@spaceshiplabs.com>
2013-05-23 16:10:27 -0500

    table headers
    Former-commit-id: f3677762f997b7e12209923b70be32e9dd96e9dd
    Former-commit-id: f17577cf818b1b4d0ba33d3270fbaeb8c0afbbd0
    Former-commit-id: 47ffc4e9f1e5749604653ed333de5c52ac69a33f
    Former-commit-id: 3b940f8350589b1870dfa8fe33f7b92e606a40fb

--- templates/mtev1/compara/index.php
+++ templates/mtev1/compara/index.php
@@ -1,4 +1,4 @@
-<div class='perfil container comparar'>
+<div class='perfil container comparar resultados'>
 	<div class='compara-tabs'>
 		<a href='#' class='general on'><span></span>General</a>
 		<a href='#' class='posicion-nacional'><span></span>Posición Nacional</a>
@@ -7,5 +7,15 @@
 		<a href='#' class='mapa'><span></span>Mapa</a>
 		<div class='clear'></div>
 	</div>
-
+	<table>
+		<tr>
+			<th class='school'>Escuelas comparadas</th>
+			<th>Nivel Escolar</th>
+			<th class='rank'>Ranking Estatal</th>
+			<th>Privada | Pública</th>
+			<th class='calificacion'>Calificación Enlace de Español</th>
+			<th class='calificacion'>Calificación Enlace de Matematicas</th>			
+			<th class='semaforos'>Semaforo Educativo</th>
+		</tr>
+	</table>
 </div>
