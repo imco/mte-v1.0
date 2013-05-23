833cdd9d5f480733212822988a9c0fec34790f71
El Sonny <sonny@spaceshiplabs.com>
2013-05-23 10:04:46 -0500

    fix
    Former-commit-id: 2c9fa0dd6996e8c06b4bcaa8930a7121ffe17d66
    Former-commit-id: a35df935b80debae584137d2c36107041d0e21a3
    Former-commit-id: b7154a8797b288f5fac11d0c4292561d0cc5664a
    Former-commit-id: f118876584105e7075290b732a0005bd8a1e6a3f

--- controlers/controler.compara.php
+++ controlers/controler.compara.php
@@ -8,6 +8,7 @@ class compara extends main{
 		$this->get_location();
 		$this->header_folder = 'compara';
 		$params->limit = '0,6';
+		$params->pagination = 6;
 		if(!$this->request('search')) $params->entidad = $this->user_location ? $this->user_location : 9 ;
 		$params->order_by = 'escuelas.promedio_general DESC';
 		$this->get_escuelas($params);
