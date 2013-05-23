c35aae2ffd964e90b42a04d2c4fd0038c7f0ed67
El Sonny <sonny@spaceshiplabs.com>
2013-05-23 07:20:58 -0500

    morning results checkpoint
    Former-commit-id: 43ac0c297d9b16c27751f9091f0169a9009569d7
    Former-commit-id: a4fc48625efb3a06bbee4576284c6931d9f01025
    Former-commit-id: 01c00206fc4846f75f5c93b1e7dfd4806b02a863
    Former-commit-id: 1470966d2d5c21ab70a18bc386bb2204cd881227

--- templates/mtev1/compara/resultados.php
+++ templates/mtev1/compara/resultados.php
@@ -1,17 +1,27 @@
+<a name='resultados'></a>
 <div class='resultados container'>
-	<h1>Resultados</h1>
+	<h1>Escuelas Similares</h1>
 	<table>
+		<tr>
+			<th class='checkbox'></th>
+			<th class='school'>Escuelas Similares</th>
+			<th class='nivel'>Nivel</th>
+			<th class='control'>Privada | Pública</th>
+			<th class='rank'>Ranking Nacional</th>
+		</tr>
 	<?php
 	if($this->escuelas){
 		foreach($this->escuelas as $escuela){
 			echo "
 			<tr>
 				<td class='checkbox'><a href='#'></a></td>
-				<td class='score'></td>
 				<td class='school'><a href='/escuelas/index/{$escuela->cct}'>".
-					$this->capitalize($escuela->nombre)." (".$this->capitalize($escuela->nivel->nombre).") | ".
-					$this->capitalize($escuela->localidad->nombre).", ".$this->capitalize($escuela->entidad->nombre).
+					$this->capitalize($escuela->nombre)." | ".
+					"<span>".$this->capitalize($escuela->localidad->nombre).", ".$this->capitalize($escuela->entidad->nombre)."</span>".
 				"</a></td>
+				<td class='nivel'>{$escuela->nivel->nombre}</td>
+				<td class='control'>{$escuela->control->nombre}</td>
+				<td class='rank'><span>{$escuela->rank_entidad}</span></td>
 			</tr>
 			";
 		}
