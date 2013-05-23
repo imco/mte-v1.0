70f2f8bb4b559a62d853c9c21885b5845e415e15
El Sonny <sonny@spaceshiplabs.com>
2013-05-23 13:11:12 -0500

    comparador
    Former-commit-id: 0bbae8732dbdadf6d70b9f5ebd234a4ebe692e1d
    Former-commit-id: 20571cd93b813087699a2a816ebc022b9b84dbb9
    Former-commit-id: f58b1d35f264d4499ef64ced765eedb718371b77
    Former-commit-id: a73fe4329fc7203316554a5da6a11c9ee5f6ebb0

---
+++ templates/mtev1/compara/index.php
@@ -0,0 +1,11 @@
+<div class='perfil container comparar'>
+	<div class='compara-tabs'>
+		<a href='#' class='general on'><span></span>General</a>
+		<a href='#' class='posicion-nacional'><span></span>Posición Nacional</a>
+		<a href='#' class='resultados-por-anio'><span></span>Resultados por año</a>
+		<a href='#' class='desempeno-de-alumnos'><span></span>Desempeño de Alumnos</a>
+		<a href='#' class='mapa'><span></span>Mapa</a>
+		<div class='clear'></div>
+	</div>
+
+</div>
