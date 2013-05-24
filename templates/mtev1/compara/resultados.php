454b3e48f0e58c782060dc0b12784106edbe0c5c
El Sonny <sonny@spaceshiplabs.com>
2013-05-24 08:45:19 -0500

    perfil resultados
    Former-commit-id: aa7952761f56226265ca26281d6ddc3e05a10285
    Former-commit-id: 9fe08dad60887746157199b2ffde480f25f52e89
    Former-commit-id: 82d64322cdbbc8a01cf8e55f3e4d0c0a4d5dc3af
    Former-commit-id: 74cb4b682112092963f6b05db5219b220d8db2b5

--- templates/mtev1/compara/resultados.php
+++ templates/mtev1/compara/resultados.php
@@ -13,19 +13,18 @@
 		</tr>
 	<?php
 	if($this->escuelas){
-		foreach($this->escuelas as $escuela){
-			$escuela->get_semaforo();
+		foreach($this->escuelas_digest->escuelas as $escuela){
 			$on = $this->compara_cookie && in_array($escuela->cct,$this->compara_cookie) ? "class='on'" : '';
 			echo "
 			<tr $on>
 				<td class='checkbox'><a class='compara-escuela' href='{$escuela->cct}'></a></td>
 				<td class='school'><a href='/escuelas/index/{$escuela->cct}'>".
-					$this->capitalize($escuela->nombre)." | ".
-					"<span>".$this->capitalize($escuela->localidad->nombre).", ".$this->capitalize($escuela->entidad->nombre)."</span>".
+					$escuela->nombre." | ".
+					"<span>".$escuela->direccion."</span>".
 				"</a></td>
-				<td class='nivel'>".$this->capitalize($escuela->nivel->nombre)."</td>
-				<td class='control'>".$this->capitalize($escuela->control->nombre)."</td>
-				<td class='rank'><span>{$escuela->rank_entidad}</span></td>
+				<td class='nivel'>".$escuela->nivel."</td>
+				<td class='control'>".$escuela->control."</td>
+				<td class='rank'><span>{$escuela->rank}</span></td>
 				<td class='semaforo sem{$escuela->semaforo}'><span></span></td>
 			</tr>
 			";
@@ -33,23 +32,23 @@
 	}	
 	?>
 	</table>
-	<div class='pagination'><?php 
-	$labels->prev_page = "<< primeras";
-	$labels->prev = "<<";
-	$labels->next_page = "últimas >>";
-	$labels->next = ">>";
-	$labels->hash = '#resultados';
-	$get = $_GET;
-	if(isset($get['action'])) unset($get['action']);
-	if(isset($get['p'])) unset($get['p']);
-	unset($get['controler']);
-	$query = http_build_query($get);
-	$query .= $query != '' ? '&' : '';
-	$this->pagination->echo_paginate('/compara/?'.$query,'p',5,false,$labels); 
+	<div class='pagination'><?php
+	if(isset($this->pagination)){
+		$labels->prev_page = "<< primeras";
+		$labels->prev = "<<";
+		$labels->next_page = "últimas >>";
+		$labels->next = ">>";
+		$labels->hash = '#resultados';
+		$get = $_GET;
+		if(isset($get['action'])) unset($get['action']);
+		if(isset($get['p'])) unset($get['p']);
+		unset($get['controler']);
+		$query = http_build_query($get);
+		$query .= $query != '' ? '&' : '';
+		$this->pagination->echo_paginate('/compara/?'.$query,'p',5,false,$labels); 
+	}
 	?></div>
-	<?php
-	$sufix = $this->compara_cookie ? implode('-',$this->compara_cookie) : '';
-	?>
+	<?php $sufix = $this->compara_cookie ? implode('-',$this->compara_cookie) : ''; ?>
 	<a id='compara-main-button' class="button-frame" href="/compara/escuelas/<?=$sufix?>">
 		<span class="button">Compara</span>
 	</a>
