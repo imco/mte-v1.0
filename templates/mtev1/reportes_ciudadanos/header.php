ee246bef2c8d35b2c600e2c6964f2b21d82e8ac2
El Sonny <sonny@spaceshiplabs.com>
2013-05-24 17:42:12 -0500

    reportes ciudadanos html css
    Former-commit-id: 55a8e094f10eae346854bc47686f5e5a6a337dae
    Former-commit-id: 0be78b5a05fd0c6134be46fece80cb2331e665b7
    Former-commit-id: 8906b558c631e7404cae86319bb085f1f2b3cf78
    Former-commit-id: cca1d5d3d5bfd2ca998d37f7fd86b4b45ebe1304

--- templates/mtev1/reportes_ciudadanos/header.php
+++ templates/mtev1/reportes_ciudadanos/header.php
@@ -1,1 +1,13 @@
-<div class='perfil container reportes-ciudadanos'></div>
+<div class='perfil container reportes-ciudadanos'>
+	<div class='box'>
+		<h1><span></span>Reportes Ciudadanos</h1>
+		<div class='content'>
+			<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis. </p>
+		</div>
+		<div class='search'>
+			<p class='adv-search'><a href='/compara/' >Búscar Escuela</a></p>
+		</div>
+		<div class='clear'></div>
+	</div>
+
+</div>
