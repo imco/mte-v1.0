d476494c485f7f566d85e28a92a66795b272d454
El Sonny <sonny@spaceshiplabs.com>
2013-05-24 17:42:12 -0500

    reportes ciudadanos html css
    Former-commit-id: af101f4814d5499201aac505c86bc6cbe19a1f5d
    Former-commit-id: 2ea1ed652bafd6dad2736f1674e631c933793d3f
    Former-commit-id: 3f4d90e8b1a8900f10de27adf26adc3c915b22ee
    Former-commit-id: fc7f5e0f10f33f044b463b6a77a8d6406aa55ca5

--- templates/mtev1/reportes_ciudadanos/index.php
+++ templates/mtev1/reportes_ciudadanos/index.php
@@ -1,1 +1,18 @@
-hello world
+<div class='container reportes'>
+	<?php for($i = 0;$i<9;$i++){ ?>
+	<div class='reporte'>
+		<div class='top'>
+			<h2>Reporte Ciudadano</h2>
+			<hr/>
+			<p> Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quisnon mollis leo ornare quis.</p>
+			<p><a href='/escuelas/index/'>Nombre de la Excuela</a></p>
+			<p><a href='/escuelas/index/'>Estado</a></p>
+			<p class='auth'>Por Daniel Martinez</p>
+		</div>
+		<div class='button votes '>353<span>VOTOS</span></div>
+		<a href='#' class='button share'>COMPARTIR</a>
+		<a href='#' class='button vote'><span></span>VOTAR</a>
+	</div>
+	<?php } ?>
+	<div class='clear'></div>
+</div>
