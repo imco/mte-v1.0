a7b507b85f90b81cecfa803d75fa23125c98b421
Irving Sulub <irving.sci@gmail.com>
2013-05-23 20:36:03 -0500

    no confiables
    Former-commit-id: beb1c36f7c7ef442c39f02d209c289789a59348e
    Former-commit-id: 01018683e7da5ce2ca44545204de0964f9dbc42e
    Former-commit-id: 74873573e31a13b1f3fc367efeac858c23258d7d
    Former-commit-id: 43fbdbe567c64c731ac1cc62dbe145fac7c43484

--- templates/mtev1/escuelas/index.php
+++ templates/mtev1/escuelas/index.php
@@ -18,7 +18,7 @@
 			<? }else if($this->escuela->semaforo == 5){?>
 				<div class='pop-up-triangle shadow'></div>
 				<div class='pop-up'>
-					<p class='Poco-confiable'>Escuela en donde arriba del 10% de los resultados se catalogan como <strong>"no confiables."</strong></p>
+					<p class='Poco-confiable'>Escuela en donde arriba del 10% de los resultados se catalogan como <strong>"no confiables". (<?= $this->escuela->porcentaje_poco_confiable ?>%)"</strong></p>
 				</div>
 			<?php }?>
 			<div class='pop-up-triangle'></div>
