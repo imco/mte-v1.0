4763245dcee8bb9a7e172d65616eb90c348aed30
El Sonny <sonny@spaceshiplabs.com>
2013-05-24 12:50:52 -0500

    busqueda mapa
    Former-commit-id: 0ca1cc0750b9f0c97fa56110574f190d73feeb2d
    Former-commit-id: f41411059054eda7b64ad25c31d363ad75f3e750
    Former-commit-id: 4afa59a96de9f04957a9fac8f8bbec1ee978a186
    Former-commit-id: b0c7dbadc3d7c8fbc9df78f36feda9e68ffa141a

--- controlers/controler.compara.php
+++ controlers/controler.compara.php
@@ -19,6 +19,25 @@ class compara extends main{
 		$this->resultados_title = 'Resultados';
 		$this->include_theme('index','resultados');
 	}
+	public function mapa(){
+		$this->load_niveles();
+		$this->load_entidades();
+		$this->load_municipios();
+		$this->load_localidades();
+		$this->load_compara_cookie();
+		$this->user_location = false;
+		$this->draw_map = true;
+		if(!$this->request('search')){
+			$params->entidad = $this->user_location ? $this->user_location : 9 ;
+		}
+		$params->pagination = 100;		
+		$params->order_by = 'escuelas.promedio_general DESC';		
+		$this->get_escuelas($params);
+		$this->process_escuelas();
+		$this->escuelas_digest->zoom += 1;
+		$this->header_folder = 'compara';
+		$this->include_theme('index','map');
+	}
 	public function escuelas(){
 		$this->header_folder ='escuelas';
 		$this->draw_map = true;
