af2085f8285a6d8a8438c939df21470d3604abf9
El Sonny <sonny@spaceshiplabs.com>
2013-05-24 19:36:17 -0500

    datos abiertos
    Former-commit-id: 874118e4d905b8d6576e177b95b81feb72f0760f
    Former-commit-id: 35c0309b8fc31e23432d078e01658e469e090be7
    Former-commit-id: cfed93a615a69cae08aec8212f5815a2dd7bf896
    Former-commit-id: d77547bac50059a5a99bef595d08cde508b1f7e1

--- controlers/controler.datos_abiertos.php
+++ controlers/controler.datos_abiertos.php
@@ -2,7 +2,12 @@
 class datos_abiertos extends main{
 	public function index(){		
 		$this->header_folder ='datos_abiertos';
+		$this->load_file_list();
 		$this->include_theme('index','index');
+	}
+	private function load_file_list(){
+		$files = file_get_contents($this->config->document_root.'/files/files-index.js');
+		$this->files = json_decode($files);
 	}
 }
 ?>
