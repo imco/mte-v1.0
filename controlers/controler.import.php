391d413df4f5e09916cda92150d6d33ca41fd7fd
Irving Sulub <irving.sci@gmail.com>
2013-05-24 23:51:17 -0500

    fix
    Former-commit-id: a5e066307e4b92f7dc31d0769c14e05f1a0100d1
    Former-commit-id: 310245c158c20512c7a32988fdc5ccb8e15a704b
    Former-commit-id: d809fed77af8bacdd603390838e88b1e0960cef5
    Former-commit-id: 8e845611ea183d7498361cad07841957555f4e05

--- controlers/controler.import.php
+++ controlers/controler.import.php
@@ -12,9 +12,9 @@ class import extends main{
 //		$this->import_no_confiables();
 		//$this->count_enlaces(31);
 		//$this->average_enlaces(21,1);
-		$this->update_schools();
-		$this->update_counties();
-		$this->update_locales();
+// 		$this->update_schools();
+// 		$this->update_counties();
+// 		$this->update_locales();
 
 	}
 	
