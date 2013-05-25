47ff7404d7eec4afe5c11abfe9481a5d7b53d0ef
urielaero <aero.uriel@gmail.com>
2013-05-24 23:12:41 -0500

    Lineas de circulos home
    Former-commit-id: 2795d4f376147eff3561c2a54a13558f53380577
    Former-commit-id: e3e7c63a95d2b5ca09288ff7ffae477426916a61
    Former-commit-id: ae2b0320164762734e59396256a4c1ef1505574b
    Former-commit-id: 4795b3424b30d287ef96e907fe0646be0c3f5665

--- templates/mtev1/home/index.php
+++ templates/mtev1/home/index.php
@@ -116,9 +116,11 @@
 	</div>
 	<div class='circle'>
 		<a href='#'></a>
-		<a href='#'></a>
-		<a href='#'></a>
-		<a href='#'></a>
+		<a class='line' href='#'></a>
+		<a class='line' href='#'></a>
+		<a class='line' href='#'></a>
+		<div class="line1"></div>
+		<div class="line2"></div>
 	
 	</div>
 	<div class='clear'></div>
