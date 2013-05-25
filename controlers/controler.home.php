a179ab2f038d21f7787e0edf3f70bec872109f40
El Sonny <sonny@spaceshiplabs.com>
2013-05-25 17:53:01 -0500

    home schools
    Former-commit-id: b65de6ece5fc798d56f24574eedc747f28e382cf
    Former-commit-id: 5fc003a396c56a8cebae039f27a7dc8da7cb7a75
    Former-commit-id: a18a118e1c0519861da25a674e6a33bd3c40f9a2
    Former-commit-id: 06496d0c214434d800d83b34f3331d60a763b1b1

--- controlers/controler.home.php
+++ controlers/controler.home.php
@@ -10,6 +10,7 @@ class home extends main{
 	}
 	public function load_escuelas(){
 		$niveles = array(12,13,22);
+		$this->get_location();
 		$params->order_by = ' ISNULL(escuelas.rank_nacional), escuelas.rank_nacional ASC, escuelas.promedio_general DESC';
 		$params->nivel = $niveles[rand(0,2)];
 		$params->rank_nacional = rand(1,100);
@@ -20,6 +21,13 @@ class home extends main{
 		$params->control = 2;
 		$this->get_escuelas($params);
 		$this->privada = $this->escuelas[0];
+		$params2->nivel = $params->nivel;
+		$params2->entidad = $this->user_location->id;
+		$params2->order_by = ' ISNULL(escuelas.rank_nacional), escuelas.rank_nacional ASC, escuelas.promedio_general DESC';
+		$params2->limit = '0,5';
+		$this->debug = true;
+		$this->get_escuelas($params2);
+		$this->process_escuelas();
 	}
 
 }
