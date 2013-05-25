ffc77d72a1bb54985abb1b6d6382588fbcb63ef6
urielaero <aero.uriel@gmail.com>
2013-05-25 03:29:18 -0500

    Decoradores compara
    Former-commit-id: f6ce79edac45d51c1f9a7aade8768ef01baf6c45
    Former-commit-id: ba1f5fdc6946b6e0598e145e744c6a582286aee0
    Former-commit-id: 861af1804033d6fd8ab028013ba84895d9a3c53b
    Former-commit-id: 3de8719c798656f1ad22a9c5818912d0b697fd8a

--- templates/mtev1/compara/header.php
+++ templates/mtev1/compara/header.php
@@ -8,4 +8,19 @@
 	</div>
 	<div class='clear'></div>
 	<?php $this->include_template('general_search','global'); ?>
+	<div class='decorations'>
+		<div class='triangle1'></div>
+		<div class='triangle2'></div>
+		<div class='circle'></div>
+		<div class='circle'></div>
+		<div class='circle'></div>
+		<div class='circle'></div>
+		<hr /><hr /><hr /><hr /><hr /><hr /><hr /><hr /><hr /><hr />
+		<?php $this->print_img_tag('home/palomita.png');?>
+		<?php $this->print_img_tag('home/birrete_small.png');?>
+		<?php $this->print_img_tag('home/footer_vineta.png');?>
+	</div>
+</div>
+<div class='decorations compara'>
+	<hr /><hr /><hr />
 </div>
