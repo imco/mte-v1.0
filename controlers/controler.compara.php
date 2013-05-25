2ea9307e2cf6788fab6cec71d7a2d8e254abc798
El Sonny <sonny@spaceshiplabs.com>
2013-05-25 16:21:32 -0500

    re order
    Former-commit-id: a33d19548bd6adb83d2f9acd3e3f4d272a90f3c6
    Former-commit-id: dacc50ede3acf1b9bffe7610f8a2e96b3461c058
    Former-commit-id: 7e24bb2f7cc6dbd7e85e85f6d2b9723b824ad5d3
    Former-commit-id: 06ba6e1dfc6aac6c1bba73c89df1e8dcd6a48410

--- controlers/controler.compara.php
+++ controlers/controler.compara.php
@@ -12,7 +12,7 @@ class compara extends main{
 			$params->entidad = $this->user_location ? $this->user_location : 9 ;
 		}
 		$params->pagination = 6;		
-		$params->order_by = ' ISNULL(escuelas.rank_entidad), escuelas.rank_entidad ASC';
+		$params->order_by = ' ISNULL(escuelas.rank_entidad), escuelas.rank_entidad ASC, escuelas.promedio_general';
 		$this->get_escuelas($params);
 		$this->process_escuelas();
 		$this->header_folder = 'compara';		
