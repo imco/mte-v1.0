d38ae27152fc6370c011ba97def9cb62155fea03
urielaero <aero.uriel@gmail.com>
2013-05-28 00:57:17 -0500

    Ayuda
    Former-commit-id: c59c993fd2d546f8aadb30aa3d9a2d0495f06b4d
    Former-commit-id: 34f90d1dc6772f4c3ca680998acb19ad82b09b81
    Former-commit-id: 10d0ba80cba581ff2705c5b3ac884123e74ab64a
    Former-commit-id: d675f9cfb69ec487352f39f547f92dd53b9b5b00

---
+++ templates/mtev1/ayuda/index.php
@@ -0,0 +1,53 @@
+<div class='container ayuda'>
+	<div class='ayuda'>
+		<h2>Preguntas frecuentes</h2>
+		<hr/>
+		<div class="text-ayuda">
+			<div class="section-one">
+				<h3>Proin gravida nibh vel velit auctor aliquet?</h3>
+				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.</p>
+				<div class="img-example">
+					<?php $this->print_img_tag('schoolchildren.png');?>
+				</div>
+
+				<h3>Proin gravida nibh vel velit auctor aliquet?</h3>
+				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed malesuada malesuada nibh, non mollis leo ornare quis.</p>
+			</div>
+			<div class="section-two">
+				<h2>Pasos de ejemplo</h2>
+				<h3>Paso 1</h3>
+				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed malesuada.</p>
+				<div class="img-example">
+					<?php $this->print_img_tag('schoolchildren.png');?>
+				</div>
+				<div class="img-example">
+					<?php $this->print_img_tag('schoolchildren.png');?>
+				</div>
+			</div>
+				
+			<div class="section-two">
+				<h3>Paso 2</h3>
+				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed malesuada.</p>
+				<div class="img-example">
+					<?php $this->print_img_tag('schoolchildren.png');?>
+				</div>
+				<div class="img-example">
+					<?php $this->print_img_tag('schoolchildren.png');?>
+				</div>
+			</div>
+
+			<div class="section-two">
+				<h3>Paso 3</h3>
+				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed malesuada.</p>
+				<div class="img-example">
+					<?php $this->print_img_tag('schoolchildren.png');?>
+				</div>
+				<div class="img-example">
+					<?php $this->print_img_tag('schoolchildren.png');?>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<div class='clear'></div>
+</div>
