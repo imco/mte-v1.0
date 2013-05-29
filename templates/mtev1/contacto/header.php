e253ff227b81626cb4be38826859466b5ef3d30e
urielaero <aero.uriel@gmail.com>
2013-05-28 21:36:59 -0500

    Contacto
    Former-commit-id: c5bcc305ff33dcddaeccb194b37658e5a6884e1f
    Former-commit-id: ac55e6088ef2b5f2e7389d1c1adecd5fce121bc3
    Former-commit-id: ef6dc70eed6560a6953acf750eaf585d2e1e66a2
    Former-commit-id: dc90767aac659259cc78cad0eac73cb1b5ed8e52

---
+++ templates/mtev1/contacto/header.php
@@ -0,0 +1,16 @@
+<div class='container reportes-ciudadanos contacto'>
+	<div class='box'>
+		<h1><span></span>Contáctenos</h1>
+		<div class='content'>
+			<form action=''>
+				<input name='' type='text' placeholder='Nombre' />
+				<input name='' type='text' placeholder='Correo Electrónico' />
+				<textarea name='' rows='7' cols='68'  placeholder='Mensaje' ></textarea>
+				<input type="submit" value="Enviar" />
+			</form>
+		</div>	
+		<p><span class='icon'></span>contacto@mejoratuescuela.org</p>
+		<div class='clear'></div>
+	</div>
+
+</div>
