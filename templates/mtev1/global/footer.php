02721511bb615d909cf793aeebf0b4f59ac19a56
urielaero <aero.uriel@gmail.com>
2013-05-28 21:40:00 -0500

    Link a contacto
    Former-commit-id: 93c39c54e99f096b27a1cc912e8c9e066f759d80
    Former-commit-id: 07052c758a82d1b1905e792535f08baae30370a6
    Former-commit-id: b008f6274b95ce49faedc3112376ef47f7ec1f83
    Former-commit-id: ad754328552fdde0bb265761974d119091412052

--- templates/mtev1/global/footer.php
+++ templates/mtev1/global/footer.php
@@ -1,6 +1,6 @@
 <p class='menu'>
 	<a href=''>¿Quienes somos?</a> |
 	<a href=''>Aviso Legal y de Privacidad</a> |
-	<a href=''>Contactanos</a>
+	<a href='/contacto'>Contactanos</a>
 </p>
 <p class='sole'>Todos los derechos reservados. | Diseño y Desarollo web por <a href='http://spaceshiplabs.com'>Spaceshiplabs.</a></p>
