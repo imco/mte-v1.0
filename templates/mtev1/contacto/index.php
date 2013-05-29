db6e46815412cbf7aabb5891fa4f38d9d11f98ea
urielaero <aero.uriel@gmail.com>
2013-05-28 21:36:59 -0500

    Contacto
    Former-commit-id: 4b7e6a9101b5e1862c4a8e56385373eb95d993d4
    Former-commit-id: 2582c25a5de93fa4c9e79ab48c78e9b5ce735f1b
    Former-commit-id: 23682bfe9506cbc222ed9e32bc57e90953d5795b
    Former-commit-id: 1383820e8392d2e5223f9a761fd4f48ce7d814ce

---
+++ templates/mtev1/contacto/index.php
@@ -0,0 +1,12 @@
+<div class='container home contacto'>
+	<div class='circle'>
+		<a href='#'></a>
+		<a class='line' href='#'></a>
+		<a class='line' href='#'></a>
+		<a class='line' href='#'></a>
+		<div class="line1"></div>
+		<div class="line2"></div>
+	
+	</div>
+	<div class='clear'></div>
+</div>
