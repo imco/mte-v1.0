e93506c45f67173e1b6cb8613feaf4b9539f4129
Paco Mekler <francisco.mekler@imco.org.mx>
2013-06-04 23:56:12 +0000

    cambio en texto en home page incluyento texto arriba de newsletter, correcion de tipos y comente la seccion de notas
    Former-commit-id: 28aca94ba878f1e4fa6b96fb985e43691e0c6e51
    Former-commit-id: 964af5d267a489fbfd70dc9cf58200c3f665d82a
    Former-commit-id: dc791e4f8b2d7de8614022e7475a1a23dbad8ea8
    Former-commit-id: b1a7d30fbb7e244b23ec79e426c33edb1e010a7a

--- templates/mtev1/global/footer.php
+++ templates/mtev1/global/footer.php
@@ -2,6 +2,6 @@
 	<a href='/metodologia'>Metodología</a> |
 	<!--<a href=''>¿Quienes somos?</a> |-->
 	<a href=''>Aviso Legal y de Privacidad</a> |
-	<a href='/contacto'>Contactanos</a>
+	<a href='/contacto'>Contáctanos</a>
 </p>
 <p class='sole'>Todos los derechos reservados. | Diseño y Desarollo web por <a href='http://spaceshiplabs.com'>Spaceshiplabs.</a></p>
