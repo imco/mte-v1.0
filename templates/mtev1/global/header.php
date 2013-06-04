cebb823f93a241b27c947b271c1186a85c449cc2
Paco Mekler <francisco.mekler@imco.org.mx>
2013-06-04 23:28:11 +0000

    cambio en reporte ciudadano. falta manejo de url
    Former-commit-id: d2f57e8360607182af1c61b72a507ddf514e7191
    Former-commit-id: 724e879108be8c285e24d54d850f47796974ca65
    Former-commit-id: f8c0d4644971dd6e25569a308cd504e2ace2a094
    Former-commit-id: ea319e9698a3cbcbe663baf75e59a2714af9ff07

--- templates/mtev1/global/header.php
+++ templates/mtev1/global/header.php
@@ -1,7 +1,7 @@
 <div class='menu <?= $this->location?>'><div class='container'>
 	<a href='/' class='logo'><?php $this->print_img_tag('home/logo.png'); ?></a>	
 	<a href='/compara'>Comparador</a>
-	<a href='/reportes-ciudadanos'>Reportes Ciudadanos</a>
+	<a href='/califica-tu-escuela'>Califica tu escuela</a>
 	<a href='/datos-abiertos'>Datos Abiertos</a>
 	<a href='/ayuda'>Ayuda</a>
 	<form method='get' action='/compara/#resultados' accept-charset='utf-8' ><input type='text' name='term' placeholder='Buscar' /><input type='hidden' name='search' value='true' /></form>
