b052983f5f562ec0b93b76dc83a442d5064132b7
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 11:30:06 -0500

    pre peticiones
    Former-commit-id: 74bba8bf848e7d7386aa58e2c829eea66acfd6ac
    Former-commit-id: 37f49fbf8c0631fb058e6ee87209bcd546e5191f
    Former-commit-id: 18435cc63a7fa57cd03ddb3f2f5dd58b829e8ab7
    Former-commit-id: 3bebcad73463e1ed61c34f69fe3264059a7d4ee1

---
+++ controlers/controler.califica_tu_escuela.php
@@ -0,0 +1,16 @@
+<?php
+class califica_tu_escuela extends main{
+	public function index(){		
+		$this->header_folder ='califica_tu_escuela';
+		$this->load_calificaciones();
+		$this->include_theme('index','index');
+
+	}
+	private function load_calificaciones(){
+		$q = new calificacion();
+		$q->search_clause = '1';
+		$q->order_by = 'califica_tu_escuela.likes DESC';
+		$this->reportes = $q->read('cct=>cct,cct=>nombre,cct=>entidad=>id,nombre_input,ocupacion,denuncia,likes,publicar,id');
+	}
+}
+?>
