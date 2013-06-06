ff9f26dad1c1d26d3538cc46a6bdd0997b68cc10
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 15:11:08 -0500

    calificaciones
    Former-commit-id: f249dc049c09a835889dd64595c075d224d3b04f
    Former-commit-id: 43928bc890171dedf5c0bf1bb50ad50e90f4b069
    Former-commit-id: 16e50248f70c8cc1febad8313466e18f0590bfea
    Former-commit-id: ddb32b00b99dbaaff9eb547a22fe0c43c01d15f4

--- controlers/controler.peticiones.php
+++ controlers/controler.peticiones.php
@@ -15,21 +15,20 @@ class peticiones extends main{
 		$petition_url = $this->post('petition_url');
 		$petition_auth_key = '3d123d2998aa55899a372ac09aef99f166e74c854df7ec877497533ee996103b';
 
-		$names = explode(' ',$this->post('name'));
+		$names = explode(' ',$this->post('nombre'));
 		$name = $names[0];
 		unset($names[0]);
-		$hidden = $this->post('public') ? 'true' : 'false';
+		$hidden = $this->post('public') ? 'false' : 'true';
 		$last_name = isset($names[1]) ? implode(' ',$names) : '';
 
 		$parameters['source'] = 'www.mejoratuescuela.org/peticiones';
 		$parameters['email'] = $this->post('email');
 		$parameters['first_name'] = $name;
 		$parameters['last_name'] = $last_name;
-		$parameters['city'] = $this->post('city');
+		$parameters['city'] = $this->post('ciudad');
 		$parameters['postal_code'] = $this->post('cp');
 		$parameters['country_code'] = $this->post('pais');
 		$parameters['hidden'] = $hidden;
-		
 		$change = new ApiChange($this->config->change_api_key,$this->config->change_secret_token);
 		$result = $change->suma_firma_peticion($petition_url,$petition_auth_key,$parameters);
 		
