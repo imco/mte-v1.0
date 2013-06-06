0b09450201df1d784f7f9b4583075f49f4336d63
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 13:33:49 -0500

    peticiones
    Former-commit-id: ce2432fb55c1726f290e4d6d395062df0f5fc388
    Former-commit-id: cef1ee25ad8cbbde7b58ed320da51fe0a0b18e17
    Former-commit-id: 314e940ca432bbacd5d4956b2296dedf38285ed7
    Former-commit-id: 3ae085637e4bd5361eba10953916ac2e99995edf

--- templates/mtev1/global/footer.php
+++ templates/mtev1/global/footer.php
@@ -5,3 +5,12 @@
 	<a href='/contacto'>Contáctanos</a>
 </p>
 <p class='sole'>Todos los derechos reservados. | Diseño y Desarollo web por <a href='http://spaceshiplabs.com'>Spaceshiplabs.</a></p>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-4404650-7', 'mejoratuescuela.org');
+  ga('send', 'pageview');
+</script>
