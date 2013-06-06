5f2f9ed455ceabfcca936b052c7832986285b4ac
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 15:33:20 -0500

    ayuda
    Former-commit-id: 78e9408c5490a691e4309752c923deee74037fd1
    Former-commit-id: 4b52862e0cc19c993c70a128b281ace46172adc1
    Former-commit-id: 2301e34189e2f24e815531f5f5ac7ea59d2cb06f
    Former-commit-id: ee4630290ee071139a0ec76186e8c161021f4595

---
+++ templates/mtev1/ayuda/content.php
@@ -0,0 +1,57 @@
+METODOLOGÍA:
+<ul>
+	<li><a href='#'>¿Dónde obtiene su información Mejora tu escuela?</a></li>
+	<li><a href='#'>¿Cómo calculamos las calificaciones de matemáticas y español de una escuela?</a></li>
+	<li><a href='#'>¿Cómo se define el nivel de una escuela en el semáforo de calidad educativa?</a></li>
+</ul>
+ 
+1. ¿Dónde obtiene su información Mejora tu escuela?
+Mejora tu escuela usa información de tres distintas bases de datos de la Secretaría de Educación Pública.
+La Evaluación nacional del logro académico en centros escolares, conocida también como prueba ENLACE.    Disponible en: www.enlace.sep.gob.mx
+El Sistema nacional de información educativa o SNIE.  Disponible en: www.snie.sep.gob.mx
+El Registro nacional de alumnos, maestros y alumnos o RNAME.  Disponible en: www.rname.sep.gob.mx
+
+Mejora tu escuela también ha creado una base de datos adicional con información cualitativa de escuelas que ha obtenido por Internet y por encuestas enviadas a correos electrónicos de directores de escuelas en todo México.
+ 
+2. ¿Cómo calculamos las calificaciones de matemáticas y español de una escuela?
+La metodología para calcular las calificaciones de matemáticas y español de una escuela se basa, estrictamente en los resultados de la prueba ENLACE.
+
+Escuelas primarias:
+·  	Para calcular el resultado de matemáticas de una escuela primaria se lleva a cabo el siguiente promedio:
+ (Calificación de matemáticas de tercero de primaria) + (calificación de matemáticas de cuarto de primaria) + (calificación de matemáticas de quinto de primaria) + (calificación de matemáticas de sexto de primaria) / 4  = calificación global de matemáticas de la escuela.
+
+·  	Para calcular el resultado de español de una escuela primaria se lleva a cabo el siguiente promedio:
+(Calificación de español de tercero de primaria) + (calificación de español de cuarto de primaria) + (calificación de español de quinto de primaria) + (calificación de español de sexto de primaria) / 4  = calificación global de español de la escuela.
+
+·  	Para calcular el resultado global de la escuela primaria se lleva a cabo el siguiente promedio:
+(calificación global de matemáticas de la escuela) + (calificación global de español de la escuela) / 2 = calificación global de la escuela primaria
+ 
+Escuelas secundarias:
+·  	Para calcular el resultado de matemáticas de una escuela secundaria a se lleva a cabo el siguiente promedio:
+ (calificación de matemáticas de primero de secundaria) + (calificación de matemáticas de segundo de secundaria) + (calificación de matemáticas de tercero de secundaria) / 3 = calificación global de matemáticas de la escuela
+
+·  	Para calcular el resultado de español de una escuela primaria se lleva a cabo el siguiente promedio:
+(calificación de español de primero de secundaria) + (calificación de español de segundo de secundaria) + (calificación de tercero de secundaria) / 3 =  calificación global de español de la escuela
+
+·  	Para calcular el resultado global de la escuela primaria se lleva a cabo el siguiente promedio:
+(calificación global de matemáticas de la escuela) + (calificación global de español de la escuela) / 2 = calificación global de la escuela secundaria
+ 
+Bachilleratos:
+·  	Para calcular el resultado de matemáticas de una escuela de bachillerato o técnica profesional se toma el único resultado disponible:
+(calificación de matemáticas de tercero de bachillerato) = calificación global de matemáticas de la escuela.
+
+·  	Para calcular el resultado de español de una escuela de bachillerato o técnica profesional se toma el único resultado disponible:
+(calificación de español de tercero de bachillerato) = calificación de global de español de la escuela.
+
+·  	Para calcular el resultado global de la escuela de bachillerato o técnica profesional se lleva a cabo el siguiente promedio:
+ (calificación global de matemáticas de la escuela) + (calificación global de español de la escuela) / 2 = calificación global de la escuela de bachillerato
+ 
+
+3. ¿Cómo se define el nivel de una escuela en el semáforo de calidad educativa?
+Para definir la posición de las escuelas en el semáforo educativo graficamos los resultados globales de las escuelas por nivel educativo.   La distribución de los resultados se comporta muy similar a la normal, lo cual facilita la metodología.	El 10% más alto de las escuelas las identificamos como excelentes.  El 10% más bajo lo identificamos como reprobadas.
+El la siguiente gráfica se pueden ver graficadas las calificaciones globales de todas las primarias del país. 
+
+La distribución de la calificación global de las secundarias es: 
+
+Finalmente, la distribución de los bachilleratos del país es: 
+
