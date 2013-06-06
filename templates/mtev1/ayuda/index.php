512116cd40b4ae93585016a595b62e0bcbe4758d
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 15:33:20 -0500

    ayuda
    Former-commit-id: edd3df5cb47472af873e8e894c6f48eb6e301bd7
    Former-commit-id: 66428dd9c1f09e10af1a44afd35fc2a165abec47
    Former-commit-id: de11ed06bb4f2b9fb00f9db92bd7007355a245c4
    Former-commit-id: 61acc1ba9abb8461dcccbd75845fd8bb529ad23c

--- templates/mtev1/ayuda/index.php
+++ templates/mtev1/ayuda/index.php
@@ -1,50 +1,28 @@
 <div class='container ayuda'>
 	<div class='ayuda'>
-		<h2>Preguntas frecuentes</h2>
+		<h2>Metodología</h2>
 		<hr/>
+		<ol>
+			<li><a href='#pregunta1'>¿Dónde obtiene su información Mejora tu escuela?</a></li>
+			<li><a href='#pregunta2'>¿Cómo calculamos las calificaciones de matemáticas y español de una escuela?</a></li>
+			<li><a href='#pregunta3'>¿Cómo se define el nivel de una escuela en el semáforo de calidad educativa?</a></li>
+		</ol>
+		 
 		<div class="text-ayuda">
 			<div class="section-one">
-				<h3>Proin gravida nibh vel velit auctor aliquet?</h3>
-				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.</p>
-				<div class="img-example">
-					<?php $this->print_img_tag('schoolchildren.png');?>
-				</div>
-
-				<h3>Proin gravida nibh vel velit auctor aliquet?</h3>
-				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed malesuada malesuada nibh, non mollis leo ornare quis.</p>
+				<a name='pregunta1'></a>
+				<h3>¿Dónde obtiene su información Mejora tu escuela?</h3>
+				<p>Mejora tu escuela usa información de tres distintas bases de datos de la Secretaría de Educación Pública.</p>
+				<p>La Evaluación nacional del logro académico en centros escolares, conocida también como prueba ENLACE.    Disponible en: www.enlace.sep.gob.mx</p>
+				<p>El Sistema nacional de información educativa o SNIE.  Disponible en: www.snie.sep.gob.mx</p>
+				<p>El Registro nacional de alumnos, maestros y alumnos o RNAME.  Disponible en: www.rname.sep.gob.mx</p>
+				<p>Mejora tu escuela también ha creado una base de datos adicional con información cualitativa de escuelas que ha obtenido por Internet y por encuestas enviadas a correos electrónicos de directores de escuelas en todo México.</p>
 			</div>
 			<div class="section-two">
-				<h2>Pasos de ejemplo</h2>
-				<h3>Paso 1</h3>
-				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed malesuada.</p>
-				<div class="img-example">
-					<?php $this->print_img_tag('schoolchildren.png');?>
-				</div>
-				<div class="img-example">
-					<?php $this->print_img_tag('schoolchildren.png');?>
-				</div>
-			</div>
-				
-			<div class="section-two">
-				<h3>Paso 2</h3>
-				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed malesuada.</p>
-				<div class="img-example">
-					<?php $this->print_img_tag('schoolchildren.png');?>
-				</div>
-				<div class="img-example">
-					<?php $this->print_img_tag('schoolchildren.png');?>
-				</div>
-			</div>
-
-			<div class="section-two">
-				<h3>Paso 3</h3>
-				<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis.Sed malesuada.</p>
-				<div class="img-example">
-					<?php $this->print_img_tag('schoolchildren.png');?>
-				</div>
-				<div class="img-example">
-					<?php $this->print_img_tag('schoolchildren.png');?>
-				</div>
+				<a name='pregunta2'></a>
+				<h3>¿Cómo calculamos las calificaciones de matemáticas y español de una escuela?</h3>
+				<p>La metodología para calcular las calificaciones de matemáticas y español de una escuela se basa, estrictamente en los resultados de la prueba ENLACE.</p>
+				<p><b>Escuelas primarias:</b></p>
 			</div>
 			
 		</div>
