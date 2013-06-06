24cd01cd68405cf28c11520c2e436b936bbc849b
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 14:55:09 -0500

    firmar
    Former-commit-id: 35a63d9364d9428ddabcec6c643afd564f67b7af
    Former-commit-id: 24a43b9bd2192d41562597fb216cde2431bc571a
    Former-commit-id: 32ebe99ebd844dd2caf6ff62aeffe3f01475576b
    Former-commit-id: cc5717085ffdf588edf08eb78b9d7b66544e301a

--- controlers/controler.peticiones.php
+++ controlers/controler.peticiones.php
@@ -7,24 +7,33 @@ class peticiones extends main{
 	}
 	private function read_peticion(){
 		date_default_timezone_set('America/Mexico_City');
-		$petition_url = 'http://www.change.org/peticiones/autoridades-educativas-del-gobierno-del-estado-de-m%C3%A9xico-exigimos-saber-como-se-gastan-nuestras-cuotas-en-la-escuela-%C3%A1ngel-maria-garibay-kintana';
+		$this->petition_url = 'http://www.change.org/peticiones/autoridades-educativas-del-gobierno-del-estado-de-m%C3%A9xico-exigimos-saber-como-se-gastan-nuestras-cuotas-en-la-escuela-%C3%A1ngel-maria-garibay-kintana';
 		$change = new ApiChange($this->config->change_api_key,$this->config->change_secret_token);
-		$this->peticion = $change->regresa_info_peticion($petition_url);
-		//var_dump($this->peticion);
+		$this->peticion = $change->regresa_info_peticion($this->petition_url);
 	}
-	public function firmar(){		
-		$change = new ApiChange($this->config->change_api_key,$this->config->change_secret_token);
+	public function firmar(){
+		$petition_url = $this->post('petition_url');
+		$petition_auth_key = '3d123d2998aa55899a372ac09aef99f166e74c854df7ec877497533ee996103b';
+
+		$names = explode(' ',$this->post('name'));
+		$name = $names[0];
+		unset($names[0]);
+		$hidden = $this->post('public') ? 'true' : 'false';
+		$last_name = isset($names[1]) ? implode(' ',$names) : '';
 
 		$parameters['source'] = 'www.mejoratuescuela.org/peticiones';
 		$parameters['email'] = $this->post('email');
-		$parameters['first_name'] = $this->post('name');
-		$parameters['last_name'] = $this->post('name');
+		$parameters['first_name'] = $name;
+		$parameters['last_name'] = $last_name;
 		$parameters['city'] = $this->post('city');
 		$parameters['postal_code'] = $this->post('cp');
 		$parameters['country_code'] = $this->post('pais');
-
-		$petition_auth_key = '3d123d2998aa55899a372ac09aef99f166e74c854df7ec877497533ee996103b';
+		$parameters['hidden'] = $hidden;
 		
+		$change = new ApiChange($this->config->change_api_key,$this->config->change_secret_token);
+		$result = $change->suma_firma_peticion($petition_url,$petition_auth_key,$parameters);
+		
+		var_dump($result);		
 	}
 }
 ?>
