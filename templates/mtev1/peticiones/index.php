93eab7209968a8ddbcca091f7933ad298043c86b
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 17:55:14 -0500

    sign petition
    Former-commit-id: 63f7ba2988b6431192a38323215bbe1af0c0f287
    Former-commit-id: a2486412c83c60eba1251b0e6ca553412308d34f
    Former-commit-id: a1be3780ac8cd4c36dce127537bf4f1c05115649
    Former-commit-id: 9a02784525c6ac358e89be4c634fb5073c0240c3

--- templates/mtev1/peticiones/index.php
+++ templates/mtev1/peticiones/index.php
@@ -6,21 +6,32 @@
 		<p><a href='<?=$this->peticion['url'] ?>'>Leer más</a></p>
 
 	</div>
-	<form action='/peticiones/firmar' method='post' accept-charset='utf-8'>
-		<h2>Firma Aquí</h2>
-		<p><input type='text' name='nombre' placeholder='Nombre' /></p>	
-		<p><input type='text' name='email' placeholder='Email' /></p>
-		<p><select name='pais' class='custom-select' >
-			<option value=''>País</option>
-			<?php $this->include_template('countries','peticiones'); ?>
-		</select></p>
-		<p><input type='text' name='ciudad' placeholder='Ciudad' /></p>
-		<p><input type='text' name='cp' placeholder='Código Postal' /></p>
-		<p>
-			<input type='submit' value='Firmar' />
-			<input type='hidden' value='<?=$this->petition_url?>' name='petition_url' />
-		</p>
-		<p><input type='checkbox' name='public' checked='checked' />publicar mi firma</p>
+	<form action='/peticiones/firmar' method='post' class='petition-form' accept-charset='utf-8'>
+		<?php 
+		if(isset($this->sign_result) && $this->sign_result->result != 'failure'){
+			echo "<h2>Gracias por firmar la peticion</h2>";
+		}else{
+			if(isset($this->sign_result) && $this->sign_result->result == 'failure'){
+				echo "<h2>Error</h2>";
+				echo "<p>".$this->sign_result->messages[0].'</p>';
+			}else{
+				echo "<h2>Firma Aquí</h2>";
+			}
+		?>			
+			<p><input type='text' name='nombre' placeholder='Nombre' class='required' /></p>	
+			<p><input type='text' name='email' placeholder='Email'  class='required email' /></p>
+			<p><select name='pais' class='custom-select' class='required'  >
+				<option value=''>País</option>
+				<?php $this->include_template('countries','peticiones'); ?>
+			</select></p>
+			<p><input type='text' name='ciudad' placeholder='Ciudad'  class='required' /></p>
+			<p><input type='text' name='cp' placeholder='Código Postal' class='required'  /></p>
+			<p>
+				<input type='submit' value='Firmar' />
+				<input type='hidden' value='<?=$this->petition_url?>' name='petition_url' />
+			</p>
+			<p><input type='checkbox' name='public' checked='checked' />publicar mi firma</p>
+		<?php } ?>
 	</form>	
 	
 	<div class='firmas'>
