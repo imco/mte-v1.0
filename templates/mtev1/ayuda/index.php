140d23346bfe9de2a0db7425230f46a6264d6a22
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 18:45:01 -0500

    ayuda
    Former-commit-id: 93a7ccd2ad67c670fe545ba7512093ae0b43b442
    Former-commit-id: cf3fad962b0a47acce4d8ab552905886e1edd761
    Former-commit-id: 1fca64a9da4d3b6cd736cc5e4f3e95ed6708c4b5
    Former-commit-id: 50eaa51b47c3366dccf2ef5dceac7e3aa98847bd

---
+++ templates/mtev1/ayuda/index.php
@@ -0,0 +1,45 @@
+<div class='container ayuda'>
+	<div class='ayuda'>
+		<h2>Ayuda</h2>
+		<hr/>
+		<ol>
+			<li><a href='#pregunta1'>¿Qué quiere decir el triángulo negro junto al nombre de mi escuela?</a></li>
+			<li><a href='#pregunta2'>¿Qué quiere decir el triángulo gris junto al nombre de mi escuela?</a></li>
+			<li><a href='#pregunta3'>¿Qué quiere decir el símbolo de precaución junto al nombre de mi escuela?</a></li>
+		</ol>
+		 
+		<div class="text-ayuda">
+			<div class="section-one">
+				<a name='pregunta1'></a>
+				<h3>¿Qué quiere decir el triángulo negro junto al nombre de mi escuela?</h3>
+				<p>El triángulo negro junto al nombre de una escuela indica que la escuela no administra la prueba ENLACE a sus alumnos.   Por lo tanto, no tenemos manera de obtener información para definir su calidad educativa.</p>
+			</div>
+			<div class="section-two">
+				<a name='pregunta2'></a>
+				<h3>¿Qué quiere decir el triángulo gris junto al nombre de mi escuela?</h3>
+				<p>El triángulo gris indica que no todos los grados de la escuela toman la prueba ENLACE.   Para los años que no tenemos resultado se asigna un cero, por lo tanto, el promedio de la escuela tiende a ser muy bajo.</p>
+				<p>Esta situación se puede dar en dos ocasiones:</p>
+				<ul>
+					<li>La escuela toma la decisión de que uno de sus grados no tome la prueba ENLACE.</li>
+					<li>La escuela es nueva y aún no tiene estudiantes en todos los grados.</li>
+				</ul>
+				<p>Para conocer los resultados por grado de la escuela por favor visita la sección de “Resultados educativos” en el perfil de la escuela.</p>
+
+				
+			</div>
+			<div class="section-one">
+				<a name='pregunta3'></a>
+				<h3>¿Qué quiere decir el símbolo de precaución junto al nombre de mi escuela?</h3>
+				<p>La prueba ENLACE utiliza tecnologías avanzadas para garantizar el control de calidad de los resultados individuales y por grupo.</p>
+ 				<p>ENLACE identifica como “copia u otras acciones inaceptables” a cualquier tendencia que indique la existencia de copia, dictado de respuestas y otras prácticas fraudulentas. </p>
+				<p>El símbolo de precaución junto al nombre de una escuela indica uno de dos posibles escenarios:</p>
+				<ul>
+					<li>Más de 10% de las pruebas de administradas en la escuela se consideran “no confiables” o “fraudulentas” por la SEP.</li>
+					<li>Menos de 85% de los alumnos tomaron la prueba.   Esta estadística potencialmente sugiere que la escuela sólo permite que sus mejores alumnos tomen la prueba.    Este porcentaje se calcula de la siguiente manera:  (número de alumnos que tomaron la prueba) / (número de niños inscritos en la escuela) x 100=  porcentaje de alumnos que toman la prueba.</li>
+				</ul>
+			</div>
+			
+		</div>
+	</div>
+	<div class='clear'></div>
+</div>
