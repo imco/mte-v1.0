659022bfa49abe21677a5cc3f47e41b469874251
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 13:33:49 -0500

    peticiones
    Former-commit-id: e0062ec085de63578862faf98974c48f44325d81
    Former-commit-id: 48b7cf47aebb062206837fcd4ff751d5b72b9b15
    Former-commit-id: 710c6460f215906839c96a73020612c5a4f43ea2
    Former-commit-id: f8a0754c465c2cbdccbf89aa0af3638322d736aa

--- templates/mtev1/peticiones/header.php
+++ templates/mtev1/peticiones/header.php
@@ -1,13 +1,30 @@
-<div class='container reportes-ciudadanos datos-abiertos'>
-	<div class='box'>
-		<h1><span></span>Datos Abiertos</h1>
-		<div class='content'>
-			<p>Sed et mollis lorem. Integer elit dolor, vehicula a lacinia euismod, ultricies vitae ante. Donec tincidunt, tellus ac consequat vulputate, orci metus accumsan dui, ac tristique sem elit id felis. Quisque in volutpat felis. Mauris placerat nunc vel nisi egestas congue vel vel elit. Cras egestas convallis justo non pellentesque. Ut quis tortor ut tellus pretium pulvinar quis eu neque. Aenean suscipit viverra dignissim. Sed malesuada malesuada nibh, non mollis leo ornare quis. </p>
-		</div>
-		<div class='search'>
-			<p class='adv-search'><a href='/compara/' >Busca tu escuela</a></p>
-		</div>
-		<div class='clear'></div>
-	</div>
+<div class='container peticion'>
+	<h1>Nombre de la Peticion</h1>
+	<div class='content'>
+		<h2>This is Photosop's Version of Lorem Ipsum.</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum dignissim egestas. Etiam sagittis diam id molestie euismod. Cras nec imperdiet ipsum. Curabitur elementum ullamcorper viverra. Donec eget vestibulum augue. Cras luctus purus ultricies arcu scelerisque, quis scelerisque turpis volutpat. Nullam id lorem in est consequat interdum et in dolor. Curabitur volutpat, sapien quis dictum porta, tortor lorem convallis lorem, sed iaculis purus tellus in lorem. Curabitur faucibus ullamcorper justo sed sodales. Duis et rutrum tellus, eget faucibus mauris. . Duis et rutrum tellus, eget faucibus mauris . Duis et rutrum tellus, eget faucibus mauris</p>
+		<p><a href=''>Leer más</a></p>
 
+	</div>
+	<form action='/peticiones/firmar' method='post' accept-charset='utf-8'>
+		<h2>Firma Aquí</h2>
+		<p><input type='text' name='email' placeholder='Email' /></p>
+		<p><input type='text' name='nombre' placeholder='Nombre' /></p>
+		<p><select name='pais' class='custom-select' >
+			<option value=''>País</option>
+			<?php $this->include_template('countries','peticiones'); ?>
+		</select></p>
+		<p><input type='text' name='ciudad' placeholder='ciudad' /></p>
+		<p><input type='text' name='pais' placeholder='pais' /></p>
+		<p><input type='submit' value='Firmar' /></p>
+
+
+	</form>	
+	<div class='shadow'></div>
+	<div class='firmas'>
+		<div class='img'></div>
+		<h3>Gracias a ti ya somos</h3>
+		<h2>23,997</h2>
+	</div>
+	<div class='clear'></div>
 </div>
