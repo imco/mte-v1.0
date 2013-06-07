13e7d68bf3a6c145193b4cac005395c0e4513a0c
El Sonny <sonny@spaceshiplabs.com>
2013-06-06 19:05:16 -0500

    aviso de privacidad
    Former-commit-id: f3ec8841f352ab7049f87f8bb2916a81fd6941ee
    Former-commit-id: b47a339810e239d676c860a5d9fd31c730a4f27b
    Former-commit-id: 994ef369277786d3d0dbe7dcd84186d22d5d95b0
    Former-commit-id: 2c921e0b3cf40bb286ca7f45598f3d11de0898c4

--- templates/mtev1/global/footer.php
+++ templates/mtev1/global/footer.php
@@ -1,7 +1,7 @@
 <p class='menu'>
 	<a href='/metodologia'>Metodología</a> |
 	<!--<a href=''>¿Quienes somos?</a> |-->
-	<a href=''>Aviso Legal y de Privacidad</a> |
+	<a href='/aviso-de-privacidad'>Aviso Legal y de Privacidad</a> |
 	<a href='/contacto'>Contáctanos</a>
 </p>
 <p class='sole'>Todos los derechos reservados. | Diseño y Desarollo web por <a href='http://spaceshiplabs.com'>Spaceshiplabs.</a></p>
