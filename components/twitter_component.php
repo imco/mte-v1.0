d425d9c90b0e45f4856ac64733e693e7978a78b7
urielaero <aero.uriel@gmail.com>
2013-06-17 23:58:16 -0500

    Twitter component
    Former-commit-id: 9e7f0e0e96ce6162a4c3ecfab5d03f00792d0b00
    Former-commit-id: 2bcffcb8443c0731a9b2ac77caa25fbb817aad6c
    Former-commit-id: 5783497108d0b950c2bb14b669eca66451f91032
    Former-commit-id: 284b9c98011ea880d9c122fe66c4d97e5a5d11f5

---
+++ components/twitter_component.php
@@ -0,0 +1,83 @@
+<?php
+class twitter_component extends component{
+
+	public function init($params){
+		// The tokens, keys and secrets from the app you created at https://dev.twitter.com/apps
+		$this->config_tweet = $params;
+		$this->whitelist = array('statuses/user_timeline.json?screen_name=MikeRogers0&count=10&include_rts=false&exclude_replies=true'=>true);
+	}
+
+	public function twitterToken($url){
+		if(!isset($url)){
+			die('No URL set');
+		}
+		if($this->config_tweet['use_whitelist'] && !isset($this->whitelist[$url])){
+			die('URL is not authorised');
+		}
+		$url_parts = parse_url($url);
+		parse_str($url_parts['query'], $url_arguments);
+		$full_url = $this->config_tweet['base_url'].$url; // Url with the query on it.
+		$base_url = $this->config_tweet['base_url'].$url_parts['path']; // Url without the query.
+		// Set up the oauth Authorization array
+		$oauth = array(
+			'oauth_consumer_key' => $this->config_tweet['consumer_key'],
+			'oauth_nonce' => time(),
+			'oauth_signature_method' => 'HMAC-SHA1',
+			'oauth_token' => $this->config_tweet['oauth_access_token'],
+			'oauth_timestamp' => time(),
+			'oauth_version' => '1.0'
+		);
+
+		$base_info = $this->buildBaseString($base_url, 'GET', array_merge($oauth, $url_arguments));
+		$composite_key = rawurlencode($this->config_tweet['consumer_secret']) . '&' . rawurlencode($this->config_tweet['oauth_access_token_secret']);
+		$oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+		$oauth['oauth_signature'] = $oauth_signature;
+
+// Make Requests
+		$header = array(
+			$this->buildAuthorizationHeader($oauth), 
+			'Expect:'
+		);
+		$options = array(
+			CURLOPT_HTTPHEADER => $header,
+			//CURLOPT_POSTFIELDS => $postfields,
+			CURLOPT_HEADER => false,
+			CURLOPT_URL => $full_url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => false
+		);
+
+		$feed = curl_init();
+		curl_setopt_array($feed, $options);
+		$result = curl_exec($feed);
+		$info = curl_getinfo($feed);
+		curl_close($feed);
+
+		// Send suitable headers to the end user.
+		if(isset($info['content_type']) && isset($info['size_download'])){
+			header('Content-Type: '.$info['content_type']);
+			header('Content-Length: '.$info['size_download']);
+		}
+
+		echo($result);
+	}
+
+	function buildBaseString($baseURI, $method, $params) {
+		$r = array();
+		ksort($params);
+		foreach($params as $key=>$value){
+		$r[] = "$key=" . rawurlencode($value);
+		}
+		return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+	}
+
+	function buildAuthorizationHeader($oauth) {
+		$r = 'Authorization: OAuth ';
+		$values = array();
+		foreach($oauth as $key=>$value)
+		$values[] = "$key=\"" . rawurlencode($value) . "\"";
+		$r .= implode(', ', $values);
+		return $r;
+	}
+}
+?>
