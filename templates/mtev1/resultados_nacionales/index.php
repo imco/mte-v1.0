6dd97cbc46c0e99c36f6d81c79f102929b948cf2
El Sonny <sonny@spaceshiplabs.com>
2013-06-26 16:02:50 -0500

    resultados nacionales index
    Former-commit-id: 6dec22890bc61fa6425baf23fa1211be57c5e988
    Former-commit-id: 1b357f9b3603228092d2e27472c9942ceccf8615
    Former-commit-id: acc2e9ed83ee30ec86c2c15129811739baaa95b6
    Former-commit-id: 78375572316d79a4bc170eefc6222d624371e9b2

--- templates/mtev1/resultados_nacionales/index.php
+++ templates/mtev1/resultados_nacionales/index.php
@@ -1,11 +1,11 @@
 <div class='container resultados-nacionales'>
-	<h1 class='full-blue'>Resultados Nacionales por Estado</h1>
+	<h1 class='full-blue'>Resultados Nacionales Por Estado</h1>
 	<?php
 	foreach($this->entidades as $entidad){
 		echo "<a href='/resultados-nacionales/entidad/{$entidad->id}' class='state-box'>";
 		$this->print_img_tag('entidades/'.$entidad->id.'.jpg');
-		echo "<span class='h2'>{$entidad->nombre}</span><span class='hover'></span></a>";
+		echo "<span class='h2'>{$entidad->nombre}</span><span class='hover'>Ver Resultados</span></a>";
 	}
 	?>
-
+	<div class='clear'></div>
 </div>
