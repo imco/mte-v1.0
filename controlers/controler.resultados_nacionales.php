a0d83125506d8f1df441b4fdcf49648dc58a1ed0
El Sonny <sonny@spaceshiplabs.com>
2013-06-26 16:02:50 -0500

    resultados nacionales index
    Former-commit-id: 032c07209da63a1457f8f9173d3656570ad1fb4b
    Former-commit-id: 22999d250bc5c32b9efdfcd78e17a6ffb3bc4767
    Former-commit-id: 951896bcff0ea4cdff193d5d39270446fbd96206
    Former-commit-id: 91d9efcb800fb30029b0b11762b5b46b2e6425a6

--- controlers/controler.resultados_nacionales.php
+++ controlers/controler.resultados_nacionales.php
@@ -5,5 +5,16 @@ class resultados_nacionales extends main{
 		$this->load_entidades();
 		$this->include_theme('index','index');
 	}
+	public function entidad(){
+		if($this->get('id')){
+			$this->header_folder ='escuelas';
+			$this->load_entidades();
+			$this->entidad = new entidad($this->get('id'));
+			$this->entidad->read('id,nombre,cct_count');
+			$this->include_theme('index','entidad');
+		}else{
+			$this->index();
+		}
+	}
 }
 ?>
