4549e542de0507569648bf4adf08e2567275658d
El Sonny <sonny@spaceshiplabs.com>
2013-06-27 07:51:31 -0500

    resultados nacionales indv
    Former-commit-id: 3bfb1f6ecab11682540adac7958953f6da9a9b44
    Former-commit-id: 9df03ee05685a337f03755179731854a62b48a63
    Former-commit-id: 911df59d1a12e7658470baa4e6b84c5804b8e047
    Former-commit-id: 19fcb5ba910f57e953a3bacbd6b6ccc46d72b730

--- templates/mtev1/resultados_nacionales/entidad.php
+++ templates/mtev1/resultados_nacionales/entidad.php
@@ -1,5 +1,25 @@
 <div class='container resultados-entidad'>
-	<h1 class='full-blue'><?=$this->entidad->nombre?><a href='/resultados-nacionales'></a></h1>
+	<h1 class='full-blue'><?=$this->entidad->nombre?><a href='/resultados-nacionales'>Ver todos los estados</a></h1>
 	
-	<div class='clear'></div>
+	<div class='content'>
+		<h2 class='promedio-gen'>Promedio General <span>87.8</span></h2>
+		<div class='column'>
+			<p class='promedio-green'>
+				Escuelas totales
+				<span class='value'>6790</span>
+			</p>
+			<p class='promedio-green'>
+				Escuelas evaluadas
+				<span class='value'>6790</span>
+			</p>
+			<div class='clear'></div>
+			<table>
+				<tr>
+					<th></th>
+					<th class='grey'></th>
+				</tr>
+			</table>
+		</div>
+		<div class='clear'></div>
+	</div>
 </div>
