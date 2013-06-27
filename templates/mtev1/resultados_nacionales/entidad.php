b7a18c13f3aa367162ae0e6203f74e6c9da6dcbd
El Sonny <sonny@spaceshiplabs.com>
2013-06-27 09:25:20 -0500

    resultados nacionales
    Former-commit-id: 33baad058ef30c7965429b5ea5f52da048b7e374
    Former-commit-id: 33adb08ffc2b2b19173005bd55df37bfdf845ddb
    Former-commit-id: f249be85d546e14fd33066eaa925e2ffab47ea69
    Former-commit-id: f8ad7ac8b9d39e3f11a6b86ef7883b6f96133dc5

--- templates/mtev1/resultados_nacionales/entidad.php
+++ templates/mtev1/resultados_nacionales/entidad.php
@@ -1,5 +1,5 @@
 <div class='container resultados-entidad'>
-	<h1 class='full-blue'><?=$this->entidad->nombre?><a href='/resultados-nacionales'>Ver todos los estados</a></h1>
+	<h1 class='full-blue'><?=$this->capitalize($this->entidad->nombre)?><a href='/resultados-nacionales'>Ver todos los estados</a></h1>
 	
 	<div class='content'>
 		<h2 class='promedio-gen'>Promedio General <span>87.8</span></h2>
@@ -13,13 +13,42 @@
 				<span class='value'>6790</span>
 			</p>
 			<div class='clear'></div>
-			<table>
-				<tr>
-					<th></th>
-					<th class='grey'></th>
-				</tr>
-			</table>
+			<p class='promedio-orange'>
+				Numero Escuelas Publicas
+				<span class='value'>790</span>
+			</p>
+			<div class='table'>
+				<p>Promedio Español <span class='value'>87.6</span></p>
+				<p>Promedio Matemáticas <span class='value'>87.6</span></p>
+			</div>
+		</div>
+		<div class='column'>
+			<p class='promedio-green blue'>
+				Escuelas totales
+				<span class='value'>6790</span>
+			</p>
+			<p class='promedio-green blue'>
+				Escuelas evaluadas
+				<span class='value'>6790</span>
+			</p>
+			<div class='clear'></div>
+			<p class='promedio-orange'>
+				Numero Escuelas Privadas
+				<span class='value'>790</span>
+			</p>
+			<div class='table'>
+				<p>Promedio Español <span class='value'>87.6</span></p>
+				<p>Promedio Matemáticas <span class='value'>87.6</span></p>
+			</div>
 		</div>
 		<div class='clear'></div>
+		<a class='graph-button' href='#'>Titulo Gráfica1</a>
+		<div class='graph on'></div>
+
+		<a class='graph-button' href='#'>Titulo Gráfica1</a>
+		<div class='graph'></div>
+
+		<a class='graph-button' href='#'>Titulo Gráfica1</a>
+		<div class='graph'></div>
 	</div>
 </div>
