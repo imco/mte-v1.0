67f0d77101ac804c26b4744f67ec2b5fc3da0307
El Sonny <sonny@spaceshiplabs.com>
2013-06-27 15:44:45 -0500

    api
    Former-commit-id: 11d67798333dd647ac2482071d342b95631e5102
    Former-commit-id: db079f67cbc37fc6235a498a8526d42ae5103c39
    Former-commit-id: 53d59f41851d682293357d980f9b6e407757e1f2
    Former-commit-id: 2e782b6d1d201cbb6e963d66cea79ceab548c745

--- controlers/controler.api.php
+++ controlers/controler.api.php
@@ -1,11 +1,34 @@
 <?php
 class api extends main{
 	public function escuelas(){
-		$params->limit = "0 ,100";
+		$params->limit = "0 ,2000";
 		$this->get_escuelas($params);
 		$this->process_escuelas();
 		echo json_encode($this->escuelas_digest->escuelas);
 	}
+	public function municipios(){
+		$this->load_municipios();
+		$digest = array();
+		$i = 0;
+		foreach($this->municipios as $municipio){
+			$digest[$i]->id = $municipio->id;
+			$digest[$i]->nombre = $municipio->nombre;
+			$digest[$i]->entidad->nombre = $municipio->entidad->nombre;
+			$digest[$i++]->entidad->id =  $municipio->entidad->id;
+		}
+		echo json_encode($digest);
+	}
+	public function entidades(){
+		$this->load_entidades();
+		$digest = array();
+		$i = 0;
+		foreach($this->entidades as $entidad){
+			$digest[$i]->id = $entidad->id;
+			$digest[$i]->nombre = $entidad->nombre;
+			$digest[$i++]->ccts = $entidad->cct_count;
 
+		}
+		echo json_encode($digest);
+	}
 }
 ?>
