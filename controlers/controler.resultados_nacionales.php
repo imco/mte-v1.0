11400789a802456683f875bffa22c8ed396a564d
El Sonny <sonny@spaceshiplabs.com>
2013-06-27 12:16:32 -0500

    histograms complete
    Former-commit-id: 97c0eff7c12605f659dff08aff3ee6ac3c22324f
    Former-commit-id: 1fd9777cce736a45ed315bbe37b00a790a093b8c
    Former-commit-id: 4093703b8e325479983bd500575b5e7fb344ca6e
    Former-commit-id: d9387f9ce00a8584f94abd1be99f52cc00fa19de

--- controlers/controler.resultados_nacionales.php
+++ controlers/controler.resultados_nacionales.php
@@ -6,15 +6,53 @@ class resultados_nacionales extends main{
 		$this->include_theme('index','index');
 	}
 	public function entidad(){
+		$this->draw_charts = true;
+		
 		if($this->get('id')){
 			$this->header_folder ='escuelas';
 			$this->load_entidades();
 			$this->entidad = new entidad($this->get('id'));
-			$this->entidad->read('id,nombre,cct_count');
+			$this->entidad->read('id,nombre,cct_count,distribucion_primarias,distribucion_secundarias,distribucion_bachilleratos');
+			$this->initialize_histograms();
 			$this->include_theme('index','entidad');
 		}else{
 			$this->index();
 		}
+	}
+	private function initialize_histograms(){
+		//primarias
+		if($this->entidad->distribucion_primarias == ''){
+			$data = $this->histogram($this->entidad->id,12);
+			$this->entidad->update('distribucion_primarias',array(json_encode($data)));
+			$this->entidad->distribucion_primarias = $data;
+		}
+		//secundarias
+		if($this->entidad->distribucion_secundarias == ''){
+			$data = $this->histogram($this->entidad->id,13);
+			$this->entidad->update('distribucion_secundarias',array(json_encode($data)));
+			$this->entidad->distribucion_secundarias = $data;
+		}
+		//prepas
+		if($this->entidad->distribucion_bachilleratos == ''){
+			$data = $this->histogram($this->entidad->id,22);
+			$this->entidad->update('distribucion_bachilleratos',array(json_encode($data)));
+			$this->entidad->distribucion_bachilleratos = $data;
+		}
+	}
+	public function histogram($entidad,$nivel){
+		set_time_limit(100000);
+		$sql = "SELECT cct,nombre,promedio_general FROM escuelas WHERE nivel = '$nivel' AND entidad = '$entidad' AND promedio_general IS NOT NULL";//" OR nivel = '13' or nivel = '22' or nivel = '21'";
+		$result = mysql_query($sql);
+		$data = array();
+		for($i = 0;$i<901;$i++){
+			$data[$i][0] = $i;
+			$data[$i][1] = 0;
+		}
+		$i = 0;
+		while($row = mysql_fetch_assoc($result)){
+			$data[round($row['promedio_general'])][1]++;
+		}
+		return $data;	
 	}
 }
 ?>
