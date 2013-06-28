81cabb31f39b33cb84ac2ad4caece99d3f5e9a9e
urielaero <aero.uriel@gmail.com>
2013-06-27 23:46:29 -0500

    Breadcrumb V2
    Former-commit-id: 0d4f4b4812f57fed289363a3d7387ff9f6ad47fa
    Former-commit-id: d7e7e94c9ccd9dd2c1116124086ada2ef7345d60
    Former-commit-id: eb158dd63ae61d897b0fc2cdc99c49ab51ec12c0
    Former-commit-id: 3790593fd3f57a9088712fb88b09d48a454582fa

--- templates/mtev1/js/interactions.js
+++ templates/mtev1/js/interactions.js
@@ -120,18 +120,22 @@ $(document).ready(function(){
 			$(this).parent().find('.line2').css('display','none');	
 	});
 
-	$('#header .breadcrum li').click(function(){
-		console.log("adasd");
-	});
+	loadBreadcrumb();
+	setTimeout(loadBreadcrumb,1000);	
+});
+
+function loadBreadcrumb(){
 	var coutBre;
 	if((coutBre = $('#header .breadcrumb li')) .length){
-		var i;
+		var i,temp;
 		for(i=2;i<coutBre.length;i++){
-			$(coutBre[i]).css('left',$(coutBre[i-1]).position().left+($(coutBre[i]).width()-3));
+			//$(coutBre[i]).css('left',$(coutBre[i-1]).position().left+($(coutBre[i]).width()+50));
+			temp = $(coutBre[i-1]);
+			$(coutBre[i]).css('left',(temp.position().left+ temp.width()+15));
 		}
 	}
-	
-});
+}
+
 function load_location_options(input,directive,options,name){
 	input.prop('disabled', true);
 	input.next().addClass('customSelectDisabled');
