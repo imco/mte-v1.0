8443ae476c05ba9d40c8417896c9f04432761dd7
El Sonny <sonny@spaceshiplabs.com>
2013-06-28 08:56:59 -0500

    uriel fixes
    Former-commit-id: 6f20593800eb3d15690836bd5db993a66c5e8e7c
    Former-commit-id: df84aca8aa18a8c3ee8f36640bc2619e99cd0db6
    Former-commit-id: 6c11bd757ab476a6aebef3d2290a1644e402d501
    Former-commit-id: 0706791443ae6ee07fc18aa2591aeff4fa9ec7f5

--- controlers/controler.contacto.php
+++ controlers/controler.contacto.php
@@ -1,7 +1,8 @@
 <?php
 class contacto extends main{
 	public function index(){		
-		$this->header_folder ='contacto';
+		$this->header_folder ='contacto';		
+		$this->breadcrumb = array('#'=>'Contacto');
 		$this->include_theme('index','index');
 	}
 }
