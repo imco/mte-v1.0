7b10d6551f301e80deed6e69610b321af7b0ee51
urielaero <aero.uriel@gmail.com>
2013-06-28 20:37:26 -0500

    fixes avances
    Former-commit-id: 290aecbe412c89ff1cd924e927d20ea8b0ac2e1e
    Former-commit-id: 2543d6b292135b11459e67312bc49a7e5501adb7
    Former-commit-id: ac1010057e613365d5bc8b3ff6477463cc86e024
    Former-commit-id: 01274012e859bc80ac60376d340e28eb1e8a0dd6

--- templates/mtev1/contacto/header.php
+++ templates/mtev1/contacto/header.php
@@ -2,14 +2,17 @@
 	<div class='box'>
 		<h1><span></span>Contáctenos</h1>
 		<div class='content'>
-			<form action='' method='post'   >
-				<input name='' type='text' placeholder='Nombre' />
-				<input name='' type='text' placeholder='Correo Electrónico' />
-				<textarea name='' rows='7' cols='68'  placeholder='Mensaje' ></textarea>
+			<form action='/contacto/enviar/' method='post' id='contacto'  >
+				<input name='nombre' type='text' placeholder='Nombre' />
+				<input name='email' type='text' placeholder='Correo Electrónico' />
+				<textarea name='mensaje' rows='7' cols='68'  placeholder='Mensaje' ></textarea>
 				<input type="submit" value="Enviar" />
 			</form>
 		</div>	
 		<p><span class='icon'></span>contacto@mejoratuescuela.org</p>
+		<p class='tel'><span class='icon'></span>5985-1017</p>
+		<p class='tel'><span class='icon'></span>5985-1018</p>
+		<p class='tel'><span class='icon'></span>5985-1019</p>	
 		<div class='clear'></div>
 	</div>
 
