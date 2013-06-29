9a44c564db77c95957f7330aadf86ebf268d05ad
urielaero <aero.uriel@gmail.com>
2013-06-29 03:43:45 -0500

    contacto listo
    Former-commit-id: 5f2cbaab95a752f29d626d63d5ef5c11248daa9d
    Former-commit-id: 71c51ce00591a7f4c21e1ef0400bb618b4c0f88f
    Former-commit-id: 029b3946fb6b7fae17d3ef081cae2cd2bdb806b7
    Former-commit-id: 466468765f00d0614d6848550ff026e9d7a8c83c

--- templates/mtev1/contacto/header.php
+++ templates/mtev1/contacto/header.php
@@ -2,10 +2,10 @@
 	<div class='box'>
 		<h1><span></span>Contáctenos</h1>
 		<div class='content'>
-			<form action='/contacto/enviar/' method='post' id='contacto'  >
-				<input name='nombre' type='text' placeholder='Nombre' />
-				<input name='email' type='text' placeholder='Correo Electrónico' />
-				<textarea name='mensaje' rows='7' cols='68'  placeholder='Mensaje' ></textarea>
+			<form action='/contacto/enviar/' method='post' class = 'contacto-form'  >
+				<input name='nombre' type='text' placeholder='Nombre' class='required' />
+				<input name='email' type='text' placeholder='Correo Electrónico' class='required email'/>
+				<textarea name='mensaje' rows='7' cols='68'  placeholder='Mensaje' class='required' ></textarea>
 				<input type="submit" value="Enviar" />
 			</form>
 		</div>	
@@ -13,6 +13,11 @@
 		<p class='tel'><span class='icon'></span>5985-1017</p>
 		<p class='tel'><span class='icon'></span>5985-1018</p>
 		<p class='tel'><span class='icon'></span>5985-1019</p>	
+		<?php 	if(isset($this->contact_status) && $this->contact_status)
+				echo '<h3 class="msj" >Mensaje enviado</h3>';
+			else if(isset($this->contact_status) && !$this->contact_status)
+				echo '<h3 class="msj" >Error</h3>';
+		?>
 		<div class='clear'></div>
 	</div>
 
