305d7758d8d0c654dd3e149169123ea1356750fe
urielaero <aero.uriel@gmail.com>
2013-07-04 20:25:28 -0500

    share buttons
    Former-commit-id: 91e32888f8e927fcc386c0ebf40e35f4f22e119b
    Former-commit-id: 239128c2a9dded3d635040b4be40d9f899388002
    Former-commit-id: 3b4bf6fe02db81a424308d5f1a93efd4dcf95908
    Former-commit-id: 56be59069d01e37a751bda227325d43e9427fdae

--- templates/mtev1/compara/resultados.php
+++ templates/mtev1/compara/resultados.php
@@ -51,7 +51,7 @@
 				 return false;">
 				  	    Facebook
 				  </a>
-			  <a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-text="El perfil de <?=$this->escuela->nombre; ?> " data-via='***REMOVED***'>
+			  <a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-text="El perfil de <?=$this->escuela->nombre; ?> " data-via='mejoratuescuela'>
 			  	Tweet
 			  </a>
 			  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
