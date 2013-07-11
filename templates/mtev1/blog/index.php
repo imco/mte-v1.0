6d699d07c22ce357a8dec67f431446ce005de60f
urielaero <aero.uriel@gmail.com>
2013-07-10 20:24:52 -0500

    blog
    Former-commit-id: b1b78e6eec6e6d763137b820dfbb4c344591f615
    Former-commit-id: 58ca9561d66e14c9c0ca3cd2eb986803d89922e3
    Former-commit-id: 5792e6f22444e68030b8ee73b9001972f2a908a7
    Former-commit-id: 1dcf10d3ef6d541a663724241bc3b658db450b06

---
+++ templates/mtev1/blog/index.php
@@ -0,0 +1,54 @@
+<div class='blog container'>
+	<div class='column left'>
+	<?php for($p=0;$p<2;$p++) { ?>
+		<div class='post'>
+			<h1 class='title'>Titulo de la Nota para Blog</h1>
+			<h2 class='info'>Nombre del Autor | 
+				<span class="date">día. Mes. Año</span>
+			</h2>
+
+			<p>
+				<?php $this->print_img_tag('schoolchildren.png'); ?>
+				This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet,
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam molestie id magna eu eleifend. Donec nec ligula porta, interdum ante sit amet, ullamcorper odio. Sed lacinia porta mi, quis iaculis nibh eleifend et. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent rutrum magna quis consequat sodales. Praesent sit amet urna dignissim, dignissim enim at, imperdiet eros. Maecenas viverra arcu dapibus lectus fermentum tempus. Nulla sit amet sapien rutrum, consectetur felis id, sagittis nisl. Nullam quis nunc ultricies, eleifend tortor vitae, pharetra mauris. 			</p>
+			<div class="share-bt bl">
+				<div class="social">
+					<div class="btns">
+						<a href="#" class='share-face' 
+				 	 	onclick="
+						      window.open(
+				            		'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 
+					          	'El perfil', 
+						       	'width=626,height=436'); 
+							 return false;">
+						  </a>
+		
+						<div class="tweet">
+							<span class="twitter-icon"></span>
+							 <a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-text="Blog title" data-via='mejoratuescuela'>
+							  </a>
+						</div>
+						  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+					</div>
+				</div>
+				<a href="#" class="button-frame">
+					<span class="bt-share">
+						<?php $this->print_img_tag('compartir/compartir.png');?>
+						Compartir
+					</span>
+				</a>
+			</div>
+		<div class="clear"></div>
+		</div>
+		<?php } ?>
+	</div>
+	<div class='column right'>
+		<h1>NOTAS RECIENTES</h1>
+		<?php for($i=0;$i<10;$i++){ ?>
+		<p>
+			<a href="">Titulo de la Nota para Blog</a>
+		</p>
+		<?php } ?>
+	</div>
+	<div class="clear"></div>
+</div>
