6d318aa15e9e91e6ea2dd21cda09946c79be0afd
urielaero <aero.uriel@gmail.com>
2013-07-30 20:19:50 -0500

    menu
    Former-commit-id: dbe70bf720a19df57624c4d4c77882348679266a
    Former-commit-id: e3b48dee477b5efa5a8742794401d1c18ac86916
    Former-commit-id: 065f740faa1dcda636ac2676536411a98d423840
    Former-commit-id: 0a6e1cce84a9f980491406ef15ca871825b5379a

--- templates/mtev1/global/header.php
+++ templates/mtev1/global/header.php
@@ -1,16 +1,27 @@
 
 <div class='menu <?= $this->location?>'><div class='container'>
 	<a href='/' class='logo'><?php $this->print_img_tag('home/logo.png'); ?></a>	
-	<a href='/compara'>Comparador</a>
-	<!--<a href='/califica-tu-escuela'>Califica tu escuela</a>-->
+	<a href='/conoce'>CONOCE</a>
+	<a href='/compara'>COMPARA</a>
+	<a href='/califica-tu-escuela'>CALIFICA</a>
+	<a href='/mejora'>MEJORA</a>
+	<!--
 	<a href='/resultados-nacionales'>Resultados Nacionales</a>
 	<a href='/peticiones'>Peticiones</a>
-	<a href='/ayuda'>Ayuda</a>
-	<form method='get' action='/compara/#resultados' accept-charset='utf-8' ><input type='text' name='term' placeholder='Buscar' /><input type='hidden' name='search' value='true' /></form>
-	<div class='social'>
-		<a href='https://www.facebook.com/MejoraTuEscuela' class='fb'></a>
-		<a href='https://twitter.com/mejoratuescuela' class='twitter'></a>
+	-->
+	<div class='submenu'>
+		<div class='social'>
+			<a href='https://twitter.com/mejoratuescuela' class='twitter'></a>
+			<a href='https://www.facebook.com/MejoraTuEscuela' class='fb'></a>
+			<div class='clear'></div>
+		</div>
+		<form method='get' action='/compara/#resultados' accept-charset='utf-8' ><input type='text' name='term' placeholder='Buscar' /><input type='hidden' name='search' value='true' />
+			<input type='submit' value='' />
+		</form>
+		<a href='/quienes-somos'>¿Quiénes somos?</a>
+		<a href='/ayuda'>Ayuda</a>
 	</div>
+
 	<div class='clear'></div>
 </div></div>
 
@@ -25,7 +36,7 @@
 
 	<?php foreach($this->breadcrumb as $url => $breadcrumb){ ?>
 			<li>
-				<?if($url!='#') {?>
+				<?php if($url!='#') {?>
 					<a href="<?=$url ?>"><?=$breadcrumb ?></a>
 				<?php } else { ?>
 					<a class='current' href="<?=$url ?>"><?=$breadcrumb?></a>
