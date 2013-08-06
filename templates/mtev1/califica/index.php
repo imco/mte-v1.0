c94cf6ad577f5987c4a101c316cd7228df56df03
urielaero <aero.uriel@gmail.com>
2013-08-06 01:28:04 -0500

    avances...
    Former-commit-id: 7dce47aa23ede21b54afe6113dddf5be57eac795
    Former-commit-id: b83b8d1958e0b138d4fb38bef923f868ebed0aa4
    Former-commit-id: 9e736b6eceb9a55a010aabc4f801a1dbd0aa40b0
    Former-commit-id: 4a2b5185c98c4eedbd0c13f459bac146bb9df739

---
+++ templates/mtev1/califica/index.php
@@ -0,0 +1,102 @@
+<div class='container califica'>
+	<h2>Califica tu escuela seleccionando para cada campo una calificación de 1-10.
+	<br />
+	Estas calificaciones se promedian para generar la calificacion general de tu escuela.
+	</h2>
+
+	<div class='calificacion'>
+		<h2>Preparación de los maestros</h2>
+		<p>¿Qué tan preparados y capacitados estan los maestros 
+		<br />
+		de tu escuela?
+		</p>
+		<div class='wrap_cal'>
+			<span>1</span>
+			<span>2</span>
+			<span>3</span>
+			<span>4</span>
+			<span>5</span>
+			<span>6</span>
+			<span>7</span>
+			<span>8</span>
+			<span>9</span>
+			<span>10</span>
+		</div>
+		<a href='#' class='button-frame'>
+			<span class='button'>Califica</span>
+		</a>
+		<div class='clear'></div>
+	</div>
+	<div class='calificacion'>
+		<h2>Asistencia de los maestros</h2>
+		<p>¿Los maestros faltan a clases constantemente o siem- 
+		<br />
+		pre estan en el aula?
+		</p>
+		<div class='wrap_cal'>
+			<span>1</span>
+			<span>2</span>
+			<span>3</span>
+			<span>4</span>
+			<span>5</span>
+			<span>6</span>
+			<span>7</span>
+			<span>8</span>
+			<span>9</span>
+			<span>10</span>
+		</div>
+		<a href='#' class='button-frame'>
+			<span class='button'>Califica</span>
+		</a>
+	</div>
+
+	<div class='calificacion'>
+		<h2>Preparación de los maestros</h2>
+		<p>¿Qué tan preparados y capacitados estan los maestros 
+		<br />
+		de tu escuela?
+		</p>
+		<div class='wrap_cal'>
+			<span>1</span>
+			<span>2</span>
+			<span>3</span>
+			<span>4</span>
+			<span>5</span>
+			<span>6</span>
+			<span>7</span>
+			<span>8</span>
+			<span>9</span>
+			<span>10</span>
+		</div>
+		<a href='#' class='button-frame'>
+			<span class='button'>Califica</span>
+		</a>
+	</div>
+	<div class='calificacion'>
+		<h2>Asistencia de los maestros</h2>
+		<p>¿Los maestros faltan a clases constantemente o siem- 
+		<br />
+		pre estan en el aula?
+		</p>
+		<div class='wrap_cal'>
+			<span>1</span>
+			<span>2</span>
+			<span>3</span>
+			<span>4</span>
+			<span>5</span>
+			<span>6</span>
+			<span>7</span>
+			<span>8</span>
+			<span>9</span>
+			<span>10</span>
+		</div>
+		<a href='#' class='button-frame'>
+			<span class='button'>Califica</span>
+		</a>
+	</div>
+
+
+
+
+	<div class='clear'></div>
+</div>
