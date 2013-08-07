861a18eeb4ba494b298ad21d5f3f9ad65ec8e4c2
urielaero <aero.uriel@gmail.com>
2013-08-07 17:23:23 -0500

    mtv4
    Former-commit-id: 29a6329c5c873930fd24cf801e8a949a6d9f94ce
    Former-commit-id: 0d267b5783d92d10552a833dddf4fd64871ca129
    Former-commit-id: 26fddaa16cfd2e02ec30610d6d1b2236c01164b8
    Former-commit-id: 4a45dceda69932b43cdfff4db9ffbaa22c58ff0c

--- controlers/controler.quienes_somos.php
+++ controlers/controler.quienes_somos.php
@@ -3,6 +3,7 @@ class quienes_somos extends main{
 	public function index(){
 		$this->title_header = '¿QUIÉNES SOMOS?';
 		$this->header_folder = 'compara';
+		$this->breadcrumb = array('#'=>'¿Quiénes somos?');
 		$this->include_theme('index','index');
 	}
 }
