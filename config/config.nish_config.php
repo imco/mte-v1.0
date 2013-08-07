9b5cbe01d5e06b6af9748ce8a97c70639e65fad3
Nish <nmukh1@gmail.com>
2013-08-07 18:12:57 -0400

    Linux settings for Nish
    Former-commit-id: 045e686662dc9431c44f62f117f9d9ab7e6e5319
    Former-commit-id: a8b0fab953d32bd44c6c5e5894c040a256d16159
    Former-commit-id: 4ef4c4828c362883df35613b8d38f35f0d9fc9cd
    Former-commit-id: 195331e9d3b8c0a629513750ab5bdf7d292e277b

--- config/config.nish_config.php
+++ config/config.nish_config.php
@@ -4,13 +4,13 @@ class nish_config extends default_config{
 		parent::__construct();
 		//Site
 		$this->http_address = 'http://comparatuescuela/';
-		$this->mxnphp_dir = "/home/nish/webdev/mxnphp/";
+		$this->mxnphp_dir = "/home/nish/Development/framework";
 		
 		//Database
 		$this->db_host = '***REMOVED***';
 		$this->db_name = 'comparatuescuela';
 		$this->db_user = 'root';
-		$this->db_pass = 'qwer1985*';
+		$this->db_pass = '';
 		
 		//MXNPHP
 		$this->dev_mode = true;
