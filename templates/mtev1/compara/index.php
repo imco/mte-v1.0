757a81df46cdb20c2883a9213ee3e2bdcc8ab522
Paco Mekler <oscar.mekler@gmail.com>
2013-08-09 18:44:39 +0000

    cambios en headers de seccion de compara
    Former-commit-id: d9482b7887f47b7a9cfa9e4180fc34ac093241be
    Former-commit-id: 171ca93a029db3f8d95246f8484610931c3efc50
    Former-commit-id: 26838fc279d3c7de3eae9d3e8f4beb3894428580
    Former-commit-id: 097211b7101907281ceb4e102e2b5d6f5bbe7a64

--- templates/mtev1/compara/index.php
+++ templates/mtev1/compara/index.php
@@ -1,9 +1,9 @@
 <div class=' container comparar resultados'>
 	<div class='compara-tabs'>
 		<a href='#' class='general on'><span></span>General</a>
-		<a href='#' class='posicion-nacional'><span></span>Posición Nacional</a>
+		<a href='#' class='posicion-nacional'><span></span>Posición nacional</a>
 		<a href='#' class='resultados-por-anio'><span></span>Resultados por año</a>
-		<a href='#' class='desempeno-de-alumnos'><span></span>Desempeño de Alumnos</a>
+		<a href='#' class='desempeno-de-alumnos'><span></span>Desempeño por alumno</a>
 		<a href='#' class='mapa' id='compare-map-tab'><span></span>Mapa</a>
 		<div class='clear'></div>
 		<div class='shadow'></div>
