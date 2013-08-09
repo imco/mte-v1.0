d4dbb09e0feacf57d9f58b933fea29519c68821a
urielaero <aero.uriel@gmail.com>
2013-08-09 02:54:12 -0500

    datos faltantes en mas escuelas en perfil
    Former-commit-id: e8909bf40cf701b3ec9010a19cb28e30f2592558
    Former-commit-id: f43bc0413b7f3979c1308c3e7854848db5531a6f
    Former-commit-id: 630ea5fbdd3227bd2e9dc6f18ad7015b4102b27b
    Former-commit-id: c7ed07fb3062061ef93571f6d14e461d199a2dd4

--- templates/mtev1/compara/resultados.php
+++ templates/mtev1/compara/resultados.php
@@ -16,12 +16,15 @@
 	<?php
 	if(isset($this->escuelas)){
 		foreach($this->escuelas as $escuela){
+			/*
 			$esc = new escuela();
 			$esc->poco_confiables = $escuela->poco_confiables;
 			$esc->total_evaluados = $escuela->total_evaluados;
 			$esc->promedio_general = $escuela->promedio_general;
 			$esc->nivel->id = $escuela->nivel;
 			$esc->get_semaforo();
+			*/
+			$escuela->get_semaforo();
 			$on = $this->compara_cookie && in_array($escuela->cct,$this->compara_cookie) ? "class='on'" : '';
 			$controles = array(1=>'Publica', 2=>'Privada');
 			$matematicas = $escuela->promedio_matematicas >= 0 ? round($escuela->promedio_matematicas) : '';
@@ -37,12 +40,12 @@
 				"</a></td>
 				<td class='rank matematicas'><span>".$matematicas."</span></td> 
      				<td class='rank espanol'><span>".$espaniol."</span></td>
-				<td class='nivel'>".$this->capitalize($escuela->nom_nivel)."</td>
-				<td class='control'>".$this->capitalize($escuela->control->nombre)."</td>
+				<td class='nivel'>".$this->capitalize($escuela->nivel->nombre)."</td>
+				<td class='control'>".$controles[$escuela->control->id]."</td>
 				<td class='rank'><span>{$rank_entidad}</span></td>
-				<td class='semaforo sem{$esc->semaforo}'><span></span>
+				<td class='semaforo sem{$escuela->semaforo}'><span></span>
 					<div class='icon'><span class='icon-popup'>
-						".$this->config->semaforos[$esc->semaforo]."
+						".$this->config->semaforos[$escuela->semaforo]."
 					</span></div>
 				</td>
 			</tr>
