98cb683fdf1613d669b573cc58a9cb80bbb4fdd9
urielaero <aero.uriel@gmail.com>
2013-08-13 02:34:55 -0500

    fixes del correo dudas y avances en los del archivo compartido
    Former-commit-id: e70ba0c8165c29dff9f28156083109d62481f7b9
    Former-commit-id: 461405ad17f3d4d980ad965e0a31f4dc6ce97c16
    Former-commit-id: be8f8b30254e05c759bc9b00c45ee06480c27b97
    Former-commit-id: 0614653322a5e98c6b5839dbb49f64fce9b73b95

--- controlers/controler.mejora.php
+++ controlers/controler.mejora.php
@@ -2,6 +2,7 @@
 class mejora extends main{
 	public function index(){
 		$this->title_header = 'Mejora tu escuela';
+		$this->subtitle_header = 'Aquí encontrarás herramientas para que actúes como agente <br />de cambio positivo en tu comunidad educativa. <br />¡Participa e involúcrate!';
 		$this->header_folder = 'compara';
 		$this->breadcrumb = array('#'=>'Mejora');
 		$this->include_theme('index','index');
