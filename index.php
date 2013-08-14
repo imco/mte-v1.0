189cc16cc6d741a404e3b49de4d68663a2b5dc0f
El Sonny <sonny@spaceshiplabs.com>
2013-08-14 11:13:38 -0500

    last minute
    Former-commit-id: c1faa3135d7df5b7d884666b978a482a22407891
    Former-commit-id: e619c0f9e7ec7e393dee0e4bd64bf4e2b4ee827e
    Former-commit-id: 04ff1b37b22c26f1dd4426727d9583da1563db74
    Former-commit-id: 214c820b9d6d8d0f957eacdd4f589c9ad7b7c555

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ else
 	$config_name = 'production_config';
 require_once "config/config.default_config.php"; 
 require_once "config/config.$config_name.php";
+//echo $config_name;
 $config = new $config_name();
 require_once $config->mxnphp_dir."/scripts/autoload.php";
 $mxnphp = new mxnphp($config);
