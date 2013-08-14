c0ee8e0a9c6c17bb6108d4bbdeb703b0638af783
El Sonny <sonny@spaceshiplabs.com>
2013-08-14 09:43:14 -0500

    rank entidad
    Former-commit-id: d85d3fc7badfc779e3d0c042f59f955b070684dc
    Former-commit-id: 2cbc43065fae34f1b861efd34ac040cb01d568af
    Former-commit-id: f85bd763330667098ea66a7427d84a5e31eb7da2
    Former-commit-id: 1ba400ef7a25f3f057d4022e982d887eebd3c9a8

--- rank_entidad.php
+++ rank_entidad.php
@@ -34,7 +34,7 @@ $niveles = array(12,13,22);
 				@prev_val := promedio_general AS promedio_general,
 				cct
 				FROM escuelas
-				WHERE nivel = 22 AND `promedio_general` IS NOT NULL AND total_evaluados >= 0 and poco_confiables<=.1*total_evaluados
+				WHERE nivel = 22 AND entidad = $entidad AND `promedio_general` IS NOT NULL AND total_evaluados >= 0 and poco_confiables<=.1*total_evaluados
 				ORDER BY promedio_general DESC) t2
 				ON t1.cct=t2.cct
 				SET t1.rank_entidad=t2.rank;";
