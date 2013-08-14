4c87d64de8183baba12793b645a26ab3edfa46f5
El Sonny <sonny@spaceshiplabs.com>
2013-08-14 08:32:57 -0500

    rank entidad
    Former-commit-id: ea43fc6de8e400db8b8d39b22b55bcd5dcf3db43
    Former-commit-id: aa9dabbd5606e3f71719a44d6f2bccdf6ccae309
    Former-commit-id: 15b8c9396d46b37af51d4f483650ba101dc7e623
    Former-commit-id: 019a0d93c86509ac1f9a9d4e3609a31aadd4e57f

--- rank_entidad.php
+++ rank_entidad.php
@@ -5,7 +5,7 @@ $user = 'root';
 $pwd = 'RtG/()rERtfkfGKLF'; /* Put your password here */
 $db = 'comparatuescuela';
 //$db = 'compara';
-
+exit;
 $conn = new mysqli($host,$user,$pwd,$db);
 
 if (mysqli_connect_errno()) {
