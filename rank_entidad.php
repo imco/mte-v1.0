4d8755e6a4e35461da94240a9416e64fa2a4ac67
El Sonny <sonny@spaceshiplabs.com>
2013-08-14 08:47:03 -0500

    ranknacional
    Former-commit-id: c85924fbf496f598582c1f5e7727a4740b89d562
    Former-commit-id: 08bc97548a02db60a982f03c255f1265a507ae41
    Former-commit-id: 53d569574522cc271fc9e601d94014910c6d7b0f
    Former-commit-id: 8969db4c0edb162520ffccd436f9adb6a03b2050

--- rank_entidad.php
+++ rank_entidad.php
@@ -15,11 +15,11 @@ if (mysqli_connect_errno()) {
 /*This will rank the schools at the entidad level */
 
 $entidades = range(1,32);
-//$niveles = array(12,13,22);
+$niveles = array(12,13,22);
 
-//foreach($niveles as $nivel){
+foreach($niveles as $nivel){
 
-	foreach($entidades as $entidad){
+	//foreach($entidades as $entidad){
 		/*Using bound variables for efficiency
 		$stmt = $conn->stmt_init();
 		$stmt->prepare($query);
@@ -34,16 +34,16 @@ $entidades = range(1,32);
 				@prev_val := promedio_general AS promedio_general,
 				cct
 				FROM escuelas
-				WHERE nivel = 22 AND entidad = '$entidad' AND `promedio_general` IS NOT NULL AND total_evaluados >= 4 and poco_confiables<=.1*total_evaluados
+				WHERE nivel = 22 AND `promedio_general` IS NOT NULL AND total_evaluados >= 4 and poco_confiables<=.1*total_evaluados
 				ORDER BY promedio_general DESC) t2
 				ON t1.cct=t2.cct
-				SET t1.rank_entidad=t2.rank;";
+				SET t1.rank_nacional=t2.rank;";
 		if(!$conn->query($sql)){
 			echo "Table creation failed: (" . $conn->errno . ") " . $conn->error;
 		}
 		echo $sql.'<br/>';
-	}
-//}
+	//}
+}
 
 $conn->close();
 
