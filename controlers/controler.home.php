66fd17a9aa60be5265c8ed50e66167c5c17b9f37
El Sonny <sonny@spaceshiplabs.com>
2013-08-14 22:39:06 -0500

    home rank
    Former-commit-id: b6f729b7cc03c1dcac5bef84a157b591a7a3a707
    Former-commit-id: 55ef05c1e5cfa1ddb0e38b19e14748e1aa9f5588
    Former-commit-id: 886ba3e3d251a07ee088231708db0e571c907801
    Former-commit-id: 8f99c045f3d937c3e4fd86a12d16874186e3a184

--- controlers/controler.home.php
+++ controlers/controler.home.php
@@ -11,23 +11,11 @@ class home extends main{
 	public function load_escuelas(){
 		$niveles = array(12,13,22);
 		$this->get_location();
-		$params->order_by = ' ISNULL(escuelas.rank_nacional), escuelas.rank_nacional ASC, escuelas.promedio_general DESC';
-		$params->nivel = $niveles[rand(0,2)];
-		$params->rank_nacional = rand(1,100);
-		$params->control = 1;
-		$params->limit = '0,1';
+		$params->order_by = ' ISNULL(escuelas.rank_entidad), escuelas.rank_entidad ASC, escuelas.promedio_general DESC';
+		$this->nivel_5 = $params->nivel = $niveles[rand(0,2)];
+		$params->entidad = $this->user_location->id;
+		$params->limit = '0,5';
 		$this->get_escuelas($params);
-		$this->publica = $this->escuelas[0];
-		$params->control = 2;
-		$this->get_escuelas($params);
-		$this->privada = $this->escuelas[0];
-		$params2->nivel = $niveles[rand(0,2)];
-		$this->nivel_5 = $params2->nivel;
-		$params2->entidad = $this->user_location->id;
-		$params2->order_by = ' ISNULL(escuelas.rank_nacional), escuelas.rank_nacional ASC, escuelas.promedio_general DESC';
-		$params2->limit = '0,5';
-		//$this->debug = true;
-		$this->get_escuelas($params2);
 		$this->process_escuelas();
 	}
 
