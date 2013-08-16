8d71b5b7135abe26a12012b5ab7f83fa07ce5eff
El Sonny <sonny@spaceshiplabs.com>
2013-08-16 13:02:59 -0500

    widget final
    Former-commit-id: 4087751a969c6d838517651d42159700787810ee
    Former-commit-id: c1543f36539f18251e9fe28d3149c05b1b7478e6
    Former-commit-id: f9ddccb4746b03d1aba839835c197a0d05403ba6
    Former-commit-id: b5f1b03ce5b9acfa1a1c49210b7eb9c4199e91ab

--- controlers/controler.widgets.php
+++ controlers/controler.widgets.php
@@ -1,7 +1,17 @@
 <?php
 class widgets extends main{
 	public function index(){	
+		$this->widget = json_decode( stripslashes(urldecode($this->get('w'))));
 		$this->include_template('index','widgets');
+	}
+	public function generate(){
+		$widget->title = 'Ayuda a transformar tu colegio';
+		$widget->text = 'Consulta los resultados de Enlace de las escuelas públicas y privadas del País y aprende cómo puedes ayudar a mejorar la educación de tu centro escolar.';
+		$widget->news_title = 'Cabeza de la nota aquí';
+		$widget->news_items[0]->title = 'nota';
+		$widget->news_items[0]->url = 'http://www.mejoratuescuela.org';
+		$code = urlencode(json_encode($widget));
+		echo $this->config->http_address.'/widgets/?w='.$code;
 	}
 }
 ?>
