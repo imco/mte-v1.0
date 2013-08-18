261f02446788a671126c99a7010c27152ac791e5
El Sonny <sonny@spaceshiplabs.com>
2013-08-18 09:02:53 -0500

    preguntas frecuents y css fix
    Former-commit-id: 6f27033925c75121d36fa0e30df35e768f707e75
    Former-commit-id: e0997419015a8cadcaf47799a8f0125adea7a35c
    Former-commit-id: d061f25671e71fa4006dddb25e98ab6a162df6ae
    Former-commit-id: fa8945ef5393cf9b6375b8417721e2507b74630e

---
+++ templates/mtev1/preguntas_frecuentes/index.php
@@ -0,0 +1,75 @@
+<div class='container ayuda'>
+	<div class='ayuda'>
+		<h2>Preguntas Frecuentes</h2>
+		<hr/>
+		<ol>
+			<li><a href='#pregunta1'>¿Qué hago si no encuentro mi escuela?</a></li>
+			<li><a href='#pregunta2'>¿Cómo se calcula la posición estatal de mi escuela?</a></li>
+			<li><a href='#pregunta3'>¿Cómo se calcula la ubicación de mi escuela en el semáforo educativo?</a></li>
+			<li><a href='#pregunta4'>¿Por qué mi escuela tiene una mala calificación en el semáforo educativo si mi hij@ está obteniendo buenas calificaciones en la escuela?</a></li>
+			<li><a href='#pregunta5'>¿Qué es la prueba ENLACE?</a></li>
+			<li><a href='#pregunta6'>¿Qué quiere decir el triángulo negro junto al nombre de mi escuela?</a></li>
+			<li><a href='#pregunta7'>¿Qué quiere decir el triángulo gris junto al nombre de mi escuela?</a></li>
+			<li><a href='#pregunta8'>¿Qué quiere decir el símbolo de precaución junto al nombre de mi escuela?</a></li>
+			<li><a href='#pregunta9'>¿Cómo puedo crear mi propia petición? </a></li>
+		</ol>
+		<div class="text-ayuda">
+			<div class="section-one">
+				<a name='pregunta1'></a>
+				<h3>1. ¿Qué hago si no encuentro mi escuela?</h3>
+				<p>Nuestro sitio tiene perfiles de todas las escuelas primarias, secundarias y bachilleratos públicas y privadas registradas ante la SEP a nivel nacional. Si intentaste buscar tu escuela por nombre y no te aparece en los resultados, intenta hacer una nueva búsqueda por zona geográfica. Si aún no encuentras tu escuela, escríbenos a contacto@mejoratuescuela.org y te ayudaremos a encontrarla. </p>
+				<a name='pregunta2'></a>
+				<h3>2. ¿Cómo se calcula la posición estatal de mi escuela?</h3>
+				<p>Para generar las posiciones hacemos una lista de todas las escuelas del estado, de cada nivel educativo, por sus calificaciones globales. La calificación global de una escuela está basada en un promedio ponderado de sus resultados de matemáticas y español en la prueba ENLACE del último año. Si quieres conocer a detalle la metodología detrás de nuestro sitio te invitamos a leer nuestra ficha metodológica.  (link a: www.mejoratuescuela.org/metodologia)</p>
+				<a name='pregunta3'></a>
+				<h3>3. ¿Cómo se calcula la ubicación de una escuela en el semáforo educativo?</h3>
+				<p>Ya que tenemos los promedios globales de cada escuela, graficamos la distribución de escuelas y generamos los cortes para el semáforo educativo. Los cortes son los siguientes: </p>
+				<p><b>Para primaria 2012:</b> <p>
+				<p><span class="green">Excelente:</span> calificación por encima de 664 puntos. <p>
+				<p><span class="amarillo">Bien:</span> por encima de 587 pero menos de 664.  <p>
+				<p><span class="naranja">De panzazo:</span> por encima de 546 pero menos de 587.<p>
+				<p><span class="red">Reprobado:</span> menos de 546 de promedio. <p>
+				<p><b>Para secundaria 2012:</b> <p>
+				<p><span class="green">Excelente:</span> calificación por encima de 622 puntos. <p>
+				<p><span class="amarillo">Bien:</span> por encima de 567 pero menos de 622.  <p>
+				<p><span class="naranja">De panzazo:</span> por encima de 530 pero menos de 567.<p>
+				<p><span class="red">Reprobado:</span> menos de 530 de promedio. <p>
+				<p><b>Para bachillerato 2013:</b> <p>
+				<p><span class="green">Excelente:</span> calificación por encima de 632 puntos. <p>
+				<p><span class="amarillo">Bien:</span> por encima de 580 pero menos de 632.  <p>
+				<p><span class="naranja">De panzazo:</span> por encima de 551 pero menos de 580.<p>
+				<p><span class="red">Reprobado:</span> menos de 551 de promedio. <p>
+				<a name='pregunta4'></a>
+				<h3>4. ¿Por qué mi escuela tiene una mala calificación en el semáforo educativo si mi hij@ está obteniendo buenas calificaciones en la escuela?</h3>
+				<p>Las calificaciones individuales de tu hij@ son independientes del desempeño de la escuela en conjunto o de su calidad educativa. En la boleta de calificaciones de la escuela, tu hij@ puede tener resultados muy altos, pero eso no quiere decir que tu hij@ esta cumpliendo con los estándares nacionales de aprendizaje. Te sugerimos que revises los resultados de tu hij@ en la prueba ENLACE, su calificación te puede dar un mejor estimado de su nivel de aprendizaje comparad@ con otros estudiantes de su edad. Sus resultados conocerlos en: </p>
+				<p>Para primaria y secundaria: <a href='http://201.175.44.205/Enlace/Resultados2012/Basica2012/R12Folio.aspx' >http://201.175.44.205/Enlace/Resultados2012/Basica2012/R12Folio.aspx</a> </p>
+				<p>Y para bachillerato en: </p>
+				<p><a href='http://201.175.44.205/Enlace/Resultados2013/MediaSuperior2013/R13msFolio.aspx'>http://201.175.44.205/Enlace/Resultados2013/MediaSuperior2013/R13msFolio.aspx</a></p>
+				<a name='pregunta5'></a>
+				<h3>5. ¿Qué es la prueba ENLACE?</h3>
+				<p>La Evaluación Nacional de Logro Académico en Centros Escolares (ENLACE) es una prueba del Sistema Educativo Nacional,  cuyo objetivo, es contribuir al avance educativo de los alumnos mexicanos de cada centro escolar y entidad federativa.</p>
+				<p>A nivel primaria, la prueba se aplica a alumnos de tercer grado a sexto de primaria.</p>
+				<p>En secundaria, la prueba se aplica a jóvenes de primero, segundo y tercero de secundaria.</p>
+				<p>En bachillerato o preparatoria, la prueba solamente se administra a los estudiantes que cursan el último año.</p>
+				<p>Las pruebas cubren temas en función de los planes y programas de estudios oficiales en las los campos de comunicación y comprensión lectora en español y en el campo de las matemáticas.  La prueba incluye también una tercer asignatura que cambia cada año, por ejemplo, en el 2008 fue ciencias, en el 2009 formación cívica, en el 2010 historia, en el 2011 geografía y en el 2012 nuevamente ciencias.</p>
+				<p>El propósito de ENLACE es generar una sola escala de carácter nacional que proporcione información comparable de los conocimientos y habilidades que tienen los estudiantes en los temas evaluados.  Para más información sobre la prueba, visita: <a href='http://enlace.sep.gob.mx/'>http://enlace.sep.gob.mx/</a></p>
+				<a name='pregunta6'></a>
+				<h3>6. ¿Qué quiere decir el triángulo negro junto al nombre de mi escuela?</h3>
+				<p>El triángulo negro junto al nombre de una escuela indica que la escuela no administra la prueba ENLACE a sus alumnos. Por lo tanto, no tenemos manera de obtener información para definir su calidad educativa.  Si este es el caso para tu escuela, te invitamos a que firmes una petición para que la prueba ENLACE se administre a todas las escuelas de tu estado. Encuentra las distintas peticiones <a href='http://www.mejoratuescuela.org/peticiones/'> aquí.</a></p>
+				<a name='pregunta7'></a>
+				<h3>7. ¿Qué quiere decir el triángulo gris junto al nombre de mi escuela?</h3>
+				<p>El triángulo gris indica que no todos los grados de la escuela toman la prueba ENLACE. Para los años que no tenemos resultado se asigna un cero, por lo tanto, el promedio de la escuela tiende a ser muy bajo.  Esta situación se puede dar en dos ocasiones:</p>
+				<p>La escuela toma la decisión de que uno de sus grados no tome la prueba ENLACE.</p>
+				<p>La escuela es nueva y aún no tiene estudiantes en todos los grados.</p>
+				<a name='pregunta8'></a>
+				<h3>8. ¿Qué quiere decir el símbolo de precaución junto al nombre de mi escuela?</h3>
+				<p>La prueba ENLACE utiliza tecnologías avanzadas para garantizar el control de calidad de los resultados individuales y por grupo.  La SEP identifica como “copia u otras acciones inaceptables” cualquier tendencia que indique la existencia de copia, dictado de respuestas y otras prácticas fraudulentas.  El símbolo de precaución junto al nombre de una escuela indica que la SEP  considera más del 10% de las pruebas ENLACE administradas por esa escuela como “no confiables” o “fraudulentas”.</p>
+				<a name='pregunta9'></a>
+				<h3>9. ¿Cómo puedo crear mi propia petición?</h3>
+				<p>Si quieres crear tu propia petición te invitamos a visitar el sitio de nuestros aliados: www.Change.org   Change.org® es un sitio independiente y apartidista que permite que cualquier persona, desde cualquier lugar del mundo pueda iniciar una campaña para generar cambios sociales en su comunidad. </p>
+				<p>Para iniciar una petición deberás registrarte en el sitio de Change.org, describir la problemática que deseas compartir, escoger a qué autoridades quieres que se le envíe tu petición, y finalmente compartirla con tus amigos y conocidos por medio de correos electrónicos o redes sociales. Si quieres que tu petición se muestre en MejoraTuEscuela.org, mándanos la liga a tu petición a: contacto@mejoratuescuela.org  y con mucho gusto la revisaremos. Mejora tu escuela busca promover iniciativas y peticiones ciudadanas que buscan mejorar la calidad de la educación en México. </p>
+			</div>	
+		</div>
+	</div>
+	<div class='clear'></div>
+</div>
