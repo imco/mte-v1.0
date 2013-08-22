45769b914934e3c3a2c583cac845386a98d26c62
El Sonny <sonny@spaceshiplabs.com>
2013-08-22 15:41:20 -0500

    recalculate avg
    Former-commit-id: cd8f1f6c3637a150e110b203386e47678289248e
    Former-commit-id: a4118f2dbafec3731b1de08c13f6fe020bef0129
    Former-commit-id: 32efe609f42c3d1f385cb09cc45d8d02cacb8a50
    Former-commit-id: 969dad5163c297ff3e03a1de84806f1ad27dda8f

--- templates/mtev1/widgets/index.php
+++ templates/mtev1/widgets/index.php
@@ -5,7 +5,7 @@
 	<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
 	<?php
 		$css_scripts = array("widget2.css");		
-		$cssmin = new mxnphp_min($this->config,$css_scripts,"css","css-min--mte-widget");
+		$cssmin = new mxnphp_min($this->config,$css_scripts,"css","css-min--mte-widget-2");
 		$cssmin->tag('css');
 	?>
 	<title><?=$this->page_title;?></title>
@@ -26,5 +26,15 @@
  			<div class='clear'></div>
  		</form>
  	</div>
+
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-4404650-7', 'mejoratuescuela.org');
+ga('send', 'pageview');
+</script>
  </body>
  </html>
