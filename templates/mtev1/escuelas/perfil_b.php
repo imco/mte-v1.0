45c60208aba4017c695dc221cbac90dcb174db40
urielaero <aero.uriel@gmail.com>
2013-08-27 00:07:21 -0500

    semaforo grados
    Former-commit-id: d39d2b8b42921c7e05c0263ff79f68c0705772e7
    Former-commit-id: 5ee3ca81a414579c389d902dc0dfcf214d74bdca
    Former-commit-id: f57819b4a7a968fc54643476b7cd784cf3a30303
    Former-commit-id: 3e492b53e7a6226086a8e0a62209fa8a5b842937

--- templates/mtev1/escuelas/perfil_b.php
+++ templates/mtev1/escuelas/perfil_b.php
@@ -276,7 +276,7 @@ EOD;
 			</div>
 			<?php
 				if($this->escuela->semaforo >= 4){
-					$semaforos = array('Escuela que no tomo prueba ENLACE','Escuela no Confiable');
+					$semaforos = array('Escuela que no tomo prueba ENLACE','Escuela no Confiable','Esta escuela no tomo la prueba enlace para todos los años');
 					echo "<div class='sem-overlay'><div class='icon icon{$this->escuela->semaforo}'></div><div class='clear'></div>".
 					$semaforos[$this->escuela->semaforo-4]."</div>";
 				}
