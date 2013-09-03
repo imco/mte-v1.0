e0a6273ce545a3098b5aae5e670af258bfb21438
urielaero <aero.uriel@gmail.com>
2013-09-02 21:59:55 -0500

    min js funcionando corectamente
    Former-commit-id: ebc66a3d536ebba86b4b9cf0dd5476787fadea05
    Former-commit-id: a64e42251af94f295531c59a4c0622ec980ac039
    Former-commit-id: fa4c9baffb664d735daacfba2be61c52e8658544
    Former-commit-id: 9558f54caf5d97a5b8487987b3016cf3d12b9c66

--- templates/mtev1/themes/index.php
+++ templates/mtev1/themes/index.php
@@ -14,27 +14,31 @@
 		$js_scripts = array(
 			"jquery.js",
 			"jquery-ui.js",
-			"jquery.tweet.js",
 			'jquery.mousewheel.js',
 			'jquery.jscrollpane.min.js',
 			'jquery.formatDate.js',
 			'jquery.customSelect.min.js',
 			'jquery.validate.min.js',
 			'jquery.cookie.js',
-			"interactions.js"
+			'interactions.js',
+			'school-charts.js',
+			//'entidad-charts.js', ya no se usa
+			'infobox_packed.js',
+			'map.js'
 		);
 		if($this->location == 'escuelas'){
-			$js_scripts[] = 'school-charts.js';
+			//$js_scripts[] = 'school-charts.js'; // si no hay cambios en el js no renderizara 
+			//correctamente el min.
 			echo '<script type="text/javascript" src="https://www.google.com/jsapi"></script>';
 		}
 		if($this->draw_charts){
-			$js_scripts[] = 'entidad-charts.js';
+			//$js_scripts[] = 'entidad-charts.js';
 			echo '<script type="text/javascript" src="https://www.google.com/jsapi"></script>';
 		}
 		if($this->draw_map){
 			echo '<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBlzbyX3J7GwOXdoRwMDfYVbqxNG1D9Jy0&sensor=true"></script>';
-			$js_scripts[] = 'infobox_packed.js';
-			$js_scripts[] = 'map.js';
+			//$js_scripts[] = 'infobox_packed.js';
+			//$js_scripts[] = 'map.js';
 		}
 		$cssmin = new mxnphp_min($this->config,$css_scripts,"css","css-min-mte");
 		$jsmin = new mxnphp_min($this->config,$js_scripts,"js","js-min-mte");
