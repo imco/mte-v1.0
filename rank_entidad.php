c68bc7fa8fadf2d3dc27f9fff6185e56e2fc47b9
El Sonny <sonny@spaceshiplabs.com>
2013-09-03 18:14:58 -0500

    rank entidad
    Former-commit-id: 6750236e6dda5cc9ca323f8340d0549a0070e31a
    Former-commit-id: 400659996ea3e726828a811795a0cd1ca10500e6
    Former-commit-id: f56762c15a054bd14a91a9a00369a649962fccd2
    Former-commit-id: b5d4d92179d625c251206aaae2349787081dafdd

--- rank_entidad.php
+++ rank_entidad.php
@@ -18,18 +18,17 @@ $conn->query('UPDATE escuelas SET rank_entidad = NULL WHERE 1');
 foreach($niveles as $i => $nivel){
 	$grado = $grados[$i];
 	foreach($entidades as $entidad){
-		$sql = "SET @rownum = 0, @rank = 0, @prev_val = NULL; ";
-		$conn->query($sql);
-
+		$conn->query("SET @rownum = 0, @rank = 0, @prev_val = NULL; ");
 		$sql = "UPDATE escuelas t1
 				JOIN (
-				SELECT @rownum := @rownum + 1 AS row,
-				@rank := IF(@prev_val!=promedio_general,@rownum,@rank) AS rank,
-				@prev_val := promedio_general AS promedio_general,
-				cct
-				FROM escuelas
-				WHERE nivel = $nivel AND entidad = $entidad AND `promedio_general` IS NOT NULL AND total_evaluados >= 0 AND poco_confiables<=.1*total_evaluados AND grados >= $grado
-				ORDER BY promedio_general DESC) t2
+					SELECT @rownum := @rownum + 1 AS row,
+					@rank := IF(@prev_val!=promedio_general,@rownum,@rank) AS rank,
+					@prev_val := promedio_general AS promedio_general,
+					cct
+					FROM escuelas
+					WHERE nivel = '$nivel' AND entidad = '$entidad' AND `promedio_general` IS NOT NULL AND total_evaluados >= 0 AND poco_confiables<=.1*total_evaluados AND grados >= $grado
+					ORDER BY promedio_general DESC
+				) t2
 				ON t1.cct=t2.cct
 				SET t1.rank_entidad=t2.rank;";
 		if(!$conn->query($sql)){
