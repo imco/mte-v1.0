5ee05ed92d247cd89058fca6a2db96bbb29e0798
El Sonny <sonny@spaceshiplabs.com>
2013-09-03 17:35:48 -0500

    rank entidad fix
    Former-commit-id: df75429f1dcef25146f8240eaca9308b2fb3e989
    Former-commit-id: c3c7bec5e2aa4e5694b73f2e8b1fa6f0540edfb6
    Former-commit-id: 7c364cd00a86ce14ac68f2a473573b316c2a0838
    Former-commit-id: db1b34768230104c2c8c089f6e8ef9b715dac46c

--- rank_entidad.php
+++ rank_entidad.php
@@ -1,33 +1,22 @@
 <?php
 $host = '***REMOVED***';
 $user = '***REMOVED***';
-//$pwd = ''; /* Put your password here */
-$pwd = '***REMOVED***'; /* Put your password here */
+$pwd = '***REMOVED***'; 
 $db = '***REMOVED***';
-
-//$db = 'compara';
 $conn = new mysqli($host,$user,$pwd,$db);
-//exit;
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-
-/*This will rank the schools at the entidad level */
-
 $entidades = range(1,32);
 $niveles = array(12,13,22);
-
-//foreach($niveles as $nivel){
-
-	//foreach($entidades as $entidad){
-	/*Using bound variables for efficiency
-		$stmt = $conn->stmt_init();
-		$stmt->prepare($query);
-		$stmt->bind_param('ii', $nivel, $municipio);
-		$stmt->execute();*/
+$grados = array(4,3,1);
+foreach($niveles as $i => $nivel){
+	$grado = $grados[$i];
+	foreach($entidades as $entidad){
 		$sql = "SET @rownum = 0, @rank = 0, @prev_val = NULL; ";
 		$conn->query($sql);
+
 		$sql = "UPDATE escuelas t1
 				JOIN (
 				SELECT @rownum := @rownum + 1 AS row,
@@ -35,16 +24,16 @@ $niveles = array(12,13,22);
 				@prev_val := promedio_general AS promedio_general,
 				cct
 				FROM escuelas
-				WHERE nivel = 12 AND `promedio_general` IS NOT NULL AND total_evaluados >= 0 AND poco_confiables<=.1*total_evaluados AND grados >= 4
+				WHERE nivel = $nivel AND `promedio_general` IS NOT NULL AND total_evaluados >= 0 AND poco_confiables<=.1*total_evaluados AND grados >= $grado
 				ORDER BY promedio_general DESC) t2
 				ON t1.cct=t2.cct
-				SET t1.rank_nacional=t2.rank;";
+				SET t1.rank_entidad=t2.rank;";
 		if(!$conn->query($sql)){
 			echo "Table creation failed: (" . $conn->errno . ") " . $conn->error;
 		}
 		echo $sql.'<br/>';
-	//}
-//}
+	}
+}
 
 $conn->close();
 
