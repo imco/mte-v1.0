079c3104889b058b8090eb252375ba99b241e295
El Sonny <sonny@spaceshiplabs.com>
2013-09-03 17:50:21 -0500

    rank_nacional
    Former-commit-id: 14681b3bc2fbfbad2e3e7be412176201ea9d8417
    Former-commit-id: 1535feface7ac0e74cee590ac297ece5c02b0f2d
    Former-commit-id: 896d7849ee21a62eb8cb9d934ea4c01fb96dad0e
    Former-commit-id: 171ed79d31ec28a43edf1f44aac967f204610cda

---
+++ rank_nacional.php
@@ -0,0 +1,42 @@
+<?php
+$host = '***REMOVED***';
+$user = '***REMOVED***';
+$pwd = '***REMOVED***'; 
+$db = '***REMOVED***';
+$conn = new mysqli($host,$user,$pwd,$db);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+/*This will rank the schools at the entidad level */
+
+$entidades = range(1,32);
+$niveles = array(12,13,22);
+$grados = array(4,3,1);
+$conn->query('UPDATE escuelas SET rank_nacional = NULL WHERE 1');
+foreach($niveles as $i => $nivel){
+	$grado = $grados[$i];
+	$sql = "SET @rownum = 0, @rank = 0, @prev_val = NULL; ";
+	$conn->query($sql);
+
+	$sql = "UPDATE escuelas t1
+			JOIN (
+			SELECT @rownum := @rownum + 1 AS row,
+			@rank := IF(@prev_val!=promedio_general,@rownum,@rank) AS rank,
+			@prev_val := promedio_general AS promedio_general,
+			cct
+			FROM escuelas
+			WHERE nivel = $nivel AND `promedio_general` IS NOT NULL AND total_evaluados >= 0 AND poco_confiables<=.1*total_evaluados AND grados >= $grado
+			ORDER BY promedio_general DESC) t2
+			ON t1.cct=t2.cct
+			SET t1.rank_nacional=t2.rank;";
+	if(!$conn->query($sql)){
+		echo "Table creation failed: (" . $conn->errno . ") " . $conn->error;
+	}
+	echo $sql.'<br/>';
+}
+
+$conn->close();
+
+?>
