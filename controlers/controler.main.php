24ea0601f4907d0571964158a4bc12d4d22db4a8
urielaero <aero.uriel@gmail.com>
2013-09-04 22:21:13 -0500

    test de turno en busqueda por term
    Former-commit-id: 3448295e28b18a3a0e582b0081210c4dd880fe0e
    Former-commit-id: ccbf4940abc1bf68683b3c2592bd6f365d19d041
    Former-commit-id: 4e5a538bd59933622b7c3cdfef7320d87c0afa08
    Former-commit-id: 6b919d0d27b2a271fb592943ea136128f90c5e20

--- controlers/controler.main.php
+++ controlers/controler.main.php
@@ -203,7 +203,6 @@ class main extends controler{
 		$response = json_decode(file_get_contents($url));
 		$this->escuelas = $response->response->docs;
 		$this->num_results = $response->response->numFound;
-		var_dump($response->response);
 	}
 
 	public function load_niveles(){
