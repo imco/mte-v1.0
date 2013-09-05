689b678218fe9dc7bcd71efa7565b88824ec0b00
urielaero <aero.uriel@gmail.com>
2013-09-04 21:54:00 -0500

    correcciones ortograficas
    Former-commit-id: 91cf0ff2da79a21bdcb9b9fe6068fdf97b3f5699
    Former-commit-id: c50653c3e86e9b17c37ac0e86f130f2161579968
    Former-commit-id: fe32894bff634097056300f4ad7cafccdf116240
    Former-commit-id: 4377f2cdb94860480b9dc96a6fb69e99d8874d9b

--- config/config.default_config.php
+++ config/config.default_config.php
@@ -16,7 +16,7 @@ class default_config{
 		$this->dev_mode = false;
 		
 		//MTE
-		$this->semaforos = array('Reprobado','De panzazo','Bien','Excelente','No Enlace','Poco confiable','Esta escuela no tomo la prueba enlace para todos los años');
+		$this->semaforos = array('Reprobado','De panzazo','Bien','Excelente','No Enlace','Poco confiable','Esta escuela no tomó la prueba ENLACE para todos los años');
 
 		//Change.org
 		$this->change_api_key = '***REMOVED***';
