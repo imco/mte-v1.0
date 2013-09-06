1978aab6704cf0d21e8ce0de66919fa2a14d20dc
Paco Mekler <francisco.mekler@imco.org.mx>
2013-09-06 04:07:06 +0000

    subtitulo en quienes somos prueba
    Former-commit-id: 28883b124d471cc3cf2fe9837b9d1f064faa4709
    Former-commit-id: e8c0b212ad74ea9c02369e35b9651787bd252050
    Former-commit-id: a961b7e886fddd3f9179540abf7795f0959109d8
    Former-commit-id: 4a711e267fac3cc8b9f5bb646256804bbeecae2c

--- controlers/controler.quienes_somos.php
+++ controlers/controler.quienes_somos.php
@@ -4,6 +4,10 @@ class quienes_somos extends main{
 		$this->title_header = '¿Quiénes somos?';
 		$this->header_folder = 'compara';
 		$this->breadcrumb = array('#'=>'¿Quiénes somos?');
+		$this->subtitle_header = '
+			MejoraTuEscuela.org es una plataforma que busca <br />
+			promover la participación ciudadana para transformar <br />
+			la educación en México.';
 		$this->include_theme('index','index');
 	}
 }
