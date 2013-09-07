b5e71954109bf176b13f09a1c42128dce8b6d487
urielaero <aero.uriel@gmail.com>
2013-09-07 03:20:03 -0500

    avances del post-lanzamiento, bug en peticiones por el jscrollpane desaparecido correjido
    Former-commit-id: 09783c38c95bb2cfc421c259d00c4c2152cacaa3
    Former-commit-id: 6dec2e74c7ce44ef4886888a93c89d90b77c85c8
    Former-commit-id: ea33114e4708a6ec46e0173dec3da9b91d15e984
    Former-commit-id: 31f7e91ea95a6d9ca84993dbb3ff352fd5671972

--- templates/mtev1/aviso_de_privacidad/index.php
+++ templates/mtev1/aviso_de_privacidad/index.php
@@ -5,18 +5,18 @@
 		
 		<div class="text-ayuda">
 			<div class="section-one">
-				<p>La organización responsable de la administración del sitio www.mejoratuescuela.org es el Instituto Mexicano para la Competitividad A.C. (IMCO) <a href='http://www.imco.org.mx'>www.imco.org.mx</a></p>
-				<p>Contacto en <a href='mailto:contacto@mejoratuescuela.org'>contacto@mejoratuescuela.org</a></p>
-				<p>Los datos que solicitamos serán utilizados para las siguientes finalidades:</p>
+				<p>Los datos solicitados en www.mejoratuescuela.org  son tratados por el Instituto Mexicano para la Competitividad A.C. (IMCO), <a href='http://www.imco.org.mx'>www.imco.org.mx</a></p>
+				<p>Para más información, escríbenos a: <a href='mailto:contacto@mejoratuescuela.org'>contacto@mejoratuescuela.org</a></p>
+				<p>Los datos personales que recopilamos son utilizados para los siguientes propósitos:</p>
 				<ul>
-					<li>Para el envío de noticias e información.</li>
-					<li>El sitio recaba información del flujo de ingreso de usuarios al portal con fines exclusivamente estadísticos para medir el uso y mejorar el contenido del sitio web.</li>
-					<li>En algunas áreas del portal como reportes ciudadanos y denuncias, el sitio requiere que el Titular facilite datos personales como nombre, sexo, correo electrónico, etc.). Estos datos estarán protegidos con base en el tratamiento de datos personales establecido en las presentes Condiciones de Uso y Aviso de Privacidad.</li>
-					<li>Además, el sitio podrá recoger indirectamente información sobre el Titular cuando use ciertos servicios de terceros en el sitio web.</li>
-					<li>Para su integración a redes sociales incluyendo Facebook y Twitter.</li>
-					<li>Geolocalización del Titular para presentar propuestas basadas en una ubicación geográfica y presentar contenido relevante para el usuario.</li>
+					<li>Envío de noticias e información.</li>
+					<li>Recabar información del flujo de ingreso de usuarios al portal con fines exclusivamente estadísticos que permitan medir el uso y mejorar tanto el contenido como el funcionamiento del sitio web.</li>
+					<li>En algunas áreas del portal (como reportes ciudadanos y denuncias), el sitio requiere que el Titular facilite datos personales (nombre, sexo, correo electrónico, etc.). Estos datos estarán protegidos con base en el tratamiento de datos personales establecido en las presentes Condiciones de Uso y Aviso de Privacidad.</li>
+					<li> El sitio podrá recabar, indirectamente, información sobre el Titular cuando utilice ciertos servicios de terceros en el sitio web.</li>
+					<li>Para su integración a redes sociales, incluyendo Facebook y Twitter.</li>
+					<li>Geolocalización del Titular para elaborar propuestas basadas en su ubicación geográfica y presentar contenido relevante para el usuario.</li>
 				</ul>
-				<p>Para leer nuestro aviso de privacidad completo favor de hacer click <a href='/files/AvisoPrivacidadMTE_Jun7.pdf'>aquí</a></p>
+				<p>Para leer nuestro aviso de privacidad completo favor de hacer click <a class='aviso-completo' href='/files/AvisoPrivacidadMTE_Jun7.pdf'>aquí</a></p>
 			</div>		
 		</div>
 	</div>
