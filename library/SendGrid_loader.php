21b1d00e29df83f94e2e423cceb167e1ebc3c929
Paco Mekler <francisco.mekler@imco.org.mx>
2013-09-11 16:39:58 +0000

    sendgrid loader
    Former-commit-id: 01a57b6e5861b6c20000e37a270ea33d4fd8b09e
    Former-commit-id: d1318cea6f26bc5258787c8cdcd538ecf9883f99
    Former-commit-id: dc950d2ac68e32530b4da9e75c1fa56523e6d305
    Former-commit-id: ee57185c92190f12233f6ad29e79a0efe07dd099

--- library/SendGrid_loader.php
+++ library/SendGrid_loader.php
@@ -7,6 +7,7 @@ function sendGridLoader($string)
   if ($string == 'SendGrid' || stripos($string, 'SendGrid\\') === 0)
   {
     $file = str_replace('\\', '/', "$string.php");
+    var_dump( ROOT_DIR . $file);exit; 
     include_once ROOT_DIR . $file;
   }
 }
