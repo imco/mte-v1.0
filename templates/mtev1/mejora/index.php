a3f040d1b8a14ddf9eddda0a75e13f93e3b55c6a
El Sonny <sonny@spaceshiplabs.com>
2013-09-18 19:54:39 -0500

    ver mas botones
    Former-commit-id: e79fddc549b423be4553da43dd1bf5231d918a74
    Former-commit-id: b46229d280969141139452653e9583baaedf4db2
    Former-commit-id: aa8bfd944382fa9189ae5b2c3359c8a52951eec7
    Former-commit-id: 28523afc273855cb88a1283eb3de620954bef3cc

--- templates/mtev1/mejora/index.php
+++ templates/mtev1/mejora/index.php
@@ -1,4 +1,13 @@
 <div class='container mejora'>
 	<?=file_get_contents($this->config->blog_address."mejora") ?>
 	<div class='clear'></div>
+	<div class="share-bt bl left">
+		<a class="button-frame static" href="<?=$this->config->blog_address?>">
+			<span class="bt-share button-efect">		
+				Mas Notas
+			</span>
+		</a>
+	</div>
+	<div class='clear'></div>
 </div>
+
