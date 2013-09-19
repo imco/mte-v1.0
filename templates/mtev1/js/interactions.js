ddab3f932035ce5cbe4c6b4c5ed4d98581c9d244
El Sonny <sonny@spaceshiplabs.com>
2013-09-19 13:53:46 -0500

    twitter api
    Former-commit-id: c8d6c91de9f3123356eb83d6b161a25d1a5b538a
    Former-commit-id: 1f3ebb52eccfc24ebeff4697ed4aac20e2e09b00
    Former-commit-id: 1e554d9ba946eb9d59473e9b8ea4920d665e90dd
    Former-commit-id: 4bfc7c900b494422224c521f8080c4980dc31178

--- templates/mtev1/js/interactions.js
+++ templates/mtev1/js/interactions.js
@@ -344,6 +344,7 @@ function twitterIni(){
 	page_proxy = '/home/twitter';	
 	$("#tweets .tweet p").html("cargando tweets...");
 	$.getJSON(page_proxy, function(data){
+		data = data.statuses;
 		$("#tweets .tweet").css('display','none');
 		$("#tweets").append('<ul></ul>');
 		for(d in data){
