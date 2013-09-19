c5fc89de56d9156a5894edf561ed1131207a1c33
urielaero <aero.uriel@gmail.com>
2013-09-18 21:29:25 -0500

    Test califica
    Former-commit-id: 72d7b1f4f065e2d0a42142edd09adc5cec855e01
    Former-commit-id: c47afe591470ee59978b047dc7d7e46f826af4fa
    Former-commit-id: 9a1438195312bf456ea86dd48413c1122fc9d90c
    Former-commit-id: eb6951a736299ce2bb82335b8618bd25c6231ef2

--- controlers/controler.califica_tu_escuela.php
+++ controlers/controler.califica_tu_escuela.php
@@ -7,15 +7,44 @@ class califica_tu_escuela extends main{
 
 	}
 	public function califica(){
+		$this->title_header = 'Califica tu escuela';
+		$this->subtitle_header = 'Una vez que conoces y has comparado tu escuela, te invitamos a<br />que califiques algunos aspectos de la misma. Las calificaciones<br />ayudan a detectar áreas de mejora y a reconocer los<br />logros alcanzados.';
+		$this->header_folder = 'compara';
 		if($this->escuela_info()){
-			$this->title_header = 'Califica tu escuela';
-			$this->subtitle_header = 'Una vez que conoces y has comparado tu escuela, te invitamos a<br />que califiques algunos aspectos de la misma. Las calificaciones<br />ayudan a detectar áreas de mejora y a reconocer los<br />logros alcanzados.';
-			$this->header_folder = 'compara';
 			$this->breadcrumb = array('/califica-tu-escuela/'=>'Califica tu escuela','#'=>$this->escuela->nombre);
 			$this->include_theme('index','califica');
 		
 		}else{
-			header("location: /compara/");
+			#header("location: /compara/");
+			$this->load_compara_cookie();
+			if($this->get('term')){
+				
+				$params->term = $this->get('term');
+				$params->control = $this->get('control');
+				$params->nivel = $this->get('nivel');
+				$params->entidad = $this->get('entidad');
+				$params->municipio = $this->get('municipio');
+				$params->localidad = $this->get('localidad');
+				$p = $this->get('p') ? $this->get('p') : 1;
+				$this->get_escuelas_new($params,$p);
+			
+				$this->cct_count_entidad();
+				$this->resultados_title = 'Resultados de tu búsqueda';
+				$this->set_info_user_search($this->num_results);
+				
+				/*$params->pagination = 6;
+				$params->order_by = ' ISNULL(escuelas.rank_entidad), escuelas.rank_entidad ASC, escuelas.promedio_general DESC';
+				$this->get_escuelas($params);
+				$this->process_escuelas();
+				*/
+			}
+			if($this->compara_cookie){
+				$temp = isset($this->escuelas)?$this->escuelas:array();
+				$params2->ccts = $this->compara_cookie;
+				$this->get_escuelas($params2);
+				$this->escuelas = array_merge($temp,$this->escuelas);
+			}
+			$this->include_theme('index','index');
 		}
 	}
 
