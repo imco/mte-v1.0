59c220a9cbeac9bf5437ba9220e9976a70cce683
El Sonny <sonny@spaceshiplabs.com>
2013-09-25 14:40:49 -0500

    import fix
    Former-commit-id: 7a30499caf10765292b3bcfc71bebdc7fec7d059
    Former-commit-id: 255bab692088a196009bb7bf6cc7243db17f84fb
    Former-commit-id: 84991baf70382a1b182cba4649019a4843cfaf23
    Former-commit-id: 3591c6c2da48ce58b6bd3bf4739373902eda6e30

--- templates/mtev1/home/index.php
+++ templates/mtev1/home/index.php
@@ -33,7 +33,7 @@ $niveles = array(12 => 'primarias',13 => 'secundarias',22 => 'bachilleratos')
 			<div class="share-bt bl" style='position:absolute;right:0;bottom:30px'>
 				<a class="button-frame static" href="<?=$this->config->blog_address?>">
 					<span class="bt-share button-efect">		
-						Mas Notas
+						Más notas
 					</span>
 				</a>
 			</div>
