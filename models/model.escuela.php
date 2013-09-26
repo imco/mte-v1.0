475d74f79bb54acef2fc4c6b62c554af11a2c172
Paco Mekler <francisco.mekler@imco.org.mx>
2013-09-26 18:14:20 +0000

    cambio en algoritmo de clasificacion de mte get_semaforo
    Former-commit-id: 278ab58a14d46b764b58a8ba77e655b856c929a6
    Former-commit-id: 59f054c54e557325eb3f2c7b0c501a63223b92b1
    Former-commit-id: 19e61171eb6eda5bfcaba9c60e46f44030dffb2d
    Former-commit-id: 84d64a8f999882487d9ae3e41a6c34d9fb91af1b

--- models/model.escuela.php
+++ models/model.escuela.php
@@ -40,6 +40,7 @@ class escuela extends table{
 		$turnos = isset($this->turno->num) ? $this->turno->num : 1;
 
 		if(isset($this->grados) && $this->grados>0 ){
+			echo 'entro 1';exit;
 			if($this->nivel->nombre != "BACHILLERATO"  && ($this->grados < 4 * $turnos && $this->nivel->nombre == "PRIMARIA") || ($this->grados < 3 * $turnos && $this->nivel->nombre == "SECUNDARIA") ){
 				$this->semaforo = 6;//no se cuentan
 			}else if($porcentaje_poco_confiable > 0 && $porcentaje_poco_confiable >= $this->semaforo_poco_confiable){
