0bb8a27a5188e440c5106c624eebdcf12d6b82c9
Paco Mekler <francisco.mekler@imco.org.mx>
2013-09-26 17:31:35 +0000

    cambio en algoritmo de clasificacion de mte get_semaforo
    Former-commit-id: 320e0ec06880b003f6f3c49ba22ecb57ddf3b5d3
    Former-commit-id: 4567219b658560fa0b2f12c2db72c12eed52af6b
    Former-commit-id: 6ae389b10ffa589a21c3b144b0645607a6a7e4ab
    Former-commit-id: b8a3c00ad8c821c82595920aafe124c324f91a46

--- models/model.escuela.php
+++ models/model.escuela.php
@@ -34,13 +34,13 @@ class escuela extends table{
 
 	}
 	function get_semaforo(){
-		$this->semaforo = ;
+		$this->semaforo = 4;
 		$porcentaje_poco_confiable = $this->poco_confiables > 0 && $this->total_evaluados > 0?($this->poco_confiables * 100) / $this->total_evaluados:0;
 		$this->porcentaje_poco_confiable = number_format($porcentaje_poco_confiable,2);
 		$turnos = isset($this->turno->num) ? $this->turno->num : 1;
 		var_dump($this->grados,
 			isset($this->grados), $this->grados>0,$this->nivel->nombre != "BACHILLERATO" , ($this->grados < 4 * $turnos && $this->nivel->nombre == "PRIMARIA") || ($this->grados < 3 * $turnos && $this->nivel->nombre == "SECUNDARIA") ,
-			((isset($this->grados) && $this->grados>0 )&& $this->nivel->nombre != "BACHILLERATO"  && ($this->grados < 4 * $turnos && $this->nivel->nombre == "PRIMARIA") || ($this->grados < 3 * $turnos && $this->nivel->nombre == "SECUNDARIA")) );exit;
+			((isset($this->grados) && $this->grados>0 ) && $this->nivel->nombre != "BACHILLERATO"  && ($this->grados < 4 * $turnos && $this->nivel->nombre == "PRIMARIA") || ($this->grados < 3 * $turnos && $this->nivel->nombre == "SECUNDARIA")) );exit;
 
 		if(isset($this->grados) && $this->grados>0 && $this->nivel->nombre != "BACHILLERATO"  && ($this->grados < 4 * $turnos && $this->nivel->nombre == "PRIMARIA") || ($this->grados < 3 * $turnos && $this->nivel->nombre == "SECUNDARIA") ){
 			$this->semaforo = 6;
