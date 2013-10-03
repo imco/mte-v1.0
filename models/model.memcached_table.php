ac021abf6d8b3964cd3551c9d619611b48c9111a
El Sonny <sonny@spaceshiplabs.com>
2013-10-03 12:48:36 -0500

    memcached mods
    Former-commit-id: d9eb862500380ce2361ceb17f829fcd08a736cba
    Former-commit-id: 8e4c7a821584221cf5c7ee3c5d6443854fb61885
    Former-commit-id: c4d9eeee85633d3c21b9cddd71c35b1a0ea05d50
    Former-commit-id: ef20305528da003c9ba03fe80698f370aa3a8719

---
+++ models/model.memcached_table.php
@@ -0,0 +1,24 @@
+<?php
+class memcached_table extends table{
+	function read($fields){
+		if(class_exists('Memcache')){
+			$memcache = new Memcache;	
+			$memcache->connect('***REMOVED***', 11211) or die ("Could not connect memcache");
+			$this->execute = false;
+			parent::read($fields);
+			$this->execute = true;
+			$query_hash = md5($this->sql);
+			if($result = $memcache->get($query_hash)){
+				echo 'memcached';
+				return $result;
+			}else{
+				print("<br/>Query from DB<br/>");
+				$result = parent::read();				
+				$memcache->set($query_hash,$result,false,0);
+			}
+		}else{
+			return parent::read($fields);
+		}		
+	}
+}
+?>
