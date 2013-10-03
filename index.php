052ca695dce3410f4744174093d6ebc765914a7f
El Sonny <sonny@spaceshiplabs.com>
2013-10-03 13:05:13 -0500

    memcached dev
    Former-commit-id: 16caf96eca0be22ba961dbfcc145702310a03c85
    Former-commit-id: 0c42cf0ef8bdc3eb87c3e1a302400a24cc1c9ef1
    Former-commit-id: 2ac9806d8b8c96e560d3bf55831f5aeba936c1c3
    Former-commit-id: 0ba6985625c00f6a56f4cf18088ef33b7029abd3

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-phpinfo();
-/*
+
 $env = getenv('APPLICATION_ENV');
 if($env != "")
 	$config_name= $env;
@@ -15,5 +14,5 @@ require_once $config->mxnphp_dir."/scripts/autoload.php";
 $mxnphp = new mxnphp($config);
 $mxnphp->load_model();
 $mxnphp->load_controler();
-*/
+
 ?>
