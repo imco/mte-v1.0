f945cd6e46ee63c5c1cff1ec5f2813ded6867bcf
El Sonny <sonny@spaceshiplabs.com>
2013-10-04 16:10:50 -0500

    memcache
    Former-commit-id: ceb8d8d0359d869cc3bdaf5e245f488864ca2c2c
    Former-commit-id: 78ff272dbed9473c4aa76f60f5fcc8dd8fb4add7
    Former-commit-id: 32db6fb368556a7d004f59de6a7037a0a188e7cd
    Former-commit-id: e22ebce146e38861eed26ca7f59bb5f4bd57e3d2

--- models/model.memcached_table.php
+++ models/model.memcached_table.php
@@ -12,14 +12,14 @@ class memcached_table extends table{
 			if($result = $memcache->get($query_hash)){				
 				$time_end = microtime(true);
 				$time = $time_end - $time_start;
-				echo 'Memcached: '.$time.'<br/>';
+				//echo 'Memcached: '.$time.'<br/>';
 				return $result;
 			}else{
 				$result = parent::read($fields);				
 				$time_end = microtime(true);
 				$memcache->set($query_hash,$result,false,0);
 				$time = $time_end - $time_start;
-				echo "Query from DB:".$time."<br/>";
+				//echo "Query from DB:".$time."<br/>";
 
 				return $result;
 			}
