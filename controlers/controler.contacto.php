196b07bc12a37188796b4d53c18628767d9a7f35
urielaero <aero.uriel@gmail.com>
2013-10-22 02:10:39 -0500

    docu: califica_tu_escuela, contacto,escuelas,home
    Former-commit-id: 54730aad1447ee1d9b4c0c45cfbb54340978afef
    Former-commit-id: 89e428e77b39783db7126b98e9286496444ee741
    Former-commit-id: 0b42ebc45d4696054b45192ff2cf1a18121738c1
    Former-commit-id: a521cf83f2dfd99222dad9a8cb8e02c08c0775c1

--- controlers/controler.contacto.php
+++ controlers/controler.contacto.php
@@ -1,11 +1,16 @@
 <?php
 class contacto extends main{
+	/* Controlador: /contacto/*
+	*/
 	public function index(){		
 		$this->header_folder ='contacto';		
 		$this->breadcrumb = array('#'=>'Contacto');
 		$this->include_theme('index','index');
 	}
+
 	public function enviar(){
+		/* Obtiene los datos del formulario de contacto y estos son enviados a la dirección de correo electrónico del atributo del archivo de configuración 'contact_email'.
+		*/
 		$this->contact_status = $this->send_email(
 			$this->config->contact_email,
 			'Correo electronico desde Mejora tu escuela',
