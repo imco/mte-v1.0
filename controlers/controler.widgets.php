8ca81011b8df0e9c21fd9164a1da6a928ca295c6
urielaero <aero.uriel@gmail.com>
2013-10-22 22:18:49 -0500

    docu faltante
    Former-commit-id: 9ebffd0f4c62cf3ccfd6feb141db9a5ec4e4fee0
    Former-commit-id: ea8df309a5a006c3df1498a3b77b313b1697a884
    Former-commit-id: c53fc823fe02170afc384927df35a844bc7869b5
    Former-commit-id: 17f5bccbc6dac673577001536e37d9317263906b

--- controlers/controler.widgets.php
+++ controlers/controler.widgets.php
@@ -1,14 +1,20 @@
 <?php
 class widgets extends main{
+	/* Controlador: host/widget/*
+	   Se encarga de mostrar elementos que pueden ser embebidos en otras paginas.
+	*/
 	public function index(){
+		/* Obtiene los datos necesarios para el correcto funcionamiento de las vistas. */
 		$this->page_title = 'Mejoratuescuela.org widget';	
 		$this->include_template('index','widgets');
 	}	
-	public function reforma(){	
+	public function reforma(){
+		/* Obtiene los datos necesarios para el correcto funcionamiento de las vistas. */
 		$this->page_title = 'Mejoratuescuela.org widget';
 		$this->include_template('reforma','widgets');
 	}
 	public function generate(){
+		/* Genera y imprime la url para acceder al widget que se encuentra en el 'index'.*/
 		$widget->title = 'Ayuda a transformar tu colegio';
 		$widget->text = 'Consulta los resultados de Enlace de las escuelas públicas y privadas del País y aprende cómo puedes ayudar a mejorar la educación de tu centro escolar.';
 		$widget->news_title = 'Cabeza de la nota aquí';
