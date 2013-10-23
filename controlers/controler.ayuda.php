6ea005919e9e6f92b5f65c87ac9ce014ad7c5082
urielaero <aero.uriel@gmail.com>
2013-10-22 22:18:49 -0500

    docu faltante
    Former-commit-id: 594e0b1c84b31a02812f7921b6c84ee3da6ee5da
    Former-commit-id: b4e4946267161004762aeda8f229085eb6d73420
    Former-commit-id: 57e47e9833991f3639ffe127ba778cef890c6586
    Former-commit-id: d81221eb9f4e13387a8b5df1aa17d8c789705d60

--- controlers/controler.ayuda.php
+++ controlers/controler.ayuda.php
@@ -1,6 +1,9 @@
 <?php
 class ayuda extends main{
-	public function index(){		
+	/* Controlador: host/ayuda/*
+	*/
+	public function index(){
+		/* Obtiene los datos necesarios para el correcto funcionamiento de las vistas. */
 		$this->header_folder ='escuelas';
 		$this->breadcrumb = array('#'=>'Ayuda');
 		$this->include_theme('index','index');
--- controlers/controler.quienes_somos.php
+++ controlers/controler.quienes_somos.php
@@ -1,6 +1,9 @@
 <?php
 class quienes_somos extends main{
+	/* Controlador: host/quienes_somos/*
+	*/
 	public function index(){
+		/* Obtiene los datos necesarios para el correcto funcionamiento de las vistas. */
 		$this->title_header = '¿Quiénes somos?';
 		$this->header_folder = 'compara';
 		$this->breadcrumb = array('#'=>'¿Quiénes somos?');
