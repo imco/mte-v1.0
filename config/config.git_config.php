12a068899fca9e026c7dc763b95bac34e6d50955
El Sonny <sonny@spaceshiplabs.com>
2013-10-25 12:00:05 -0500

    config
    Former-commit-id: 125973dfea0802d93b9849f050166479c83a3148
    Former-commit-id: d974919a67a44e90cc8d7bdecb17f693591bb2c3
    Former-commit-id: 4a7d8838c9c9a37c079c3c35d0c38a2516184288
    Former-commit-id: aca1ccf957965bfa0ee3d871902feca200925ece

---
+++ config/config.git_config.php
@@ -0,0 +1,30 @@
+<?php
+class git_config extends default_config{
+	public function git_config(){
+		parent::__construct();
+		//Site
+		$this->http_address = 'http://comparatuescuela.projects.spaceshiplabs.com/'; 
+		$this->blog_address = 'http://comparatuescuela-blog.proyects.spaceshiplabs.com/';
+		$this->mxnphp_dir = "/var/www/mxnphp/";
+		$this->contact_email = 'aero.uriel@gmail.com';
+
+		$this->solr_server = 'http://busquedas.mejoratuescuela.org/solr/mte/';
+		
+		//Database
+		$this->db_host = '***REMOVED***';
+		$this->db_name = 'comparatuescuela';
+		$this->db_user = 'root';
+		$this->db_pass = 'RtG/()rERtfkfGKLF';
+
+		
+		$this->db_host = '***REMOVED***';
+		$this->db_name = '***REMOVED***';
+		$this->db_user = '***REMOVED***';
+		$this->db_pass = '***REMOVED***';
+		
+		//MXNPHP
+		$this->dev_mode = true;
+		
+	}
+}
+?>
