c6a79baf6979b41726c292a7e0c6b05d87a63be4
Angel Roca <angel_roca89@hotmail.com>
2013-10-29 09:50:17 -0600

    Comentarios a controlers
    Former-commit-id: 26574611fc09856183b2e8af0c9ef3f0a201b295
    Former-commit-id: 06cac1484ae24d21138d27e2602ee9ed9851139d
    Former-commit-id: a5dadc46c029bd20584e1fe4025423c2668b9855
    Former-commit-id: 2e9f1ed985b64fc6264936dbe37d60947158674c

--- controlers/controler.widgets.php
+++ controlers/controler.widgets.php
@@ -1,20 +1,35 @@
 <?php
+
+/**
+* Clase widgets Extiende main.
+* Controlador: host/widget
+* Se encarga de mostrar elementos que pueden ser embebidos en otras paginas
+*/
 class widgets extends main{
-	/* Controlador: host/widget/*
-	   Se encarga de mostrar elementos que pueden ser embebidos en otras paginas.
+	
+	/**
+	* Funcion Publica index.
+	* Obtiene los datos necesarios para el correcto funcionamiento de las vistas
 	*/
 	public function index(){
-		/* Obtiene los datos necesarios para el correcto funcionamiento de las vistas. */
 		$this->page_title = 'Mejoratuescuela.org widget';	
 		$this->include_template('index','widgets');
 	}	
+
+	/**
+	* Funcion Publica reforma.
+	* Obtiene los datos necesarios para el correcto funcionamiento de las vistas
+	*/
 	public function reforma(){
-		/* Obtiene los datos necesarios para el correcto funcionamiento de las vistas. */
 		$this->page_title = 'Mejoratuescuela.org widget';
 		$this->include_template('reforma','widgets');
 	}
+
+	/**
+	* Funcion Publica generate.
+	* Genera y imprime la url para acceder al widget que se encuentra en el 'index'
+	*/
 	public function generate(){
-		/* Genera y imprime la url para acceder al widget que se encuentra en el 'index'.*/
 		$widget->title = 'Ayuda a transformar tu colegio';
 		$widget->text = 'Consulta los resultados de Enlace de las escuelas públicas y privadas del País y aprende cómo puedes ayudar a mejorar la educación de tu centro escolar.';
 		$widget->news_title = 'Cabeza de la nota aquí';
