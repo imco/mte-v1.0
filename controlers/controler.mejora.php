692d23d918a7b3ba0d30aeeb3597f54f7c77ff7c
Angel Roca <angel_roca89@hotmail.com>
2013-10-29 09:50:17 -0600

    Comentarios a controlers
    Former-commit-id: 4e80a959909a11c734a65bfacd761b087d9746d2
    Former-commit-id: 77e02881a45b0e9b1e86eac05c942c28a8537d42
    Former-commit-id: 6c21f3730b97c6bf35ff179fd72324f6161a89a9
    Former-commit-id: 2dae0802f3d208c7cde05aa85a682687fd46959b

--- controlers/controler.mejora.php
+++ controlers/controler.mejora.php
@@ -1,9 +1,16 @@
 <?php
+
+/**
+* Clase mejora Extiende main.
+* Controlador: host/mejora
+*/
 class mejora extends main{
-	/* Controlador: host/mejora/*
+	
+	/**
+	* Funcion Publica index.
+	* Obtiene los datos necesarios para el correcto funcionamiento de las vistas.
 	*/
 	public function index(){
-		/* Obtiene los datos necesarios para el correcto funcionamiento de las vistas. */
 		$this->title_header = 'Mejora tu escuela';
 		$this->subtitle_header = 'Aquí encontrarás herramientas para que actúes como agente <br />de cambio positivo en tu comunidad educativa. <br />¡Participa e involúcrate!';
 		$this->header_folder = 'compara';
@@ -11,6 +18,5 @@ class mejora extends main{
 		$this->meta_description = "Tú puedes ayudar a tus hijos a hacer sus tareas y formar hábitos de lectura. En Mejora tu escuela tenemos tips para papás y niños que les ayudarán a aprender mejor.";
 		$this->include_theme('index','index');
 	}
-
 }
 ?>
