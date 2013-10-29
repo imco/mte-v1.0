5a92b4d4ad2a04530f00e35f4e27ec72156acf4e
El Sonny <sonny@spaceshiplabs.com>
2013-10-29 21:12:27 +0100

    rm debug
    Former-commit-id: 9ca2a8aa716f26c9aaba5e8fda3ad0bee4770ff5
    Former-commit-id: 0f25a0dc0d86cc32054ca8256c453831c78b42a3
    Former-commit-id: 9124b2ad5bd579c9df8cc8ff0ae9d93e087caf05
    Former-commit-id: 1ab66ac7efe51c6c59e688f2d0a334acbdb19365

--- controlers/controler.home.php
+++ controlers/controler.home.php
@@ -34,7 +34,7 @@ class home extends main{
 		$this->nivel_5 = $params->nivel = $niveles[rand(0,2)];
 		$params->entidad = $this->user_location->id;
 		$params->limit = '0,5';
-		$this->debug = true;
+		$this->debug = false;
 		$this->get_escuelas($params);
 		$this->process_escuelas();
 	}
