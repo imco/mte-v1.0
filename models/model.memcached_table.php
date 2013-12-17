510a1e259b08ea18921fc3f30fd0e2fc40137d82
El Sonny <sonny@spaceshiplabs.com>
2013-12-16 19:27:18 -0600

    memcached debug
    Former-commit-id: b47b3571e7a773cd74ae8774439f447d045759ad
    Former-commit-id: d5ee03cb7f0aea4c723c5eea5afcb67f9e9df97b
    Former-commit-id: abee6af0ffb788a7e65f5c066c7a372a9512344e
    Former-commit-id: 8053e0a042d880313fac309e8eb274418d0ba6c9

--- models/model.memcached_table.php
+++ models/model.memcached_table.php
@@ -1,25 +1,25 @@
 <?php
 class memcached_table extends table{
 	function read($fields){
-		//$time_start = microtime(true);
+		$time_start = microtime(true);
 		if(class_exists('Memcache')){
 			$memcache = new Memcache;	
-			$memcache->connect('***REMOVED***', 11211) or die ("Could not connect memcache");
+			$memcache->connect('10.208.99.46', 11211) or die ("Could not connect memcache");
 			$this->execute = false;
 			parent::read($fields);
 			$this->execute = true;
 			$query_hash = sha1($this->sql);
 			if($result = $memcache->get($query_hash)){				
-				//$time_end = microtime(true);
-				//$time = $time_end - $time_start;
-				//echo 'Memcached: '.$time.'<br/>';
+				$time_end = microtime(true);
+				$time = $time_end - $time_start;
+				echo 'Memcached: '.$time.'<br/>';
 				return $result;
 			}else{
 				$result = parent::read($fields);				
-				//$time_end = microtime(true);
+				$time_end = microtime(true);
 				$memcache->set($query_hash,$result,false,0);
-				//$time = $time_end - $time_start;
-				//echo "Query from DB:".$time."<br/>";
+				$time = $time_end - $time_start;
+				echo "Query from DB:".$time."<br/>";
 
 				return $result;
 			}
