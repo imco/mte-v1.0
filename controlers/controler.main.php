081fd16a22f4fe376ac29fd3db61c89dfa0a6aca
El Sonny <sonny@spaceshiplabs.com>
2014-01-11 13:14:29 -0600

    compare fix
    Former-commit-id: ccc93c72af9bfd1c9da84333e1543d9f24906103
    Former-commit-id: 3165c25097d66caa08042783961e5a40bec96665
    Former-commit-id: 3d8eb29249dd924a1a1a3522760d6f9d714d3da6
    Former-commit-id: af2660855ae97a9bbff9d81e74965b562fe1d8e3

--- controlers/controler.main.php
+++ controlers/controler.main.php
@@ -21,7 +21,7 @@ class main extends controler{
 		$this->breadcrumb = false; 
 		$this->draw_map = false; 
 		$this->draw_charts = false; 
-		//$this->get_data_compara_float();
+		$this->get_data_compara_float();
 	}
 
 	/** 
