7a5f31156aebf594c02e297f1f7f9e8a7ed72b68
luis <luis19prz@gmail.com>
2014-01-16 16:24:19 -0600

    comentarios y share buttons
    Former-commit-id: 5e17486e5dac027a18fedaf5789c0ea4e0bdbe30
    Former-commit-id: 84daccf5465766ff91521215b2ad66c405595b5b
    Former-commit-id: 0499b61113beaae72c06fcbf3e03a34f749cceea
    Former-commit-id: 0165c9f0a75923e06963f55cc2c2687611fe93c4

--- controlers/controler.main.php
+++ controlers/controler.main.php
@@ -504,14 +504,14 @@ class main extends controler{
 		//$captcha = new Recaptcha($this->config->recaptcha_public_key,$this->config->recaptcha_private_key);
 		//return $captcha->form();
 
-		/*$sweetcaptcha = new Sweetcaptcha(
+		$sweetcaptcha = new Sweetcaptcha(
 		  $this->config->SWEETCAPTCHA_APP_ID, 
 		  $this->config->SWEETCAPTCHA_KEY, 
 		  $this->config->SWEETCAPTCHA_SECRET, 
 		  $this->config->SWEETCAPTCHA_PUBLIC_URL
-		);*/
+		);
 
-		//echo $sweetcaptcha->get_html();
+		echo $sweetcaptcha->get_html();
     }
 
     public function get_data_compara_float(){
