cb363397d55552e07d7ce7e11f2d32a5a6014612
luis <luis19prz@gmail.com>
2014-01-20 09:18:15 -0600

    header text programas
    Former-commit-id: d55aa0977d23c98846436c65499f3e3535de6d9b
    Former-commit-id: 11d636eb59be5acdc8b187864e016b3e2a8ea3fa
    Former-commit-id: 34aeafbf9e39f20a6a50b5dc65a922d188f77a34
    Former-commit-id: 2326551e4372a10e9a2acfe84aef2f2b72106604

--- controlers/controler.programas.php
+++ controlers/controler.programas.php
@@ -11,13 +11,13 @@ class programas extends main{
 	*/
 	public function index(){
 		/* Obtiene los datos necesarios para el correcto funcionamiento de las vistas. */
-		$this->title_header = '¿Quiénes somos?';
+		$this->title_header = 'Programas';
 		$this->header_folder = 'compara';
-		$this->breadcrumb = array('#'=>'¿Quiénes somos?');
-		/*$this->subtitle_header = '
+		$this->breadcrumb = array('#'=>'Programas');
+		$this->subtitle_header = '
 			MejoraTuEscuela.org es una plataforma que busca <br />
 			promover la participación ciudadana para transformar <br />
-			la educación en México.';*/
+			la educación en México.';
 		$this->include_theme('index','index');
 	}
 }
