89bedec7bb99ac38072964dbd4a218869088d776
urielaero <aero.uriel@gmail.com>
2014-01-24 02:16:28 +0800

    tab charts
    Former-commit-id: 87fef532638601be81eaa5075e8378ec3e9f656c
    Former-commit-id: 678ce91e5cb9b1943bdcdd6b9e7e2dc82f3b6db9
    Former-commit-id: da9b1e9988bbe58d45d741072c4d19c87d7ac537
    Former-commit-id: 5b399f09252c286eebd4d109f82dac7ad438d093

--- templates/mtev1/js/school-charts.js
+++ templates/mtev1/js/school-charts.js
@@ -11,7 +11,8 @@ function drawChart(materia){
 	var raw_data = $.parseJSON($('#line-chart-data-'+materia).html());
 	var data = google.visualization.arrayToDataTable(raw_data);
 	var options = {
-	   chartarea : {width:210,height:200}
+	   chartArea : {width:295,height:94,left:40,top:35},
+	   legend: {position:'none'}
 	};
 	var chart = new google.visualization.LineChart(document.getElementById('profile-line-chart-'+materia));
 	chart.draw(data, options);
