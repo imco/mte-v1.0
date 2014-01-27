8ec9515da986c831853d1749f252b1fd996f9728
erick <erick@spaceshiplabs.com>
2014-01-27 13:43:06 -0600

    header and home
    Former-commit-id: c4369de6baab2d335d0dffb794af77e4f8ad47cb
    Former-commit-id: 3fc7ed685475497093786da885a99326ebedeaf9
    Former-commit-id: c0c2da2021adeb58f31237d8b12e0d3a8754f245
    Former-commit-id: dd6b47a163f2adfe2ebc82ed10a702b248ee1427

--- templates/mtev1/global/simple_search.php
+++ templates/mtev1/global/simple_search.php
@@ -1,4 +1,7 @@
 <form action='<?=$this->get('action') == 'califica'?'/califica-tu-escuela/califica':'/compara#resultados'?>' method='get' accept-charset='utf-8' class='general-search' id='general-search'>
+	<p class='button-frame search-adv'>
+		<a href="/compara/">Búsqueda avanzada</a>
+	</p>
 	<p class='button-frame'>
 		<input name='term' id='name-input' type='text' placeholder='Busca tu escuela' value='<?=$this->request('term');?>' />
 		<input type='submit' class='integrated' value='' />
@@ -10,8 +13,6 @@
 		<span class='icon sprites'></span>
 	</a></p>
 	-->
-	<p class='button-frame search-adv'>
-		<a href="/compara/">Búsqueda avanzada</a>
-	</p>
+	<div class='clear'></div>
  </form>
 
