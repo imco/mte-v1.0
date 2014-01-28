736f9057ad4d19be656e69770e15026cb6cb4b6b
luis <luis19prz@gmail.com>
2014-01-28 09:21:16 -0600

    fix programas
    Former-commit-id: 8e43b227ced8e9d86c349a7073dff398e78a3241
    Former-commit-id: 4f977a1b84c1da69cd146eacf7519843dea8d2cb
    Former-commit-id: b95e7e5e6b8b69a7744f11daa7cf5c12f7567d81
    Former-commit-id: b593038609d50eac1bb25b144e28f873b3b629ca

--- templates/mtev1/programas/index.php
+++ templates/mtev1/programas/index.php
@@ -124,7 +124,14 @@
 
 		</script>
 
-		<div class="overlay-map"></div>
+		<div class="overlay-map">
+			<script>
+				<?php $arrayEstados = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,31); ?>
+				<?php foreach ($arr as $item) : ?>
+	   				arr.push('<?php echo $item?>');
+			   <?php endforeach; ?>
+			</script>
+		</div>
 
 	</div>
 	<div class="column right">
