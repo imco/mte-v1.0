eea81d0693df31fcdf48967a0611b188f113cb83
urielaero <aero.uriel@gmail.com>
2014-02-01 00:55:20 -0600

    restableciendo archivos
    Former-commit-id: 90c18ddca53790b543deab437be95cd55779140e
    Former-commit-id: fef41b455ac3ec98d9deb3c7073c8f3f45a27406
    Former-commit-id: 5525541cd203f9e8e9f29f48a689684298683028
    Former-commit-id: 37ee8c59fdb6d4391ff2f7372cd8030e3f699bf5

---
+++ controlers/controler.programas.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+* Clase programas Extiende main.
+* Controlador: host/programas
+*/
+class programas extends main{
+	/**
+	* Funcion Publica index.
+	* Obtiene los datos necesarios para el correcto funcionamiento de las vistas.
+	*/
+	public function index(){
+		/* Obtiene los datos necesarios para el correcto funcionamiento de las vistas. */
+		$this->programa_info();
+        $this->load_programas();
+		$this->title_header = 'Programas';
+		$this->header_folder = 'compara';
+		$this->breadcrumb = array('#'=>'Programas');
+		$this->subtitle_header = '
+			MejoraTuEscuela.org es una plataforma que busca <br />
+			promover la participación ciudadana para transformar <br />
+			la educación en México.';
+		$this->include_theme('index','index');
+	}
+
+	private function programa_info(){
+		$this->programa = new programa($this->get('id'));
+		$this->programa->read("id,nombre,tema,descripcion,zonas,requisitos,direccion,telefono,mail,telefono_contacto,sitio_web,m_collection");
+        $this->programa->entidad_escuelas_count = $this->get_estado_escuelas_count($this->programa->m_collection);
+	}
+
+    /**
+     * requiere m_collection seteado
+     * setead escuelas y estado_escuelas
+     * */
+    private function get_estado_escuelas_count($m_collection = false){
+        $estado_escuelas = array();
+
+        if (!$m_collection) return $estado_escuelas;
+        try {
+            $m = new MongoClient("mongodb://***REMOVED***:***REMOVED***@***REMOVED***:27017/mte_produccion");
+            $db = $m->selectDB("mte_programas");
+            $c = $db->selectCollection($m_collection);//pec,jornada_amplia,siat,censo_2013
+
+            $max_aux = $c->find()->sort(array ("anio" => -1))->limit(1);
+            $aux = $max_aux->getNext();
+            $max_anio = isset($aux['anio']) ? $aux['anio'] : false ;
+
+            for($i=1;$i<=32;$i++) {
+                $aux = $i;
+                if ($i < 10) {
+                    $aux = '0'.$i;
+                }
+                if ($max_anio) {
+                    $estado_escuelas[$i] = $c->count(array( "anio" => $max_anio , "cct" => array('$regex' => '\A'.$aux.'.*') ));
+                } else {
+                    $estado_escuelas[$i] = $c->count(array( "cct" => array('$regex' => '\A'.$aux.'.*') ));
+                }
+            }
+
+            $m->close();
+        } catch(Exception $ex) {
+            if ($this->debug) {
+                var_dump($ex);
+                throw $ex;
+            }
+            return $estado_escuelas;
+        }
+
+        return $estado_escuelas;
+    }
+
+    private function get_estado_escuelascct($programa,$estado_id){
+        $escuelas = array();
+        $this->programa = new programa($programa);
+        $this->programa->read("id,m_collection");
+
+        if (!$this->programa->m_collection) return $estado_escuelas;
+        try {
+            $m = $this->mongo_connect();
+            $db = $m->selectDB("mte_programas");
+            $c = $db->selectCollection($this->programa->m_collection);//pec,jornada_amplia,siat,censo_2013
+
+            $max_aux = $c->find()->sort(array ("anio" => -1))->limit(1);
+            $aux = $max_aux->getNext();
+            $max_anio = isset($aux['anio']) ? $aux['anio'] : false ;
+            if ($max_anio) {
+                $escuelasaux = $c->find(array( "anio" => $max_anio , "cct" => array('$regex' => '\A'.$estado_id.'.*') ))->limit(20);
+            } else {
+                $escuelasaux = $c->find(array( "cct" => array('$regex' => '\A'.$estado_id.'.*') ))->limit(20);
+            }
+
+            $i = 0;
+            while($escuelasaux->hasNext()) {
+                $aux = $escuelasaux->getNext();
+                $escuelas[$i++] = $aux['cct'];
+            }
+
+            $m->close();
+        } catch(Exception $ex) {
+            if ($this->debug) {
+                var_dump($ex);
+                throw $ex;
+            }
+            return $escuelas;
+        }
+        return $escuelas;
+    }
+
+    public function estado_escuelas(){
+        $programa = $this->post('id');
+        $estado = $this->post('es');
+        if ($estado < 10) $estado = '0'.$estado;
+        $ccts = $this->get_estado_escuelascct($programa,$estado);
+        $params = new stdClass();
+        $params->ccts = $ccts;
+        $params->limit = 20;
+        $params->order_by = "ISNULL(escuelas.rank_entidad), escuelas.rank_entidad ASC, escuelas.promedio_general DESC";
+        $this->get_escuelas($params);
+        $this->include_template("estado_escuelas","programas/partial");
+    }
+
+}
+
+?>
---
+++ models/model.escuela.php
@@ -0,0 +1,172 @@
+<?php
+class escuela extends memcached_table{
+	public function info(){
+		$this->table_name = "escuelas";
+		$this->key = 'id';
+		$this->objects['localidad'] = 'localidad';
+		$this->objects['entidad'] = 'entidad';
+		$this->objects['municipio'] = 'municipio';
+		$this->objects['nivel'] = 'nivel';
+		$this->objects['turno'] = 'turno';
+		$this->objects['tipo'] = 'tipo';
+		$this->objects['subnivel'] = 'subnivel';
+		$this->objects['servicio'] = 'servicio';
+		$this->objects['control'] = 'control';
+		$this->objects['subcontrol'] = 'subcontrol';
+		$this->objects['sostenimiento'] = 'sostenimiento';
+		$this->objects['status'] = 'status';
+
+		$this->has_many['enlaces'] = 'enlace';
+		$this->has_many_keys['enlaces'] = 'cct';
+
+		$this->has_many['calificaciones'] = 'calificacion';
+		$this->has_many_keys['calificaciones'] = 'cct';
+
+		$this->has_many['reportes_ciudadanos'] = 'reporte_ciudadano';
+		$this->has_many_keys['reportes_ciudadanos'] = 'id_cct';
+
+		$this->semaforos = array('Reprobado','De Panzazo','Bien','Excelente','Sin Enlace','Poco confiable');
+		#$this->semaforo_rangos[12] = array(433,524,615,900);
+		$this->semaforo_rangos[12] = array(559,601,662,900);
+		$this->semaforo_rangos[13] = array(511,544,591,900);
+		$this->semaforo_rangos[22] = array(551,580,632,900);
+		$this->semaforo_poco_confiable = 10;
+
+	}
+	public function get_semaforo(){
+		$this->semaforo = 4;
+		$porcentaje_poco_confiable = $this->poco_confiables > 0 && $this->total_evaluados > 0?($this->poco_confiables * 100) / $this->total_evaluados:0;
+		$this->porcentaje_poco_confiable = number_format($porcentaje_poco_confiable,2);
+		$turnos = isset($this->turno->num) ? $this->turno->num : 1;
+
+		if($this->nivel->nombre=="PREESCOLAR"){
+			$this->semaforo = 7;		
+		}
+
+		if(isset($this->grados) && $this->grados>0 ){
+			if($this->nivel->nombre != "BACHILLERATO"  && ($this->grados < 4 * $turnos && $this->nivel->nombre == "PRIMARIA") || ($this->grados < 3 * $turnos && $this->nivel->nombre == "SECUNDARIA") ){
+				$this->semaforo = 6;//no se cuentan
+			}else{
+				
+				if($porcentaje_poco_confiable > 0 && $porcentaje_poco_confiable >= $this->semaforo_poco_confiable){
+					$this->semaforo = 5;//no confiables
+				}else{
+					
+					if( $this->promedio_general > 0){
+						
+						if( $this->promedio_general < $this->semaforo_rangos[$this->nivel->id][0])
+							$this->semaforo=0;
+						else
+							if( $this->promedio_general < $this->semaforo_rangos[$this->nivel->id][1] )
+								$this->semaforo = 1;
+							else
+								if( $this->promedio_general < $this->semaforo_rangos[$this->nivel->id][2] )
+									$this->semaforo = 2;
+								else
+									$this->semaforo = 3;
+				}
+			}
+		}
+
+/*		
+		if(isset($this->grados) && $this->grados>0 ){
+			if( $this->nivel->nombre != "BACHILLERATO"  && ($this->grados < 4 * $turnos && $this->nivel->nombre == "PRIMARIA") || ($this->grados < 3 * $turnos && $this->nivel->nombre == "SECUNDARIA") ){
+				$this->semaforo = 6;
+			}else if($porcentaje_poco_confiable > 0 && $porcentaje_poco_confiable >= $this->semaforo_poco_confiable){
+				$this->semaforo = 5;
+			}else if(isset($this->semaforo_rangos[$this->nivel->id]) && $this->promedio_general > 0){
+				while($this->promedio_general > $this->semaforo_rangos[$this->nivel->id][$this->semaforo])$this->semaforo++;
+			}
+		}*/
+		}
+	}
+	public function rank($nivel,$entidad = false,$municipio = false){
+		$entidad_clause = $entidad ? " AND entidad LIKE '$entidad'" : '';
+		$sql = "SET @rownum = 0, @rank = 0, @prev_val = NULL; ";
+		mysql_query($sql);
+		$sql = "UPDATE escuelas t1
+				JOIN (
+				SELECT @rownum := @rownum + 1 AS row,
+				@rank := IF(@prev_val!=promedio_general,@rownum,@rank) AS rank,
+				@prev_val := promedio_general AS promedio_general,
+				cct
+				FROM escuelas
+				WHERE nivel LIKE '$nivel' $entidad_clause AND `promedio_general` IS NOT NULL
+				ORDER BY promedio_general DESC) t2
+				ON t1.cct=t2.cct
+				SET t1.rank_entidad=t2.rank;";
+		return mysql_query($sql);		
+	}
+	public function get_chart($materia){
+		$grados = array();
+		$enlaces = array();
+		$puntaje_name = 'puntaje_'.$materia;
+		if(isset($this->enlaces) && $this->enlaces){
+			$variable = array();
+			foreach($this->enlaces as $enlace){
+				$enlaces[$enlace->anio][$enlace->grado] = $enlace->$puntaje_name;
+				$grados[$enlace->grado] = $enlace->grado;
+			}
+			ksort($enlaces);
+			$grados = array_values($grados);
+			sort($grados);
+			$keys = array_flip($grados);
+			array_unshift($grados,'Año');
+			$variable[] = $grados;
+			foreach($enlaces as $anio => $grados){				
+				$row = array_fill(0,count($keys),0);
+				foreach($grados as $key => $puntaje){
+					//var_dump($keys[$key]);
+					$row[$keys[$key]] = intval($puntaje);
+				}
+				array_unshift($row,strval($anio));
+				$variable[] = $row;
+			}
+		}else{
+			$variable = false;
+		}
+		return $variable;
+	}
+	public function get_mongo_info($client){
+		if($client){			
+			//Produccion
+			$db = $client->selectDB("mte_produccion");
+			$c = $db->selectCollection('censo_2013');
+			$this->censo_2013 = $c->find(array('cct'=>$this->cct));
+			$c = $db->selectCollection('snie');
+			$this->snie = $c->find(array('cct'=>$this->cct));
+			$this->infraestructura = false;
+			if($this->snie){
+				$keys = array(12=>'primaria_pub_infraestructura',13=>'primaria_pub_infraestructura',22=>'primaria_pub_infraestructura');
+
+				foreach($this->snie as $e){
+					$this->infraestructura = json_decode($e[$keys[$this->nivel->id]]);
+					break;
+				}
+				$this->infraestructura = is_array($this->infraestructura) ? $this->infraestructura : false;
+			}
+			//Programas Federales
+			$db = $client->selectDB("mte_programas");
+			$programas = array('pec','pes','petc');
+			$this->load_programas($programas,$db);
+			//OSCs
+			$programas = array('proeducacion','tarahumara','teach_mexico','mexprim','empresa_impulsa','emprender_impulsa','emprendedores_impulsa','dinero_impulsa','fundacion_televisa','naciones_unidas');
+			$this->load_programas($programas,$db);
+
+			$client->close();
+		}else{
+			$programas = array('censo_2013','snie','infraestructura', 'pec','pes','petc','proeducacion','tarahumara','teach_mexico','mexprim','empresa_impulsa','emprender_impulsa','emprendedores_impulsa','dinero_impulsa','fundacion_televisa','naciones_unidas');
+			foreach($programas as $programa){
+				$this->$programa = false;
+			}
+		}
+	}
+	private function load_programas($programas,$db){
+		foreach($programas as $programa){
+			$c = $db->selectCollection($programa);
+			$this->$programa = $c->find(array('cct'=>$this->cct));
+			$this->$programa = iterator_to_array($this->$programa);
+		}
+	}
+}
+?>
