75171a290ce45c07159b522c9b4bbec7350fdb0a
El Sonny <sonny@spaceshiplabs.com>
2014-02-06 20:03:09 -0600

    config updates
    Former-commit-id: 70c00d6d83a84ddae431b9118f389609a2154b64
    Former-commit-id: 21535188ea9f8995b22630ebf513ab1c3e91b24a
    Former-commit-id: 56a1109040ea10db0dfcb788210e18eabb394da7
    Former-commit-id: 0b7788e79e4026b4f6f53b3d5cf969edabe25fe9

--- config/config.default_config.php
+++ config/config.default_config.php
@@ -15,56 +15,24 @@ class default_config{
                 $this->lang = "en";
                 $this->dev_mode = true;
                 $this->search_location = false;
-                $this->memcache_host = '***REMOVED***';
-
-                //MTE
-                $this->semaforos = array('Reprobado','De panzazo','Bien','Excelente','No tomÃ³ la <br /> prueba <br />ENLACE','Poco confiable','Esta escuela no tomÃ³ la prueba ENLACE para todos los aÃ±os','La prueba ENLACE no esta disponible para este nivel escolar');
-
-                //Change.org
-                $this->change_api_key = '***REMOVED***';
-                $this->change_secret_token = '***REMOVED***';
-
-                //twitter
-                $this->twitter_access_token ='***REMOVED***';
-                $this->twitter_access_token_secret = '***REMOVED***';
-                $this->twitter_consumer_key = '***REMOVED***';
-                $this->twitter_consumer_secret = '***REMOVED***';
-
-                //hoot suite
-                $this->hootSuite_api_key = '***REMOVED***';
-
-                //recapcha
-                $this->recaptcha_public_key = '***REMOVED***';
-                $this->recaptcha_private_key = '***REMOVED***';
-
+                $this->memcache_host = '***REMOVED***';                
                 $this->contact_email = 'contacto@mejoratuescuela.org';
                 $this->image_email = 'sonny@spaceshiplabs.com';
                 $this->image_email = 'ariadna.camargo@imco.org.mx';
                 $this->tynt = false;
 
-
-                //sweetcaptcha
-                $this->SWEETCAPTCHA_APP_ID = 40218; // your application id (change me)
-                $this->SWEETCAPTCHA_KEY = '***REMOVED***'; // your application key (change me)
-                $this->SWEETCAPTCHA_SECRET = '***REMOVED***'; // your application secret (change me)
-                $this->SWEETCAPTCHA_PUBLIC_URL ='sweetcaptcha.php'; // public http url to this file
-
-                //signs sizes
+                //Image Sizes
                 $this->icon_sizes = json_decode('[
                         {"width":"50","height":"50","slug":"tiny"},
                         {"width":"156","height":"112","slug":"signs" ,"resize_type":"best fit"}
                 ]');
+               //MTE
+                $this->semaforos = array('Reprobado','De panzazo','Bien','Excelente','No tomá la <br /> prueba <br />ENLACE','Poco confiable','Esta escuela no tomá la prueba ENLACE para todos los años','La prueba ENLACE no esta disponible para este nivel escolar');
 
-                //sendGrid
-                $this->send_grid_user = "***REMOVED***";
-                $this->send_grid_key  = "***REMOVED***";
-
-                //rack space
-                $this->rack_space_user = "***REMOVED***";
-                $this->rack_space_key = "***REMOVED***";
-
-                $this->mongo_server = '***REMOVED***';
-                $this->mongo_user = '***REMOVED***:***REMOVED***';
+                
         }
+
+                
+               
 }
 ?>
--- config/config.sample_config.php
+++ config/config.sample_config.php
@@ -16,6 +16,37 @@ class sample_config extends default_config{
 
 		//MXNPHP
 		$this->dev_mode = true;
+
+		/****
+		Keys
+		****/
+		//Change.org
+		$this->change_api_key = false;
+		$this->change_secret_token = false;
+		//Twitter
+		$this->twitter_access_token = false;
+		$this->twitter_access_token_secret = false;
+		$this->twitter_consumer_key = false;
+		$this->twitter_consumer_secret = false;
+		//Hoot Suite
+		$this->hootSuite_api_key = false;
+		//recapcha
+		$this->recaptcha_public_key = false;
+		$this->recaptcha_private_key = false;
+		//sweetcaptcha
+		$this->SWEETCAPTCHA_APP_ID false;
+		$this->SWEETCAPTCHA_KEY = false;
+		$this->SWEETCAPTCHA_SECRET = false;
+		$this->SWEETCAPTCHA_PUBLIC_URL = false;
+		//sendGrid
+		$this->send_grid_user = false;
+		$this->send_grid_key  = false;
+		//rack space
+		$this->rack_space_user = false;
+		$this->rack_space_key = false;
+		//Mongo
+		$this->mongo_server = false;
+		$this->mongo_user = false;
 		
 	}
 }
