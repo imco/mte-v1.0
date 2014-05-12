0e350f6f0835a8a1fc0e10e03c9ebf36a137e730
luis <luis19prz@gmail.com>
2014-05-12 16:32:11 -0500

    pagina bases

---
+++ controlers/controler.bases.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Clase bases Extiende main.
+* Controlador: host/bases
+*/
+class bases extends main{
+	/*
+	* Funcion Publica index 
+	* Obtiene los datos necesarios para el correcto funcionamiento de las vistas.
+	*/
+	public function index(){
+		//$this->header_folder ='escuelas';
+		$this->breadcrumb = array('#'=>'Descargas');
+		$this->include_theme('index','index');
+	}
+}
+?>
