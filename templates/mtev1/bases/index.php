49de744f33a269f447d7991410185298e27aedfd
luis <luis19prz@gmail.com>
2014-05-13 21:37:51 -0500

    pagina bases fixes

--- templates/mtev1/bases/index.php
+++ templates/mtev1/bases/index.php
@@ -1,17 +1,16 @@
 <div class="container bases">
 	<img src="/templates/mtev1/img/bases/BANNER_IMCO_datos.jpg" alt="Bases de datos">
-	<h1>Descarga aqui bases de datos de MejoraTuEscuela.org</h1>
 	<ul>
-		<li><p>Base con datos del Censo de Escuelas, Maestros y Alumnos, SEP,INEGI</p><p>disponible en <a href="#">cemabe.inegi.org.mx</a></p><a href="" class="button-download blue-effect">Descargar</a></li>
-		<li><p>Base con datos del Censo de Escuelas, Maestros y Alumnos, SEP,INEGI</p><p>disponible en <a href="#">cemabe.inegi.org.mx</a></p><a href="" class="button-download blue-effect">Descargar</a></li>
-		<li><p>Base con datos del Censo de Escuelas, Maestros y Alumnos, SEP,INEGI</p><p>disponible en <a href="#">cemabe.inegi.org.mx</a></p><a href="" class="button-download blue-effect">Descargar</a></li>
-		<li><p>Base con datos del Censo de Escuelas, Maestros y Alumnos, SEP,INEGI</p><p>disponible en <a href="#">cemabe.inegi.org.mx</a></p><a href="" class="button-download blue-effect">Descargar</a></li>
+		<li><p>Base con datos del Censo de Escuelas, Maestros y Alumnos, SEP,INEGI</p><p>disponible en <a href="http://cemabe.inegi.org.mx/">cemabe.inegi.org.mx</a></p><a href="" class="button-download blue-effect">Descargar</a></li>
+		<li><p>Base con datos del Censo de Escuelas, Maestros y Alumnos, SEP,INEGI</p><p>disponible en <a href="http://cemabe.inegi.org.mx/">cemabe.inegi.org.mx</a></p><a href="" class="button-download blue-effect">Descargar</a></li>
+		<li><p>Base con datos del Censo de Escuelas, Maestros y Alumnos, SEP,INEGI</p><p>disponible en <a href="http://cemabe.inegi.org.mx/">cemabe.inegi.org.mx</a></p><a href="" class="button-download blue-effect">Descargar</a></li>
+		<li><p>Base con datos del Censo de Escuelas, Maestros y Alumnos, SEP,INEGI</p><p>disponible en <a href="http://cemabe.inegi.org.mx/">cemabe.inegi.org.mx</a></p><a href="" class="button-download blue-effect">Descargar</a></li>
 
 	</ul>
 	<div class="from">
-		<p>Datos obtenidos de:</p>
-		<a href="#"><img src="/templates/mtev1/img/bases/datamx.png" alt="datamx"></a>
-		<a href="#"><img src="/templates/mtev1/img/bases/codeando.png" alt="codeandomexico"></a>
+		<p>Plataforma para compartir bases de datos de:</p>
+		<img src="/templates/mtev1/img/bases/datamx.png" alt="datamx">
+		<img src="/templates/mtev1/img/bases/codeando.png" alt="codeandomexico">
 
 	</div>
 </div>
