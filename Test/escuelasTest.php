b6e693f0abd27065b9db23c72aed8aff884ed8fc
urielaero <aero.uriel@gmail.com>
2014-07-18 00:51:16 -0500

    database integrity testing

--- Test/escuelasTest.php
+++ Test/escuelasTest.php
@@ -11,6 +11,7 @@ class escuelasTest extends defaultTest{
 		$fields = array('grados','promedio_matematicas','promedio_espaniol','promedio_general','total_evaluados');
 		$fieldsTxt = "tipo,cct,".implode(",",$fields)."\n";
 		//echo $fieldsTxt;
+		echo "cct,estatus,alumnos db,alumnos recalculo\n";
 		$nivel = new nivel();
 		$nivel->search_clause = 1;
 		$niveles = $nivel->read('id');
@@ -23,54 +24,70 @@ class escuelasTest extends defaultTest{
 	private function escuela_grados($nivel,$fields){
 		$escuela = new escuela();
 		$escuela->search_clause = "nivel = $nivel";
+		$escuela->limit = 15;
 		$escuela = $escuela->read('cct,grados,promedio_matematicas,promedio_espaniol,promedio_general,total_evaluados');
 		foreach($escuela as $e){
 			if($e->cct != "NA"){
-				$enlace =  new enlace();	
-				$enlace->search_clause = 'cct="'.$e->cct.'" AND anio = "2013"';
-				$enlaces = $enlace->read('id,anio,nivel,grado,turnos,puntaje_espaniol,puntaje_matematicas,puntaje_geografia,alumnos_que_contestaron_total');
 				$values = array();
 				foreach($fields as $key){
 					$values[] = $e->$key;
 				}
-				if(count($enlaces)){
-					$grados = $sum_spa = $sum_mat = $sum_geo = 0;
-					$alumnos_total = 0;
-					foreach($enlaces as $enlace){
-						if($enlace->alumnos_que_contestaron_total != 0 && ($enlace->puntaje_espaniol != 0 && $enlace->puntaje_matematicas != 0)){
-							$grados++;
-							$sum_spa += $enlace->puntaje_espaniol;
-							$sum_mat += $enlace->puntaje_matematicas;
-							$alumnos_total += $enlace->alumnos_que_contestaron_total;
-						}
-					}
-					$prom_mat = $sum_mat / $grados;
-					$prom_spa = $sum_spa / $grados;
-					$gen = ($prom_spa * .2) + ($prom_mat *.8);
-					$expected = array($grados,$prom_mat,$prom_spa,$gen,$alumnos_total);
-				}else{
-					$expected = array(0,0,0,0,0);
+				$expected = $this->enlace_por_anio(2013,$e->cct);
+				if(!$expected){//no enlace
 					continue;
 				}
-				//$dataNow = "actual,".$e->cct;
-				//$dataNew = "nuevo,".$e->cct;
-				if($e->grados != $expected[0]){
-					/*
-					foreach($values as $i=>$value){
-						$dataNow.=",".$value;
-						$dataNew.=",".$expected[$i];
-
+				if($e->grados != $expected[0]){	
+					$valid = $this->is_equals($values,$expected);
+					//$this->AssertEquals($values,$expected);
+					if(!$valid){
+						$expected2 = $this->enlace_por_anio(2012,$e->cct);
+					    	$valid = $this->is_equals($values,$expected2);
+					    	//$this->AssertEquals($values,$expected2);
+					    	if($valid){
+					    		echo $e->cct.",coincide con el año 2012";
+					    	}else{
+					    		echo $e->cct.",no coincide recalculo 2013 ni 2012";
+					    	}
+					    	echo ",".$e->total_evaluados.",$expected[4]\n";
 					}
-					echo $dataNow."\n";
-					echo $dataNew."\n";
-					*/
-					$this->AssertEquals($values,$expected);
 				}
-
-			
 			}
 		}
 
+	}
+
+	private function enlace_por_anio($anio,$cct){
+		$enlace =  new enlace();	
+		$enlace->search_clause = 'cct="'.$cct.'" AND anio = "'.$anio.'"';
+		$enlaces = $enlace->read('id,anio,nivel,grado,turnos,puntaje_espaniol,puntaje_matematicas,puntaje_geografia,alumnos_que_contestaron_total');
+		if(count($enlaces)){
+			$grados = $sum_spa = $sum_mat = $sum_geo = 0;
+			$alumnos_total = 0;
+			foreach($enlaces as $enlace){
+				if($enlace->alumnos_que_contestaron_total != 0 && ($enlace->puntaje_espaniol != 0 && $enlace->puntaje_matematicas != 0)){
+					$grados++;
+					$sum_spa += $enlace->puntaje_espaniol;
+					$sum_mat += $enlace->puntaje_matematicas;
+					$alumnos_total += $enlace->alumnos_que_contestaron_total;
+				}
+			}
+			$prom_mat = $sum_mat / $grados;
+			$prom_spa = $sum_spa / $grados;
+			$gen = ($prom_spa * .2) + ($prom_mat *.8);
+			return  array($grados,$prom_mat,$prom_spa,$gen,$alumnos_total);
+		}else
+			return false;
+
+	}
+
+	private function is_equals($values,$expected){
+		foreach($values as $i=>$value){
+			if($value != $expected[$i]){
+				return false;
+			}
+		}
+		return true;
+	
 	}
 
 }
