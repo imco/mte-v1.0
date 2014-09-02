146061166278f11af09a42e9c948a7f7393a30fe
el-sonny <sonny@spaceshiplabs.com>
2014-09-02 14:35:25 -0500

    escuelas d excelencia

--- templates/mtev1/escuelas/perfil_b.php
+++ templates/mtev1/escuelas/perfil_b.php
@@ -1,5 +1,6 @@
 <div class='perfil container B'>
     <?php 
+    var_dump($this->escuela->programas);
     if(isset($this->escuela->programas['escuelas_de_excelencia']))
         $this->include_template('escuelas_excelencia','global');
     ?>
