22c5b03bd4a58088f93e6606fff2559ca0692c3e
el-sonny <sonny@spaceshiplabs.com>
2014-09-25 02:38:01 -0500

    filter

--- templates/mtev1/bases/censo.php
+++ templates/mtev1/bases/censo.php
@@ -3,7 +3,7 @@
 	<img src="http://3903b795d5baf43f41af-5a4e2dc33f4d93e681c3d4c060607d64.r40.cf1.rackcdn.com/banerdescarga.jpg" alt="pasos" />
 	<form class='file-search-form' method='get' action='#'   >
 			<label>Bases de datos del censo por estado</label>
-			<input type='text' name='state' ng-model='state' placeholder='Buscar estado' />
+			<input type='text' name='state' ng-model='searchText' placeholder='Buscar estado' />
 			<input type='submit' value='' class='submit' />
 			<div class='clear'></div>
 	</form>
@@ -20,7 +20,7 @@
 		</thead>
 		<tbody>
 			<tr class='space'><td></td><td></td><td></td><td></td><td></td></tr>
-			<tr ng-repeat='entidad in entidades' class='state' >
+			<tr ng-cloak ng-repeat='entidad in entidades | filter:searchText' class='state' >
 				<td>
 					<img ng-src='/templates/mtev1/img/bases/estados/{{entidad.entidad}}.png' alt='{{entidad.nombre}}' />
 					<p class='title1' ng-bind='entidad.nombre'></p>
